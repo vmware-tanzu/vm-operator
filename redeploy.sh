818058d47b042af5996bb8ef473d1df5597eade3
Derek Beard <dbeard@vmware.com>
2019-01-04 12:14:10 -0600

    Remove redeploy script in lieu of make targets

--- redeploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-apiserver-boot build container --image vmop
-docker tag vmop derekbeard/vmoperator:vmop
-docker push derekbeard/vmoperator:vmop
-kubectl delete -f config/apiserver.yaml
-kubectl apply -f config/apiserver.yaml --validate=false

8cd5f4c35b7ce077174d25bff06da4fd34a96dbc
Varun Jain <varunjain213@gmail.com>
2019-02-27 13:21:10 -0800

    VM Operator Health Check
    This patch adds a liveness check probe to apiserver and controller
    of VM Operator. The probe checks for liveness of these two containers
    for every 3 seconds and restarts the pod if either of them is found
    to be unhealthy.

--- cmd/controller-manager/main.go
+++ cmd/controller-manager/main.go
@@ -6,12 +6,27 @@ package main
 
 import (
 	"flag"
+	"net/http"
 
 	"github.com/golang/glog"
 	controllerlib "github.com/kubernetes-incubator/apiserver-builder-alpha/pkg/controller"
 
 	"vmware.com/kubevsphere/pkg/controller"
 )
+
+func runHealthServer() {
+	// Setup health check handler and corresponding listener on a custom port
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+                _, err := w.Write([]byte("ok"))
+                if err != nil {
+                        glog.Fatalf("ResponseWriter error: %s", err)
+                }
+	})
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		glog.Fatalf("ListenAndServe error: %s", err)
+	}
+}
 
 func main() {
 	// This isn't used
@@ -25,6 +40,8 @@ func main() {
 
 	controllers, _ := controller.GetAllControllers(config)
 	controllerlib.StartControllerManager(controllers...)
+
+	go runHealthServer()
 
 	// Blockforever
 	select {}

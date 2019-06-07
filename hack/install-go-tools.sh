2af30f09d28eb7228e61c284973748854d14dce8
Bryan Venteicher <bventeicher@vmware.com>
2019-06-07 14:05:28 -0500

    Update Jenkins build
      - Build with Go 1.12
      - Use Photon 3 for container
      - Adopt golangci-lint config from WCP/GCM projects
      - Use bin/ as the path for Docker build command so only the binaries
        are copied to speed up the container build
    
    Also fix formatting issues reported by goimports that lint now warns about

--- hack/install-go-tools.sh
+++ hack/install-go-tools.sh
@@ -13,6 +13,7 @@ goInstall() {
 goInstall "dep"               "github.com/golang/dep/cmd/dep"
 goInstall "vcsim"             "github.com/vmware/govmomi/vcsim"
 goInstall "ginkgo"            "github.com/onsi/ginkgo/ginkgo"
+goInstall "goimports"         "golang.org/x/tools/cmd/goimports"
 goInstall "golangci-lint"     "github.com/golangci/golangci-lint/cmd/golangci-lint"
 goInstall "go-junit-report"   "github.com/jstemmer/go-junit-report"
 goInstall "gocover-cobertura" "github.com/t-yuki/gocover-cobertura"

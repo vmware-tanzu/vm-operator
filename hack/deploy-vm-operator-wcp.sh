14aa7ffc7fa0fda95a450357e071766b3924db05
Brian Forney <bforney@vmware.com>
2019-06-09 10:08:55 -0700

    Add support for deploying VM Operator into a WCP testbed
    This commit allows developers to build locally and then deploy to a WCP testbed.
    
    Included in this commit is the following:
    
    - Adds a hack/deploy-vm-operator-wcp.sh script that will invoke load-k8s-master,
      which is found in the bora repo, to load in the bin/manager executable
      into a new docker image. To do this, the existing VM Operator image in the
      WCP testbed is used as a starting point. The developer is expected to
      set the following shell environment variables:
    
         WCP_LOAD_K8S_MASTER - The path to the load-k8s-master script from a bora repo.
         Either:
            VCSA_IP - A reachable IP address of the VCSA.
            VCSA_PASSWORD - The root password of the VCSA. (Optional)
           or
            WCP_SA_IP - A reachable IP address of the WCP Supervisor Cluster API Server.
            WCP_SA_PASSWORD - The root password of the WCP Supervisor Cluster API Server.
    
       When VCSA_IP and VCSA_PASSWORD are set, an attempt will be made to contact the
       specified VCSA to determine the WCP_SA_IP and WCP_SA_PASSWORD. If VCSA_IP and
       VCSA_PASSWORD are not set, then WCP_SA_IP and WCP_SA_PASSOWRD will need to be determined
       manually. These values can be retried by running the
       /usr/lib/vmware-wcp/decryptK8Pwd.py script on the vCenter stand-alone appliance
       (VCSA) associated with the WCP Supervisor Cluster API Server.
    
    - Updates the hack/dev-setup.sh to install any needed homebrew packages
      required by load-k8s-master when run on macOS.
    
    - Adds cross compilation support to build Linux ABI executables required to
      run on WCP.
    
    - Updates README.md to include information about WCP testbed support.
    
    Testing done: Ran `make build-wcp && make deploy-wcp` and then verified
                  that `docker run <image name> <executable>` for apiserver
                  and manager. Also verified the docker image tag changed.

---
+++ hack/deploy-vm-operator-wcp.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+APISERVER_YAML=config/apiserver.yaml
+
+usage () {
+    echo "Usage: $(basename $0) [deploy|undeploy|redeploy]"
+    exit 1
+}
+
+verifyEnvironmentVariables() {
+    if [[ -z ${WCP_LOAD_K8S_MASTER:-} ]]; then
+        echo "Error: The WCP_LOAD_K8S_MASTER environment variable must be set " \
+             "to point to a copy of load-k8s-master"
+        exit 1
+    fi
+
+    if [[ -n ${VCSA_IP:-} ]]; then
+        if [[ -z ${VCSA_PASSWORD:-} ]]; then
+            # Often the VCSA_PASSWORD is set to a default. The below sets a
+            # common default so the user of this script does not need to set it.
+            VCSA_PASSWORD="vmware"
+        fi
+        output=$(SSHPASS="$VCSA_PASSWORD" sshpass -e ssh -o \
+                 UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+                 root@$VCSA_IP "/usr/lib/vmware-wcp/decryptK8Pwd.py" 2>&1)
+        WCP_SA_IP=$(echo $output | grep -oEI "IP: (\S)+" | cut -d" " -f2)
+        WCP_SA_PASSWORD=$(echo $output | grep -oEI "PWD: (\S)+" | cut -d" " -f2)
+    fi
+
+    if [[ -z ${WCP_SA_IP:-} ]]; then
+        echo "Error: The WCP_SA_IP environment variable must be set to the " \
+             "WCP Master's IP address"
+        exit 1
+    fi
+
+    if [[ -z ${WCP_SA_PASSWORD:-} ]]; then
+        echo "Error: The WCP_SA_PASSWORD environment variable must be set to "
+             "the WCP Supervisor Cluster API Server's root password"
+        exit 1
+    fi
+
+    if [[ ! -x $WCP_LOAD_K8S_MASTER ]]; then
+        echo "Error: Could not find the load-k8s-master script. Please " \
+             "verify the environment variable WCP_LOAD_K8S_MASTER is set " \
+             "properly. The load-k8s-master script is found at " \
+             "bora/vpx/wcp/support/load-k8s-master in a bora repo."
+        exit 1
+    fi
+
+    if ! ping -c 3 -i 0.25 $WCP_SA_IP > /dev/null 2>&1 ; then
+        echo "Error: Could not access WCP Supervisor Cluster API Server at " \
+             "$WCP_SA_IP"
+        exit 1
+    fi
+}
+
+deploy() {
+   PATH="/usr/local/opt/gnu-getopt/bin:/usr/local/bin:$PATH" \
+     $WCP_LOAD_K8S_MASTER \
+     --component vmop \
+     --binary bin/wcp/apiserver,bin/wcp/manager \
+     --k8s-master-ip $WCP_SA_IP \
+     --k8s-master-password $WCP_SA_PASSWORD
+}
+
+undeploy() {
+   echo "Not implemented"
+}
+
+redeploy() {
+   echo "Not implemented"
+}
+
+while getopts ":" opt ; do
+    case $opt in
+        \? ) usage ;;
+    esac
+done
+
+shift $((OPTIND - 1))
+
+if [[ $# -ne 1 ]] ; then
+    usage
+fi
+
+COMMAND=$1
+
+case $COMMAND in
+    "deploy"   ) verifyEnvironmentVariables; deploy ;;
+    "undeploy" ) verifyEnvironmentVariables; undeploy ;;
+    "redeploy" ) verifyEnvironmentVariables; redeploy ;;
+    * ) usage
+esac
+
+# vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4 filetype=sh

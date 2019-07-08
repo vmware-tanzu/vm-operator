ba1b870d0dfef5582be0a5ee0186590037303b26
Sreyas Natarajan <sreyasn@vmware.com>
2019-07-08 15:25:49 -0700

    Migrate tests to ginkgo
    This commit migrates existing integration tests in pkg/controller
    to use ginkgo

--- pkg/controller/virtualmachine/virtualmachine_controller_test.go
+++ pkg/controller/virtualmachine/virtualmachine_controller_test.go
@@ -7,124 +7,122 @@
 package virtualmachine
 
 import (
-	"testing"
+	"sync"
 	"time"
 
-	"k8s.io/apimachinery/pkg/api/resource"
-
-	"github.com/vmware-tanzu/vm-operator/test/integration"
-
-	"github.com/onsi/gomega"
-	vmoperatorv1alpha1 "github.com/vmware-tanzu/vm-operator/pkg/apis/vmoperator/v1alpha1"
-	"golang.org/x/net/context"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	"golang.org/x/net/context"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	vmoperatorv1alpha1 "github.com/vmware-tanzu/vm-operator/pkg/apis/vmoperator/v1alpha1"
+	"github.com/vmware-tanzu/vm-operator/test/integration"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var c client.Client
 
 const timeout = time.Second * 5
 
-func TestReconcile(t *testing.T) {
+var _ = Describe("VirtualMachine controller", func() {
 	ns := integration.DefaultNamespace
 	name := "fooVm"
 
-	classInstance := &vmoperatorv1alpha1.VirtualMachineClass{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: ns,
-			Name:      name,
-		},
-		Spec: vmoperatorv1alpha1.VirtualMachineClassSpec{
-			Hardware: vmoperatorv1alpha1.VirtualMachineClassHardware{
-				Cpus:   4,
-				Memory: resource.MustParse("1Mi"),
+	var (
+		classInstance   vmoperatorv1alpha1.VirtualMachineClass
+		instance        vmoperatorv1alpha1.VirtualMachine
+		expectedRequest reconcile.Request
+		recFn           reconcile.Reconciler
+		requests        chan reconcile.Request
+		stopMgr         chan struct{}
+		mgrStopped      *sync.WaitGroup
+		mgr             manager.Manager
+		err             error
+	)
+
+	BeforeEach(func() {
+		classInstance = vmoperatorv1alpha1.VirtualMachineClass{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: ns,
+				Name:      name,
 			},
-			Policies: vmoperatorv1alpha1.VirtualMachineClassPolicies{
-				Resources: vmoperatorv1alpha1.VirtualMachineClassResources{
-					Requests: vmoperatorv1alpha1.VirtualMachineClassResourceSpec{
-						Cpu:    resource.MustParse("1000Mi"),
-						Memory: resource.MustParse("100Mi"),
-					},
-					Limits: vmoperatorv1alpha1.VirtualMachineClassResourceSpec{
-						Cpu:    resource.MustParse("2000Mi"),
-						Memory: resource.MustParse("200Mi"),
-					},
+			Spec: vmoperatorv1alpha1.VirtualMachineClassSpec{
+				Hardware: vmoperatorv1alpha1.VirtualMachineClassHardware{
+					Cpus:   4,
+					Memory: resource.MustParse("1Mi"),
 				},
-				StorageClass: "fooStorageClass",
+				Policies: vmoperatorv1alpha1.VirtualMachineClassPolicies{
+					Resources: vmoperatorv1alpha1.VirtualMachineClassResources{
+						Requests: vmoperatorv1alpha1.VirtualMachineClassResourceSpec{
+							Cpu:    resource.MustParse("1000Mi"),
+							Memory: resource.MustParse("100Mi"),
+						},
+						Limits: vmoperatorv1alpha1.VirtualMachineClassResourceSpec{
+							Cpu:    resource.MustParse("2000Mi"),
+							Memory: resource.MustParse("200Mi"),
+						},
+					},
+					StorageClass: "fooStorageClass",
+				},
 			},
-		},
-	}
+		}
 
-	expectedRequest := reconcile.Request{NamespacedName: types.NamespacedName{Namespace: ns, Name: name}}
+		expectedRequest = reconcile.Request{NamespacedName: types.NamespacedName{Namespace: ns, Name: name}}
 
-	g := gomega.NewGomegaWithT(t)
+		// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
+		// channel when it is finished.
+		mgr, err = manager.New(cfg, manager.Options{})
+		Expect(err).NotTo(HaveOccurred())
+		c = mgr.GetClient()
 
-	// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
-	// channel when it is finished.
-	mgr, err := manager.New(cfg, manager.Options{})
-	g.Expect(err).NotTo(gomega.HaveOccurred())
-	c = mgr.GetClient()
+		recFn, requests = SetupTestReconcile(newReconciler(mgr))
+		Expect(add(mgr, recFn)).To(Succeed())
 
-	recFn, requests := SetupTestReconcile(newReconciler(mgr))
-	g.Expect(add(mgr, recFn)).To(gomega.Succeed())
+		stopMgr, mgrStopped = StartTestManager(mgr)
 
-	stopMgr, mgrStopped := StartTestManager(mgr, g)
+		err = c.Create(context.TODO(), &classInstance)
+		Expect(err).ShouldNot(HaveOccurred())
+	})
 
-	defer func() {
+	AfterEach(func() {
 		close(stopMgr)
 		mgrStopped.Wait()
-	}()
+	})
 
-	imageList := &vmoperatorv1alpha1.VirtualMachineImageList{}
-	err = c.List(context.TODO(), &client.ListOptions{Namespace: ns}, imageList)
-	if err != nil {
-		t.Logf("failed to list vm images: %v", err)
-		t.Fail()
-		return
-	}
-	/* TODO(bryan) This List call does not seem to not pass along the namespace
-	g.Expect(len(imageList.Items)).ShouldNot(gomega.BeZero())
-	imageName := imageList.Items[0].Name
-	*/
-	imageName := "DC0_H0_VM0"
-
-	instance := &vmoperatorv1alpha1.VirtualMachine{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: ns,
-			Name:      name,
-		},
-		Spec: vmoperatorv1alpha1.VirtualMachineSpec{
-			ImageName:  imageName,
-			ClassName:  classInstance.Name,
-			PowerState: "poweredOn",
-			Ports:      []vmoperatorv1alpha1.VirtualMachinePort{},
-		},
-	}
-
-	err = c.Create(context.TODO(), classInstance)
-	if err != nil {
-		t.Logf("failed to create VM class object: %v", err)
-		t.Fail()
-		return
-	}
-
-	// Create the Volume object and expect the Reconcile
-	err = c.Create(context.TODO(), instance)
-	// The instance object may not be a valid object because it might be missing some required fields.
-	// Please modify the instance object by adding required fields and then remove the following if statement.
-	if apierrors.IsInvalid(err) {
-		t.Logf("failed to create object, got an invalid object error: %v", err)
-		t.Fail()
-		return
-	}
-
-	g.Expect(err).NotTo(gomega.HaveOccurred())
-	defer func() {
-		_ = c.Delete(context.TODO(), instance)
-	}()
-	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
-}
+	Describe("when creating/deleting a VM object", func() {
+		It("invoke the reconcile method", func() {
+			imageList := &vmoperatorv1alpha1.VirtualMachineImageList{}
+			err = c.List(context.TODO(), &client.ListOptions{Namespace: ns}, imageList)
+			/* TODO() This List call does not seem to not pass along the namespace
+			Expect(err).ShouldNot(HaveOccurred())
+			imageName := imageList.Items[0].Name
+			*/
+			imageName := "DC0_H0_VM0"
+			instance = vmoperatorv1alpha1.VirtualMachine{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: ns,
+					Name:      name,
+				},
+				Spec: vmoperatorv1alpha1.VirtualMachineSpec{
+					ImageName:  imageName,
+					ClassName:  classInstance.Name,
+					PowerState: "poweredOn",
+					Ports:      []vmoperatorv1alpha1.VirtualMachinePort{},
+				},
+			}
+			// Create the VM Object the expect Reconcile
+			err = c.Create(context.TODO(), &instance)
+			Expect(err).ShouldNot(HaveOccurred())
+			Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
+			// Delete the VM Object the expect Reconcile
+			err = c.Delete(context.TODO(), &instance)
+			Expect(err).ShouldNot(HaveOccurred())
+			Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
+		})
+	})
+})

3f84ab8caf2fbfea0924ebddc48232395c0cb9d6
Sabari Murugesan <smurugesan@vmware.com>
2019-08-21 00:17:44 -0700

    Run unit tests in vmprovider/session package

--- pkg/vmprovider/providers/vsphere/vsphere_suite_test.go
+++ pkg/vmprovider/providers/vsphere/vsphere_suite_test.go
@@ -1,4 +1,4 @@
-// +build integration
+// +build !integration
 
 /* **********************************************************
  * Copyright 2019-2019 VMware, Inc.  All rights reserved. -- VMware Confidential
@@ -6,37 +6,13 @@
 package vsphere_test
 
 import (
-	"context"
-	stdlog "log"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	"github.com/vmware-tanzu/vm-operator/pkg/vmprovider/providers/vsphere"
-	"github.com/vmware-tanzu/vm-operator/test/integration"
 )
 
-var (
-	vcSim   *integration.VcSimInstance
-	session *vsphere.Session
-)
-
-func TestVMProvider(t *testing.T) {
+func TestVSphereProvider(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "vSphere Provider Suite")
 }
-
-var _ = BeforeSuite(func() {
-	stdlog.Print("setting up integration test env..")
-	vcSim = integration.NewVcSimInstance()
-	address, port := vcSim.Start()
-	config := integration.NewIntegrationVmOperatorConfig(address, port)
-	var err error
-	session, err = vsphere.NewSession(context.TODO(), config)
-	Expect(err).NotTo(HaveOccurred())
-})
-
-var _ = AfterSuite(func() {
-	integration.CleanupEnv(vcSim)
-})

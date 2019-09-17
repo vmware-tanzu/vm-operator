a63058708f0a683e43bf135ce9cfee9d7654df49
Derek Beard <dbeard@vmware.com>
2019-09-16 22:26:44 -0500

    Reaach 100% unit test coverage for virtualmachine_strategy.go
    Reach 100% unit test coverage for virtualmachine_strategy.go

--- pkg/apis/vmoperator/virtualmachine_strategy_test.go
+++ pkg/apis/vmoperator/virtualmachine_strategy_test.go
@@ -5,17 +5,32 @@
 package vmoperator
 
 import (
+	"context"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
 var _ = Describe("VirtualMachine Validation", func() {
 
-	type ValidateFunc func(*VirtualMachine) field.ErrorList
+	Describe("PrepareForCreate", func() {
 
-	Context("validate network type", func() {
+		It("Sets finalizer", func() {
+			v := VirtualMachineStrategy{}
+			ctx := context.Background()
+			vm := &VirtualMachine{Spec: VirtualMachineSpec{ImageName: "fooImage", ClassName: "fooClass"}}
+			v.PrepareForCreate(ctx, vm)
+			Expect(vm.Finalizers).Should(HaveLen(1))
+			Expect(vm.Finalizers[0]).Should(Equal("virtualmachine.vmoperator.vmware.com"))
+		})
+	})
+
+	Describe("validate network type", func() {
+		type ValidateFunc func(*VirtualMachine) field.ErrorList
+
 		DescribeTable("should reject network interface without name",
 			func(validateFunc ValidateFunc, vnifs []VirtualMachineNetworkInterface) {
 				vm := &VirtualMachine{
@@ -54,6 +69,35 @@ var _ = Describe("VirtualMachine Validation", func() {
 			Entry("nsx-t network type", validateNetworkType, []VirtualMachineNetworkInterface{{NetworkName: "dummy-network-name", NetworkType: "nsx-t"}}),
 			Entry("empty network type", validateNetworkType, []VirtualMachineNetworkInterface{{NetworkName: "dummy-network-name", NetworkType: ""}}),
 			Entry("no network interface", validateNetworkType, nil),
+		)
+	})
+
+	Describe("Validate", func() {
+		type ValidateObjFunc func(ctx context.Context, obj runtime.Object) field.ErrorList
+
+		DescribeTable("should accept valid vm",
+			func(vm *VirtualMachine) {
+				v := VirtualMachineStrategy{}
+				ctx := context.Background()
+				Expect(v.Validate(ctx, vm)).Should(BeEmpty())
+			},
+			Entry("valid vm", &VirtualMachine{Spec: VirtualMachineSpec{ImageName: "fooImage", ClassName: "fooClass"}}),
+		)
+
+		DescribeTable("should reject invalid vm",
+			func(vm *VirtualMachine, expectedErr *field.Error) {
+				v := VirtualMachineStrategy{}
+				ctx := context.Background()
+
+				result := v.Validate(ctx, vm)
+
+				Expect(result).Should(HaveLen(1))
+				Expect(result[0]).Should(Equal(expectedErr))
+			},
+			Entry("invalid image", &VirtualMachine{Spec: VirtualMachineSpec{ClassName: "foo"}},
+				field.Required(field.NewPath("spec", "imageName"), "imageName must be provided")),
+			Entry("invalid class", &VirtualMachine{Spec: VirtualMachineSpec{ImageName: "foo"}},
+				field.Required(field.NewPath("spec", "className"), "className must be provided")),
 		)
 	})
 })

040d575644d776704ac217a15da184f958fc6815
Hanlin Shi <hanlins@vmware.com>
2019-09-19 15:14:37 -0700

    Fix vm-operator cluster role
    VM operator need to access NCP CRDs, this change adds the corresponding CRD
    access to VM operator.

--- pkg/controller/virtualmachine/virtualmachine_controller.go
+++ pkg/controller/virtualmachine/virtualmachine_controller.go
@@ -90,6 +90,7 @@ type ReconcileVirtualMachine struct {
 // +kubebuilder:rbac:groups=vmoperator.vmware.com,resources=virtualmachines/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=vmoperator.vmware.com,resources=virtualmachineclasses,verbs=get;list
 // +kubebuilder:rbac:groups=storage.k8s.io,resources=storageclasses,verbs=get;list;watch
+// +kubebuilder:rbac:groups=vmware.com,resources=virtualnetworkinterfaces;virtualnetworkinterfaces/status,verbs=create;get;list;patch;delete;watch;update
 func (r *ReconcileVirtualMachine) Reconcile(request reconcile.Request) (reconcile.Result, error) {
 	ctx := context.Background()
 

00fed5472af39f4cfb160a044614de524ea00a6c
Janitha Karunaratne <jkarunaratne@vmware.com>
2019-10-09 12:36:54 -0500

    Properly tag the binaries and containers
    This change introduces version tagging to vm-operator
    - generate dynamic version based on git tags (similar to VIC) using `git describe ...` (see Makefile)
    - Remove use of `apiserver-boot build executables` and instead rely on our own Makefile build targets
    - apiserver and manager to log the build version/number/type during startup
    - Tag container additional with dynamic version as well as BUILD_NUMBER
    - gobuild: use generated VERSION file to tag the buildweb build with proper version

--- hack/build-container.sh
+++ hack/build-container.sh
@@ -11,9 +11,10 @@ BUILD_TAG=
 COMMIT=
 BRANCH=
 BINDIR=
+VERSION=
 
 usage() {
-    echo "Usage: $(basename $0) -i image -t imageTag [-n buildNumber] [-T buildTag] [-c commit] [-b branch] [-B binDir]"
+    echo "Usage: $(basename $0) -i image -t imageTag [-n buildNumber] [-T buildTag] [-c commit] [-b branch] [-B binDir] [-v version]"
     exit 1
 }
 
@@ -21,16 +22,20 @@ usage() {
 #   - the Dockerfile used is hardcoded into the source and the base image is quite old
 #   - it does not work well building the container in the Jenkins pipeline
 build() {
-    docker build -t $IMAGE:$IMAGE_TAG -f Dockerfile.build \
+    docker build -f Dockerfile.build \
+        -t $IMAGE:$IMAGE_TAG \
+        -t $IMAGE:$BUILD_NUMBER \
+        -t $IMAGE:$VERSION \
         --build-arg buildNumber=${BUILD_NUMBER} \
         --build-arg buildTag=${BUILD_TAG} \
         --build-arg commit=${COMMIT} \
         --build-arg branch=${BRANCH} \
+        --build-arg version=${VERSION} \
         --rm \
         $BINDIR
 }
 
-while getopts ":i:t:n:T:c:b:B:" opt ; do
+while getopts ":i:t:n:T:c:b:B:v:" opt ; do
     case $opt in
         "i" ) IMAGE=$OPTARG ;;
         "t" ) IMAGE_TAG=$OPTARG ;;
@@ -39,6 +44,7 @@ while getopts ":i:t:n:T:c:b:B:" opt ; do
         "c" ) COMMIT=$OPTARG ;;
         "b" ) BRANCH=$OPTARG ;;
         "B" ) BINDIR=$OPTARG ;;
+        "v" ) VERSION=$OPTARG ;;
         \? ) usage ;;
     esac
 done
@@ -57,6 +63,7 @@ BUILD_NUMBER=${BUILD_NUMBER:-0}
 COMMIT=${COMMIT:-$(git rev-parse HEAD)}
 BRANCH=${BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
 BINDIR=${BINDIR:-bin/}
+VERSION=${VERSION:"0.0"}
 
 build
 
---
+++ pkg/buildinfo.go
@@ -0,0 +1,12 @@
+/* **********************************************************
+ * Copyright 2019 VMware, Inc.  All rights reserved. -- VMware Confidential
+ * **********************************************************/
+
+package pkg
+
+var (
+	BuildCommit  string
+	BuildNumber  string
+	BuildVersion string
+	BuildType    string
+)

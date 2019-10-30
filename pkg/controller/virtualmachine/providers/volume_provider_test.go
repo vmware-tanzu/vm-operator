79d7d62223c8b6a91b6086371aad644679b651aa
Daniel Guo <gdaniel@vmware.com>
2019-10-30 15:46:21 -0700

    Fix the VM Operator bug when updating the volume status
    Bug#: 
    Summary of the fix:
    * Updating the VirtualMachineVolumeStatus in batch from UpdateVmVolumesStatus() and AttachVolumes().
    * UpdateVmVolumesStatus() and AttachVolumes() do not call client.Status().Update(), since the reconcileVm() calls it in the end.
    * New unit tests are also added for this case
    
    Testing Done:
    * Unit tests passed
    * Integration tests passed
    * gce2e passed: 
    * Manual tests: passed

--- pkg/controller/virtualmachine/providers/volume_provider_test.go
+++ pkg/controller/virtualmachine/providers/volume_provider_test.go
@@ -105,7 +105,7 @@ var _ = Describe("Volume Provider", func() {
 				err := cnsVolumeProvider.client.Create(context.TODO(), vm)
 				Expect(err).NotTo(HaveOccurred())
 				err = cnsVolumeProvider.UpdateVmVolumesStatus(context.TODO(), vm)
-				Expect(err).To(HaveOccurred())
+				Expect(err).NotTo(HaveOccurred())
 			})
 		})
 
@@ -121,12 +121,30 @@ var _ = Describe("Volume Provider", func() {
 			})
 		})
 
-		Context("when updating VirtualMachineVolumeStatus", func() {
-			It("the orphan VirtualMachineVolumeStatus should be deleted", func() {
+		Context("if volume has been added into spec.volume, but the CnsNodeVmAttachment has not been created", func() {
+			It("UpdateVmVolumesStatus() should not add the volume status", func() {
+				vm = generateDefaultVirtualMachine()
+				vm.Spec.Volumes = append(vm.Spec.Volumes, generateVirtualMachineVolumes("beta"))
+				err := cnsVolumeProvider.client.Create(context.TODO(), vm)
+				Expect(err).NotTo(HaveOccurred())
+
+				generatedCnsNodeVmAttachment := generateAttachedCnsNodeVmAttachment(vm.Name, vm.Spec.Volumes[0].Name, vm.Namespace)
+				err = cnsVolumeProvider.client.Create(context.TODO(), generatedCnsNodeVmAttachment)
+				Expect(err).NotTo(HaveOccurred())
+
+				err = cnsVolumeProvider.UpdateVmVolumesStatus(context.TODO(), vm)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(len(vm.Status.Volumes)).To(Equal(1))
+			})
+		})
+
+		Context("if volume has been deleted from spec.volume, but the CnsNodeVmAttachment has not been delete yet", func() {
+			It("UpdateVmVolumesStatus() should not delete the volume status", func() {
 				vm = generateDefaultVirtualMachine()
 				err := cnsVolumeProvider.client.Create(context.TODO(), vm)
 				Expect(err).NotTo(HaveOccurred())
-				err = cnsVolumeProvider.client.Create(context.TODO(), generateAttachedCnsNodeVmAttachment(vm.Name, vm.Spec.Volumes[0].Name, vm.Namespace))
+				generatedCnsNodeVmAttachment := generateAttachedCnsNodeVmAttachment(vm.Name, vm.Spec.Volumes[0].Name, vm.Namespace)
+				err = cnsVolumeProvider.client.Create(context.TODO(), generatedCnsNodeVmAttachment)
 				Expect(err).NotTo(HaveOccurred())
 
 				// Update the vm spec to remove the volumes, so there will be orphan volumes under vm.status.volumes
@@ -134,24 +152,26 @@ var _ = Describe("Volume Provider", func() {
 				err = cnsVolumeProvider.client.Update(context.TODO(), vm)
 				Expect(err).NotTo(HaveOccurred())
 
+				// Update CnsNodeVmAttachment status to simulate the detach
+				generatedCnsNodeVmAttachment.Status.Error = "Unable to detach the volume!"
+				err = cnsVolumeProvider.client.Status().Update(context.TODO(), generatedCnsNodeVmAttachment)
+				Expect(err).NotTo(HaveOccurred())
+
 				vmRetrieved := &vmoperatorv1alpha1.VirtualMachine{}
 				err = cnsVolumeProvider.client.Get(context.TODO(), client.ObjectKey{Name: vm.Name, Namespace: vm.Namespace}, vmRetrieved)
 				Expect(err).NotTo(HaveOccurred())
 				err = cnsVolumeProvider.UpdateVmVolumesStatus(context.TODO(), vmRetrieved)
 				Expect(err).NotTo(HaveOccurred())
-
-				vmRetrieved = &vmoperatorv1alpha1.VirtualMachine{}
-				err = cnsVolumeProvider.client.Get(context.TODO(), client.ObjectKey{Name: vm.Name, Namespace: vm.Namespace}, vmRetrieved)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(len(vmRetrieved.Status.Volumes)).To(Equal(0))
+				Expect(len(vmRetrieved.Status.Volumes)).To(Equal(1))
+				Expect(vmRetrieved.Status.Volumes[0].Error).To(Equal("Unable to detach the volume!"))
 
 			})
 		})
 	})
 
-	Describe("Unit tests for CreateCnsNodeVmAttachments()", func() {
-		Context("when creating CnsNodeVmAttachment", func() {
-			It("should not return error if there are new volumes added", func() {
+	Describe("Unit tests for AttachVolumes()", func() {
+		Context("when a new volume is added", func() {
+			It("should have no error, a new CnsNodeVmAttachment gets created, and VirtualMachineVolumeStatus is set to default value", func() {
 				vm = generateDefaultVirtualMachine()
 				vm.Status.Volumes = []vmoperatorv1alpha1.VirtualMachineVolumeStatus{}
 				err := cnsVolumeProvider.client.Create(context.TODO(), vm)
@@ -161,15 +181,87 @@ var _ = Describe("Volume Provider", func() {
 				err = cnsVolumeProvider.AttachVolumes(context.TODO(), vm, virtualMachineVolumesAdded)
 				Expect(err).NotTo(HaveOccurred())
 
-				vmRetrieved := &vmoperatorv1alpha1.VirtualMachine{}
-				err = cnsVolumeProvider.client.Get(context.TODO(), client.ObjectKey{Name: vm.Name, Namespace: vm.Namespace}, vmRetrieved)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(len(vmRetrieved.Status.Volumes)).To(Equal(1))
+				Expect(len(vm.Status.Volumes)).To(Equal(1))
 
 				cnsNodeVmAttachmentList := &cnsv1alpha1.CnsNodeVmAttachmentList{}
 				err = cnsVolumeProvider.client.List(context.TODO(), &client.ListOptions{Namespace: vm.Namespace}, cnsNodeVmAttachmentList)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(len(cnsNodeVmAttachmentList.Items)).To(Equal(1))
+
+				for _, vmVolumeStatus := range vm.Status.Volumes {
+					Expect(vmVolumeStatus.Error).To(Equal(""))
+					Expect(vmVolumeStatus.DiskUuid).To(Equal(""))
+					Expect(vmVolumeStatus.Attached).To(BeFalse())
+				}
+			})
+		})
+
+		Context("when multiple volumes are added", func() {
+			It("should create multiple CnsNodeVmAttachment instances and set VirtualMachineVolumeStatus to default values", func() {
+				vm = generateDefaultVirtualMachine()
+				vm.Spec.Volumes = append(vm.Spec.Volumes, generateVirtualMachineVolumes("beta"))
+				// Remove default vm.status.volumes
+				vm.Status.Volumes = []vmoperatorv1alpha1.VirtualMachineVolumeStatus{}
+				err := cnsVolumeProvider.client.Create(context.TODO(), vm)
+				Expect(err).NotTo(HaveOccurred())
+
+				virtualMachineVolumesAdded, _ := GetVmVolumesToProcess(vm)
+				Expect(len(virtualMachineVolumesAdded)).To(Equal(2))
+
+				err = cnsVolumeProvider.AttachVolumes(context.TODO(), vm, virtualMachineVolumesAdded)
+				Expect(err).NotTo(HaveOccurred())
+
+				// There should be multiple CnsNodeVmAttachment instances get created
+				cnsNodeVmAttachmentList := &cnsv1alpha1.CnsNodeVmAttachmentList{}
+				err = cnsVolumeProvider.client.List(context.TODO(), &client.ListOptions{}, cnsNodeVmAttachmentList)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(len(cnsNodeVmAttachmentList.Items)).To(Equal(2))
+				// There should be multiple volume status has been updated under VirtualMachine
+				Expect(len(vm.Status.Volumes)).To(Equal(2))
+
+				for _, vmVolumeStatus := range vm.Status.Volumes {
+					Expect(vmVolumeStatus.Error).To(Equal(""))
+					Expect(vmVolumeStatus.DiskUuid).To(Equal(""))
+					Expect(vmVolumeStatus.Attached).To(BeFalse())
+				}
+			})
+		})
+
+		Context("when there is an existing CnsNodeVmAttachment instance", func() {
+			It("should not return error, no additional CnsNodeVmAttachment instance gets created, and VirtualMachineVolumeStatus is set properly", func() {
+				vm = generateDefaultVirtualMachine()
+				vm.Spec.Volumes = append(vm.Spec.Volumes, generateVirtualMachineVolumes(newVirtualMachineVolumeName))
+				vm.Status.Volumes = []vmoperatorv1alpha1.VirtualMachineVolumeStatus{}
+				err := cnsVolumeProvider.client.Create(context.TODO(), vm)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Only create one CnsNodeVmAttachment instance with "attached": true
+				virtualMachineVolumesAdded, _ := GetVmVolumesToProcess(vm)
+				for virtualMachineVolume := range virtualMachineVolumesAdded {
+					if virtualMachineVolume.Name == newVirtualMachineVolumeName {
+						cnsNodeVmAttachment := generateAttachedCnsNodeVmAttachment(vm.Name, virtualMachineVolume.Name, virtualMachineVolume.Namespace)
+						err = cnsVolumeProvider.client.Create(context.TODO(), cnsNodeVmAttachment)
+						Expect(err).NotTo(HaveOccurred())
+						break
+					}
+				}
+
+				err = cnsVolumeProvider.AttachVolumes(context.TODO(), vm, virtualMachineVolumesAdded)
+				Expect(err).NotTo(HaveOccurred())
+
+				cnsNodeVmAttachmentList := &cnsv1alpha1.CnsNodeVmAttachmentList{}
+				err = cnsVolumeProvider.client.List(context.TODO(), &client.ListOptions{Namespace: vm.Namespace}, cnsNodeVmAttachmentList)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(len(cnsNodeVmAttachmentList.Items)).To(Equal(2))
+
+				// Expect only the matched virtualMachineVolumeStatus has "attached": true
+				for _, virtualMachineVolumeStatus := range vm.Status.Volumes {
+					if virtualMachineVolumeStatus.Name == newVirtualMachineVolumeName {
+						Expect(virtualMachineVolumeStatus.Attached).To(BeTrue())
+					} else {
+						Expect(virtualMachineVolumeStatus.Attached).To(BeFalse())
+					}
+				}
 			})
 		})
 	})

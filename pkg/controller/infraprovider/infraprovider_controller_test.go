6733f32418630e5704d9f74b93e26807d18f2314
Narendra Madanapalli <nmadanapalli@vmware.com>
2019-12-16 05:39:26 -0800

    - Express CPU Resource Policies in terms of CPU Cores and Convert the Cores into CPU Frequency
    Design is here: 
    
    CPU cores or Milli cores as a whole number is the Kubernetes way of specifying the CPU resource policies such as reservations and limits. This semantics make virtualmachineclass hardware agnostic. vCenter VM hardware understands these policies in terms of the CPU frequency.  We need a mechanism to bridge this gap that translates Kubernetes semantics into vCenter agreeable metric units.
    
    This feature involves work in the following areas:
    1. Express CPU reservations and limits in terms of CPU core or milli-cores. The values requests.cpu & limits.cpu are set as full reservations () i.e. they are equal to 1000m times hardware.cpus in all the VM classes.
    2. The computation layer computes the CPU min frequency (cpuMinFreq) upon provider initialization and the value is recomputed/updated upon Node addition or deletion. The update part is carried out with the help of a simple controller.
    3. The translation layer simply multiplies limits.cpu & requests.cpu with cpuMinFreq and assign them to VM config spec at the time of VM creation.
    
    Testing Done:
    1. Created MC and observed CP+W deployed with defined requests & limits
    2. Integration tests are added to test the recompute flow. The flow is invoked when a Node is added/removed
        sudo -E make test-integration
    3. Unit Tests using make test
    4. dev-integrate-wcp dev-integrate-wcp#10729

---
+++ pkg/controller/infraprovider/infraprovider_controller_test.go
@@ -0,0 +1,87 @@
+// +build integration
+
+/* **********************************************************
+ * Copyright 2019 VMware, Inc.  All rights reserved. -- VMware Confidential
+ * **********************************************************/
+
+package infraprovider
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	"golang.org/x/net/context"
+
+	"github.com/google/uuid"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/vmware-tanzu/vm-operator/test/integration"
+)
+
+var c client.Client
+
+const timeout = time.Second * 5
+
+var _ = Describe("InfraProvider controller", func() {
+	ns := integration.DefaultNamespace
+	var (
+		stopMgr                 chan struct{}
+		mgrStopped              *sync.WaitGroup
+		mgr                     manager.Manager
+		err                     error
+		leaderElectionConfigMap string
+	)
+
+	BeforeEach(func() {
+		// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
+		// channel when it is finished.
+		syncPeriod := 10 * time.Second
+		leaderElectionConfigMap = fmt.Sprintf("vmoperator-controller-manager-runtime-%s", uuid.New())
+		mgr, err = manager.New(cfg, manager.Options{SyncPeriod: &syncPeriod,
+			LeaderElection:          true,
+			LeaderElectionID:        leaderElectionConfigMap,
+			LeaderElectionNamespace: ns})
+		Expect(err).NotTo(HaveOccurred())
+		c = mgr.GetClient()
+
+		stopMgr, mgrStopped = integration.StartTestManager(mgr)
+	})
+
+	AfterEach(func() {
+		close(stopMgr)
+		mgrStopped.Wait()
+	})
+
+	Describe("when creating/deleting a Node", func() {
+
+		It("invoke the reconcile method to compute cpu min frequency", func() {
+			name := "node-02"
+			instance := v1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: name,
+				},
+			}
+
+			expectedRequest := reconcile.Request{NamespacedName: types.NamespacedName{Name: name}}
+			recFn, requests := integration.SetupTestReconcile(newReconciler(mgr))
+			Expect(add(mgr, recFn)).To(Succeed())
+			// Create the Node object and expect the Reconcile to compute cpuMinFreq
+			err = c.Create(context.TODO(), &instance)
+			Expect(err).ShouldNot(HaveOccurred())
+			Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
+			// Delete the Node object and expect the Reconcile to compute cpuMinFreq
+			err = c.Delete(context.TODO(), &instance)
+			Expect(err).ShouldNot(HaveOccurred())
+			Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
+		})
+	})
+})

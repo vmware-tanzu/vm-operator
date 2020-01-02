cce51f40e00efbe088e5edb48ac8aa6ca4832d31
Arunesh Pandey <parunesh@vmware.com>
2020-01-02 13:09:31 -0800

    Remove per namespace configMap logic
    Currently, we have logic to consume two configMaps.
    1. ConfigMap from the VM operator namespace.
    2. ConfigMap from the namespace the VM is created in.
    
    Since we are going to only use ConfigMap from the VM
    operator namespace for 1.0 release. This change
    removes the logic for per namespace ConfigMap parsing.
    This helps simplify the code and reduce the code paths
    that are unreachable in production environments.

--- pkg/vmprovider/providers/vsphere/config_test.go
+++ pkg/vmprovider/providers/vsphere/config_test.go
@@ -9,16 +9,15 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/vmware-tanzu/vm-operator/pkg/lib"
-
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 	"github.com/vmware/govmomi/simulator"
-
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 
+	"github.com/vmware-tanzu/vm-operator/pkg/lib"
 	. "github.com/vmware-tanzu/vm-operator/pkg/vmprovider/providers/vsphere"
 )
 
@@ -55,7 +54,7 @@ var _ = Describe("UpdateVMFolderAndResourcePool", func() {
 			annotations := make(map[string]string)
 			annotations[NamespaceRPAnnotationKey] = namespaceRP
 			annotations[NamespaceFolderAnnotationKey] = namespaceVMFolder
-			ns, err = clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "ns-namespace", Annotations: annotations}})
+			ns, err = clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "namespace", Annotations: annotations}})
 			Expect(err).ShouldNot(HaveOccurred())
 			providerConfig := &VSphereVmProviderConfig{}
 			Expect(UpdateVMFolderAndRPInProviderConfig(clientSet, ns.Name, providerConfig)).To(Succeed())
@@ -66,7 +65,7 @@ var _ = Describe("UpdateVMFolderAndResourcePool", func() {
 	Context("when a good provider config exists and namespace does not have annotations", func() {
 		Specify("should succeed with providerconfig unmodified", func() {
 			clientSet := fake.NewSimpleClientset()
-			ns, err = clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "ns-namespace"}})
+			ns, err = clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "namespace"}})
 			Expect(err).ShouldNot(HaveOccurred())
 			providerConfig := &VSphereVmProviderConfig{}
 			providerConfigRP := "namespace-test-RP"
@@ -90,10 +89,10 @@ var _ = Describe("UpdateVMFolderAndResourcePool", func() {
 	Context("ResourcePool and Folder not present in either providerConfig or namespace annotations", func() {
 		It("should return an error", func() {
 			clientSet := fake.NewSimpleClientset()
-			ns, err = clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "ns-namespace"}})
+			ns, err = clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "namespace"}})
 			Expect(err).ShouldNot(HaveOccurred())
 			providerConfig := &VSphereVmProviderConfig{}
-			err = UpdateVMFolderAndRPInProviderConfig(clientSet, "ns-namespace", providerConfig)
+			err = UpdateVMFolderAndRPInProviderConfig(clientSet, "namespace", providerConfig)
 			Expect(err).Should(HaveOccurred())
 			Expect(err.Error()).To(Equal("Invalid resourcepool/folder in providerConfig. ResourcePool: , Folder: "))
 		})
@@ -103,244 +102,115 @@ var _ = Describe("UpdateVMFolderAndResourcePool", func() {
 var _ = Describe("GetProviderConfigFromConfigMap", func() {
 
 	var (
-		baseConfigMapIn      *v1.ConfigMap
-		baseSecretIn         *v1.Secret
-		baseProviderConfigIn *VSphereVmProviderConfig
-		nsConfigMapIn        *v1.ConfigMap
-		nsSecretIn           *v1.Secret
-		nsProviderConfigIn   *VSphereVmProviderConfig
+		configMapIn      *v1.ConfigMap
+		secretIn         *v1.Secret
+		providerConfigIn *VSphereVmProviderConfig
 	)
 
 	BeforeEach(func() {
 		os.Unsetenv(lib.VmopNamespaceEnv)
-		baseConfigMapIn, baseSecretIn, baseProviderConfigIn = newConfig("base-namespace", "base-pnid", "base-port", "base-secret-name")
-		nsConfigMapIn, nsSecretIn, nsProviderConfigIn = newConfig("ns-namespace", "ns-pnid", "ns-port", "ns-secret-name")
+		configMapIn, secretIn, providerConfigIn = newConfig("namespace", "pnid", "port", "secret-name")
 	})
 
 	Context("when a base config exists", func() {
 
 		BeforeEach(func() {
-			os.Setenv(lib.VmopNamespaceEnv, "base-namespace")
+			os.Setenv(lib.VmopNamespaceEnv, "namespace")
 		})
 
-		Context("when a base secret doesn't exist", func() {
+		Context("when a secret doesn't exist", func() {
 			Specify("returns no provider config and an error", func() {
-				clientSet := fake.NewSimpleClientset(baseConfigMapIn)
-				_, err := clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "ns-namespace"}})
+				clientSet := fake.NewSimpleClientset(configMapIn)
+				_, err := clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "namespace"}})
 				Expect(err).To(BeNil())
-				providerConfig, err := GetProviderConfigFromConfigMap(clientSet, "ns-namespace")
+				providerConfig, err := GetProviderConfigFromConfigMap(clientSet, "namespace")
 				Expect(err).NotTo(BeNil())
 				Expect(providerConfig).To(BeNil())
 			})
 		})
 
-		Context("when a base secret exists", func() {
+		Context("when a secret exists", func() {
 			Specify("returns a good provider config", func() {
-				clientSet := fake.NewSimpleClientset(baseConfigMapIn, baseSecretIn)
-				_, err := clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "ns-namespace"}})
+				clientSet := fake.NewSimpleClientset(configMapIn, secretIn)
+				_, err := clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "namespace"}})
 				Expect(err).To(BeNil())
-				providerConfig, err := GetProviderConfigFromConfigMap(clientSet, "ns-namespace")
+				providerConfig, err := GetProviderConfigFromConfigMap(clientSet, "namespace")
 				Expect(err).To(BeNil())
-				Expect(providerConfig).To(Equal(baseProviderConfigIn))
+				Expect(providerConfig).To(Equal(providerConfigIn))
 			})
 			Specify("returns a good provider config for no namespace", func() {
-				clientSet := fake.NewSimpleClientset(baseConfigMapIn, baseSecretIn)
+				clientSet := fake.NewSimpleClientset(configMapIn, secretIn)
 				providerConfig, err := GetProviderConfigFromConfigMap(clientSet, "")
 				Expect(err).To(BeNil())
-				Expect(providerConfig).To(Equal(baseProviderConfigIn))
+				Expect(providerConfig).To(Equal(providerConfigIn))
 			})
 		})
-
-		Context("also full ns config exists", func() {
-			Specify("returns a good provider config for ns", func() {
-				clientSet := fake.NewSimpleClientset(baseConfigMapIn, baseSecretIn, nsConfigMapIn, nsSecretIn)
-				_, err := clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "ns-namespace"}})
-				Expect(err).To(BeNil())
-				providerConfig, err := GetProviderConfigFromConfigMap(clientSet, "ns-namespace")
-				Expect(err).To(BeNil())
-				Expect(providerConfig).To(Equal(nsProviderConfigIn))
-			})
-
-		})
-
-		Context("also sparse ns config exists", func() {
-			Context("where ns config is missing VcPNID", func() {
-				Specify("returns a good provider config with VcPNID from base", func() {
-					delete(nsConfigMapIn.Data, "VcPNID")
-					clientSet := fake.NewSimpleClientset(baseConfigMapIn, baseSecretIn, nsConfigMapIn, nsSecretIn)
-					_, err := clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "ns-namespace"}})
-					Expect(err).To(BeNil())
-					providerConfig, err := GetProviderConfigFromConfigMap(clientSet, "ns-namespace")
-					Expect(err).To(BeNil())
-					Expect(providerConfig.VcPNID).To(Equal(baseProviderConfigIn.VcPNID))
-				})
-			})
-		})
-
-		Context("per env variable, but only the ns config exists", func() {
-			Specify("returns a good provider config for ns", func() {
-				clientSet := fake.NewSimpleClientset(nsConfigMapIn, nsSecretIn)
-				_, err := clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "ns-namespace"}})
-				Expect(err).To(BeNil())
-				providerConfig, err := GetProviderConfigFromConfigMap(clientSet, "ns-namespace")
-				Expect(err).To(BeNil())
-				Expect(providerConfig).To(Equal(nsProviderConfigIn))
-			})
-		})
-
 	})
 
-	Context("when a ns config exists", func() {
-
-		Context("when a ns secret doesn't exist", func() {
-			Specify("returns no provider config and an error", func() {
-				clientSet := fake.NewSimpleClientset(nsConfigMapIn)
-				_, err := clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "ns-namespace"}})
-				Expect(err).To(BeNil())
-				providerConfig, err := GetProviderConfigFromConfigMap(clientSet, "ns-namespace")
-				Expect(err).NotTo(BeNil())
-				Expect(providerConfig).To(BeNil())
-			})
-		})
-
-		Context("when a ns secret exists", func() {
-			Specify("returns a good provider config", func() {
-				clientSet := fake.NewSimpleClientset(nsConfigMapIn, nsSecretIn)
-				_, err := clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "ns-namespace"}})
-				Expect(err).To(BeNil())
-				providerConfig, err := GetProviderConfigFromConfigMap(clientSet, "ns-namespace")
-				Expect(err).To(BeNil())
-				Expect(providerConfig).To(Equal(nsProviderConfigIn))
-			})
-		})
-
-	})
-
-	Context("when neither ns nor base config exist", func() {
+	Context("when base config does not exist", func() {
 		Specify("returns no provider config and an error", func() {
 			clientSet := fake.NewSimpleClientset()
-			_, err := clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "ns-namespace"}})
+			_, err := clientSet.CoreV1().Namespaces().Create(&v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "namespace"}})
 			Expect(err).To(BeNil())
-			providerConfig, err := GetProviderConfigFromConfigMap(clientSet, "ns-namespace")
+			providerConfig, err := GetProviderConfigFromConfigMap(clientSet, "namespace")
 			Expect(err).NotTo(BeNil())
 			Expect(providerConfig).To(BeNil())
 		})
 	})
-
 })
 
 var _ = Describe("configMapsToProviderConfig", func() {
 
 	var (
-		baseConfigMapIn *v1.ConfigMap
-		nsConfigMapIn   *v1.ConfigMap
-		vcCreds         *VSphereVmProviderCredentials
+		configMapIn *v1.ConfigMap
+		vcCreds     *VSphereVmProviderCredentials
 	)
 
 	BeforeEach(func() {
 		os.Unsetenv(lib.VmopNamespaceEnv)
-		baseConfigMapIn, _, _ = newConfig("base-namespace", "base-pnid", "base-port", "base-secret-name")
-		nsConfigMapIn, _, _ = newConfig("ns-namespace", "ns-pnid", "ns-port", "ns-secret-name")
+		configMapIn, _, _ = newConfig("namespace", "pnid", "port", "secret-name")
 		vcCreds = &VSphereVmProviderCredentials{"some-user", "some-pass"}
 	})
 
-	Context("when same key is in both config", func() {
-		Specify("use the key from the ns config", func() {
-			providerConfig, err := ConfigMapsToProviderConfig(baseConfigMapIn, nsConfigMapIn, vcCreds)
-			Expect(err).To(BeNil())
-			Expect(providerConfig.VcPNID).To(Equal(nsConfigMapIn.Data["VcPNID"]))
-		})
+	It("verifies that a config is correctly extracted from the configMap", func() {
+		providerConfig, err := ConfigMapToProviderConfig(configMapIn, vcCreds)
+		Expect(err).To(BeNil())
+		Expect(providerConfig.VcPNID).To(Equal(configMapIn.Data["VcPNID"]))
 	})
 
-	Context("when same key is only in ns config", func() {
-		Specify("use the key from the ns config", func() {
-			delete(baseConfigMapIn.Data, "VcPNID")
-			providerConfig, err := ConfigMapsToProviderConfig(baseConfigMapIn, nsConfigMapIn, vcCreds)
-			Expect(err).To(BeNil())
-			Expect(providerConfig.VcPNID).To(Equal(nsConfigMapIn.Data["VcPNID"]))
-		})
-	})
-
-	Context("when same key is only in base config", func() {
-		Specify("use the key from the base config", func() {
-			delete(nsConfigMapIn.Data, "VcPNID")
-			providerConfig, err := ConfigMapsToProviderConfig(baseConfigMapIn, nsConfigMapIn, vcCreds)
-			Expect(err).To(BeNil())
-			Expect(providerConfig.VcPNID).To(Equal(baseConfigMapIn.Data["VcPNID"]))
-		})
-	})
-
-	Context("when vcPNID is unset on both configs", func() {
+	Context("when vcPNID is unset in configMap", func() {
 		Specify("return an error", func() {
-			delete(nsConfigMapIn.Data, "VcPNID")
-			delete(baseConfigMapIn.Data, "VcPNID")
-			providerConfig, err := ConfigMapsToProviderConfig(baseConfigMapIn, nsConfigMapIn, vcCreds)
+			delete(configMapIn.Data, "VcPNID")
+			providerConfig, err := ConfigMapToProviderConfig(configMapIn, vcCreds)
 			Expect(err).NotTo(BeNil())
 			Expect(providerConfig).To(BeNil())
-		})
-	})
-
-	Context("when ResourcePool is unset on both configs", func() {
-		Specify("return a provider config with the ResourcePool field empty", func() {
-			delete(nsConfigMapIn.Data, "ResourcePool")
-			delete(baseConfigMapIn.Data, "ResourcePool")
-			providerConfig, err := ConfigMapsToProviderConfig(baseConfigMapIn, nsConfigMapIn, vcCreds)
-			Expect(err).To(BeNil())
-			Expect(providerConfig.ResourcePool).To(Equal(""))
 		})
 	})
 
 	Context("when vcCreds is unset", func() {
 		Specify("return an error", func() {
-			providerConfig, err := ConfigMapsToProviderConfig(baseConfigMapIn, nsConfigMapIn, nil)
+			providerConfig, err := ConfigMapToProviderConfig(configMapIn, nil)
 			Expect(err).NotTo(BeNil())
 			Expect(providerConfig).To(BeNil())
 		})
 	})
 
-	Context("when StorageClassRequired is unset on both configs", func() {
-		Specify("return a provider config with the StorageClassRequired equals to false", func() {
-			providerConfig, err := ConfigMapsToProviderConfig(baseConfigMapIn, nsConfigMapIn, vcCreds)
+	Context("StorageClassRequired", func() {
+		Specify("StorageClassRequired is unset in configMap", func() {
+			providerConfig, err := ConfigMapToProviderConfig(configMapIn, vcCreds)
 			Expect(err).To(BeNil())
 			Expect(providerConfig.StorageClassRequired).To(Equal(false))
 		})
-	})
 
-	Context("when StorageClassRequired is set on base config", func() {
-		Specify("return a provider config with the StorageClassRequired equals to true", func() {
-			baseConfigMapIn.Data["StorageClassRequired"] = strconv.FormatBool(true)
-			delete(nsConfigMapIn.Data, "StorageClassRequired")
-			providerConfig, err := ConfigMapsToProviderConfig(baseConfigMapIn, nsConfigMapIn, vcCreds)
-			Expect(err).To(BeNil())
-			Expect(providerConfig.StorageClassRequired).To(Equal(true))
-		})
+		DescribeTable("StorageClass from configMap is set in provider config",
+			func(expected bool) {
+				configMapIn.Data["StorageClassRequired"] = strconv.FormatBool(expected)
+				providerConfig, err := ConfigMapToProviderConfig(configMapIn, vcCreds)
+				Expect(err).To(BeNil())
+				Expect(providerConfig.StorageClassRequired).To(Equal(expected))
+			},
+			Entry("StorageClass set to false", false),
+			Entry("StorageClass set to true", true),
+		)
 	})
-
-	Context("when StorageClassRequired is set on ns config", func() {
-		Specify("return a provider config with the StorageClassRequired equals to true", func() {
-			delete(baseConfigMapIn.Data, "StorageClassRequired")
-			nsConfigMapIn.Data["StorageClassRequired"] = strconv.FormatBool(true)
-			providerConfig, err := ConfigMapsToProviderConfig(baseConfigMapIn, nsConfigMapIn, vcCreds)
-			Expect(err).To(BeNil())
-			Expect(providerConfig.StorageClassRequired).To(Equal(true))
-		})
-	})
-
-	Context("when StorageClassRequired is set on both configs", func() {
-		Specify("return a provider config with the StorageClassRequired equals to false", func() {
-			baseConfigMapIn.Data["StorageClassRequired"] = strconv.FormatBool(true)
-			nsConfigMapIn.Data["StorageClassRequired"] = strconv.FormatBool(false)
-			providerConfig, err := ConfigMapsToProviderConfig(baseConfigMapIn, nsConfigMapIn, vcCreds)
-			Expect(err).To(BeNil())
-			Expect(providerConfig.StorageClassRequired).To(Equal(false))
-		})
-		Specify("return a provider config with the StorageClassRequired equals to true", func() {
-			baseConfigMapIn.Data["StorageClassRequired"] = "false"
-			nsConfigMapIn.Data["StorageClassRequired"] = strconv.FormatBool(true)
-			providerConfig, err := ConfigMapsToProviderConfig(baseConfigMapIn, nsConfigMapIn, vcCreds)
-			Expect(err).To(BeNil())
-			Expect(providerConfig.StorageClassRequired).To(Equal(true))
-		})
-	})
-
 })

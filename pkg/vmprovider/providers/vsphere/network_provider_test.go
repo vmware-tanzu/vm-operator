7480cf0ebe31ec52ab6ac1d5ebb65b88d6e28e49
Bryan Venteicher <bventeicher@vmware.com>
2020-01-13 12:48:13 -0600

    Add govcsim based NSX-T NetworkProvider test
    The NetworkProvider uses the DVPG config.logicalSwitchUuid which isn't
    yet supported by govcsim yet so use workaround suggested by Doug [1].
    Convert existing test to use the simulator Run() method. Remove the
    currently Skipped test that was otherwise trying to test NSX-T on the
    simulated ESX host which I don't think will work.
    
    Without further refactoring of the NetworkProvider, it is not easy to
    test various bits because the code because it depends on NSX-T doing
    things in the background in reaction to the Interface object being
    created.
    
    [1] - https://github.com/dougm/govmomi/commit/ef3672a24c5dcfcc4637c771a7f96fff4ff23938

--- pkg/vmprovider/providers/vsphere/network_provider_test.go
+++ pkg/vmprovider/providers/vsphere/network_provider_test.go
@@ -7,6 +7,10 @@ package vsphere_test
 
 import (
 	"context"
+	"fmt"
+
+	"github.com/vmware/govmomi/vim25/methods"
+	"github.com/vmware/govmomi/vim25/soap"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -22,44 +26,30 @@ import (
 	"github.com/vmware-tanzu/vm-operator/pkg/apis/vmoperator/v1alpha1"
 	"github.com/vmware-tanzu/vm-operator/pkg/vmprovider/providers/vsphere"
 	ncpv1alpha1 "github.com/vmware-tanzu/vm-operator/external/ncp/api/v1alpha1"
-	clientset "gitlab.eng.vmware.com/guest-clusters/ncp-client/pkg/client/clientset/versioned"
 	ncpfake "gitlab.eng.vmware.com/guest-clusters/ncp-client/pkg/client/clientset/versioned/fake"
 )
 
-const (
-	dummyObjectName = "dummy"
-	dummyNamespace  = "dummy"
-)
+const networkId = "dummy-opnetwork-id"
 
 var _ = Describe("NetworkProvider", func() {
 	var (
-		err    error
-		finder *find.Finder
-		ns     *object.HostNetworkSystem
+		name        string
+		namespace   string
+		networkName string
 
-		ctx context.Context
 		vif *v1alpha1.VirtualMachineNetworkInterface
-
-		np  vsphere.NetworkProvider
-		dev types.BaseVirtualDevice
-
-		name      string
-		namespace string
+		vm  *v1alpha1.VirtualMachine
 	)
 
 	BeforeEach(func() {
-		ctx = context.TODO()
-		s := simulator.New(simulator.NewServiceInstance(esx.ServiceContent, esx.RootFolder))
-		client, err := vim25.NewClient(ctx, s)
-		Expect(err).To(BeNil())
-		finder = find.NewFinder(client)
-		host := object.NewHostSystem(client, esx.HostSystem.Reference())
-		ns, err = host.ConfigManager().NetworkSystem(ctx)
-		Expect(err).To(BeNil())
-		finder.SetDatacenter(object.NewDatacenter(client, esx.Datacenter.Reference()))
-	})
-	JustBeforeEach(func() {
-		vm := &v1alpha1.VirtualMachine{
+		name, namespace = "dummy", "dummyNs"
+		networkName = "gc-dummy-network"
+
+		vif = &v1alpha1.VirtualMachineNetworkInterface{
+			NetworkName: networkName,
+		}
+
+		vm = &v1alpha1.VirtualMachine{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      name,
 				Namespace: namespace,
@@ -70,81 +60,110 @@ var _ = Describe("NetworkProvider", func() {
 				},
 			},
 		}
-		dev, err = np.CreateVnic(ctx, vm, vif)
 	})
 
 	Context("default network provider", func() {
-		BeforeEach(func() {
-			name = dummyObjectName
-			namespace = dummyNamespace
-			vif = &v1alpha1.VirtualMachineNetworkInterface{
-				NetworkName: "dummy-network",
-			}
-			np = vsphere.DefaultNetworkProvider(finder)
-			spec := types.HostPortGroupSpec{
-				Name:        "dummy-network",
-				VswitchName: "vSwitch0",
-			}
-			Expect(ns.AddPortGroup(ctx, spec)).To(BeNil())
-		})
+		Specify("create vnic", func() {
+			res := simulator.ESX().Run(func(ctx context.Context, c *vim25.Client) error {
+				np, err := vsphere.NetworkProviderByType("", find.NewFinder(c), nil)
+				Expect(err).To(BeNil())
 
-		It("should succeed", func() {
-			Expect(err).To(BeNil())
-			Expect(dev).NotTo(BeNil())
+				host := object.NewHostSystem(c, esx.HostSystem.Reference())
+				ns, err := host.ConfigManager().NetworkSystem(ctx)
+				Expect(err).To(BeNil())
+
+				spec := types.HostPortGroupSpec{
+					Name:        networkName,
+					VswitchName: "vSwitch0",
+				}
+				Expect(ns.AddPortGroup(ctx, spec)).To(BeNil())
+
+				dev, err := np.CreateVnic(ctx, vm, vif)
+				Expect(err).To(BeNil())
+				Expect(dev).NotTo(BeNil())
+
+				return nil
+			})
+			Expect(res).To(BeNil())
 		})
 	})
 
 	Context("nsx-t network provider", func() {
-		var (
-			ncpClient clientset.Interface
-		)
-		BeforeEach(func() {
-			Skip("Skipping since HostNetworkSystem:hostnetworksystem-2 does not implement: AddVirtualNic")
-			ncpClient = ncpfake.NewSimpleClientset()
-			name = dummyObjectName
-			namespace = dummyNamespace
-			vif = &v1alpha1.VirtualMachineNetworkInterface{
-				NetworkName: "gc-network",
-			}
-			nsxtNp := vsphere.NsxtNetworkProvider(finder, ncpClient)
-			np = nsxtNp
-			// mock vnetif object
-			mockVnetif := &ncpv1alpha1.VirtualNetworkInterface{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      nsxtNp.GenerateNsxVnetifName("gc-network", dummyObjectName),
-					Namespace: dummyNamespace,
-				},
-				Spec: ncpv1alpha1.VirtualNetworkInterfaceSpec{
-					VirtualNetwork: "gc-network",
-				},
-				Status: ncpv1alpha1.VirtualNetworkInterfaceStatus{
-					MacAddress: "01-23-45-67-89-AB-CD-EF",
-					Conditions: []ncpv1alpha1.VirtualNetworkCondition{{Type: "Ready", Status: "True"}},
-					ProviderStatus: &ncpv1alpha1.VirtualNetworkInterfaceProviderStatus{
-						NsxLogicalSwitchID: "dummy-opnetwork-id",
-					},
-				},
-			}
-			_, err = ncpClient.VmwareV1alpha1().VirtualNetworkInterfaces(dummyNamespace).Create(mockVnetif)
-			Expect(err).To(BeNil())
-			portGroup := types.HostPortGroupSpec{
-				Name:        nsxtNp.GenerateNsxVnetifName("gc-network", dummyObjectName),
-				VswitchName: "vSwitch0",
-			}
-			Expect(ns.AddPortGroup(ctx, portGroup)).To(BeNil())
-			vnic := types.HostVirtualNicSpec{
-				OpaqueNetwork: &types.HostVirtualNicOpaqueNetworkSpec{
-					OpaqueNetworkId:   "dummy-opnetwork-id",
-					OpaqueNetworkType: "nsx-t",
-				},
-			}
-			_, err = ns.AddVirtualNic(ctx, "", vnic)
-			Expect(err).To(BeNil())
-		})
+		Specify("create vnic", func() {
+			res := simulator.VPX().Run(func(ctx context.Context, c *vim25.Client) error {
+				// config.logicalSwitchUuid not yet supported in govcsim.
+				pc := new(propertyCollectorRetrievePropertiesOverride)
+				pc.Self = c.ServiceContent.PropertyCollector
+				simulator.Map.Put(pc)
 
-		It("should succeed", func() {
-			Expect(err).To(BeNil())
-			Expect(dev).NotTo(BeNil())
+				finder := find.NewFinder(c)
+				ncpClient := ncpfake.NewSimpleClientset()
+				np, err := vsphere.NetworkProviderByType("nsx-t", finder, ncpClient)
+				Expect(err).To(BeNil())
+
+				// Must create the interface now with Status because Provider polls for it to be populated.
+				vnetif := &ncpv1alpha1.VirtualNetworkInterface{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      fmt.Sprintf("%s-%s-lsp", vif.NetworkName, vm.Name),
+						Namespace: namespace,
+					},
+					Spec: ncpv1alpha1.VirtualNetworkInterfaceSpec{
+						VirtualNetwork: vif.NetworkName,
+					},
+					Status: ncpv1alpha1.VirtualNetworkInterfaceStatus{
+						MacAddress: "01-23-45-67-89-AB-CD-EF",
+						Conditions: []ncpv1alpha1.VirtualNetworkCondition{{Type: "Ready", Status: "True"}},
+						ProviderStatus: &ncpv1alpha1.VirtualNetworkInterfaceProviderStatus{
+							NsxLogicalSwitchID: networkId,
+						},
+					},
+				}
+				_, err = ncpClient.VmwareV1alpha1().VirtualNetworkInterfaces(namespace).Create(vnetif)
+				Expect(err).To(BeNil())
+
+				dev, err := np.CreateVnic(ctx, vm, vif)
+				Expect(err).To(BeNil())
+				Expect(dev).NotTo(BeNil())
+
+				return nil
+			})
+			Expect(res).To(BeNil())
 		})
 	})
 })
+
+// See https://github.com/dougm/govmomi/commit/ef3672a24c5dcfcc4637c771a7f96fff4ff23938
+type propertyCollectorRetrievePropertiesOverride struct {
+	simulator.PropertyCollector
+}
+
+// RetrieveProperties overrides simulator.PropertyCollector.RetrieveProperties, returning a custom value for the "config.description" field
+func (pc *propertyCollectorRetrievePropertiesOverride) RetrieveProperties(ctx *simulator.Context, req *types.RetrieveProperties) soap.HasFault {
+	body := &methods.RetrievePropertiesBody{
+		Res: &types.RetrievePropertiesResponse{
+			Returnval: []types.ObjectContent{
+				{
+					Obj: req.SpecSet[0].ObjectSet[0].Obj,
+					PropSet: []types.DynamicProperty{
+						{
+							Name: "config.logicalSwitchUuid",
+							Val:  networkId,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, spec := range req.SpecSet {
+		for _, prop := range spec.PropSet {
+			for _, path := range prop.PathSet {
+				if path == "config.logicalSwitchUuid" {
+					return body
+				}
+			}
+		}
+	}
+
+	return pc.PropertyCollector.RetrieveProperties(ctx, req)
+}

vmware-tanzu/vm-operator

commit b306a164347a8799d0508f2cc7519b78f676126b

Reload inventorypath of resource pool and folder before checking
To check if a Resource Pool/Folder exists in vSphere, VM operator
checks if the entity exists for a given path. This path is generated
by contatenating the parent RP/Folder (corresponding to session) and
the child RP/Folder. The parent RP/Folder path is cached in session
cache.

If a user renames VC cluster or a folder, the cached inventory path
points becomes stale, resulting in VM operator trying to create
duplicate resource pools.

This change updates the DoesResourcePoolExists/DoesFolderExist folder
path to look for the child resource pool and folder under the session's
parent RP/Folder.

pkg/vmprovider/providers/vsphere/session.go
@@ -374,19 +374,55 @@ func (s *Session) ListVirtualMachines(ctx context.Context, path string) ([]*res.
 	return vms, nil
 }
 
-func (s *Session) ChildResourcePoolPath(resourcePoolName string) string {
-	return s.resourcepool.InventoryPath + "/" + resourcePoolName
+// ChildResourcePool returns a child resource pool by a given name under the session's parent
+// resource pool, returns error if no child resource pool exists with a given name.
+func (s *Session) ChildResourcePool(ctx context.Context, resourcePoolName string) (*object.ResourcePool, error) {
+	si := object.NewSearchIndex(s.client.VimClient())
+	ref, err := si.FindChild(ctx, s.resourcepool, resourcePoolName)
+	if err != nil {
+		return nil, err
+	}
+	if ref == nil {
+		// SearchIndex returns nil when child name is not found
+		log.Error(fmt.Errorf("ResourcePool not found"), "resourcePoolName", resourcePoolName)
+		return nil, &find.NotFoundError{}
+	}
+	child, ok := ref.(*object.ResourcePool)
+	if ok {
+		// this should always be the case for a ResourcePool child
+		return child, nil
+	}
+	return nil, fmt.Errorf("ResourcePool '%s' not found. '%s' is a %T", resourcePoolName, resourcePoolName, ref)
+}
+
+// ChildFolder returns a child resource pool by a given name under the session's parent
+// resource pool, returns error if no child resource pool exists with a given name.
+func (s *Session) ChildFolder(ctx context.Context, folderName string) (*object.Folder, error) {
+	si := object.NewSearchIndex(s.client.VimClient())
+	ref, err := si.FindChild(ctx, s.folder, folderName)
+	if err != nil {
+		return nil, err
+	}
+	if ref == nil {
+		// SearchIndex returns nil when child name is not found
+		log.Error(fmt.Errorf("Folder not found"), "folderName", folderName)
+		return nil, &find.NotFoundError{}
+	}
+	child, ok := ref.(*object.Folder)
+	if ok {
+		// this should always be the case for a ResourcePool child
+		return child, nil
+	}
+	return nil, fmt.Errorf("Folder '%s' not found. '%s' is a %T", folderName, folderName, ref)
 }
 
 // DoesResourcePoolExist checks if a ResourcePool with the given name exists.
 func (s *Session) DoesResourcePoolExist(ctx context.Context, namespace, resourcePoolName string) (bool, error) {
-	resourcePoolPath := s.ChildResourcePoolPath(resourcePoolName)
-
-	log.V(4).Info("Checking if ResourcePool exists", "resourcePoolName", resourcePoolName, "path", resourcePoolPath)
-	_, err := s.GetResourcePoolByPath(ctx, resourcePoolPath)
+	log.V(4).Info("Checking if ResourcePool exists", "resourcePoolName", resourcePoolName)
+	_, err := s.ChildResourcePool(ctx, resourcePoolName)
 	if err != nil {
 		switch err.(type) {
-		case *find.NotFoundError, *find.DefaultNotFoundError:
+		case *find.NotFoundError:
 			return false, nil
 		default:
 			return false, err
@@ -432,14 +468,13 @@ func (s *Session) UpdateResourcePool(ctx context.Context, rpSpec *v1alpha1.Resou
 func (s *Session) DeleteResourcePool(ctx context.Context, resourcePoolName string) error {
 	log.Info("Deleting the ResourcePool", "name", resourcePoolName)
 
-	resourcePoolPath := s.ChildResourcePoolPath(resourcePoolName)
-	resourcePool, err := s.GetResourcePoolByPath(ctx, resourcePoolPath)
+	resourcePool, err := s.ChildResourcePool(ctx, resourcePoolName)
 	if err != nil {
 		switch err.(type) {
 		case *find.NotFoundError, *find.DefaultNotFoundError:
 			return nil
 		default:
-			log.Error(err, "Error getting the to be deleted ResourcePool", "name", resourcePoolName, "path", resourcePoolPath)
+			log.Error(err, "Error getting the ResourcePool to be deleted", "name", resourcePoolName)
 			return err
 		}
 	}
@@ -462,22 +497,12 @@ func (s *Session) DeleteResourcePool(ctx context.Context, resourcePoolName strin
 	return nil
 }
 
-func (s *Session) ChildFolderPath(folderName string) string {
-	return s.folder.InventoryPath + "/" + folderName
-}
-
-func (s *Session) findFolder(ctx context.Context, folderName string) (*object.Folder, error) {
-	folderPath := s.ChildFolderPath(folderName)
-	log.V(4).Info("Checking if Folder exists", "name", folderName, "path", folderPath)
-	return s.GetFolderByPath(ctx, folderPath)
-}
-
 // DoesFolderExist checks if a Folder with the given name exists.
 func (s *Session) DoesFolderExist(ctx context.Context, namespace, folderName string) (bool, error) {
-	_, err := s.findFolder(ctx, folderName)
+	_, err := s.ChildFolder(ctx, folderName)
 	if err != nil {
 		switch err.(type) {
-		case *find.NotFoundError, *find.DefaultNotFoundError:
+		case *find.NotFoundError:
 			return false, nil
 		default:
 			return false, err
@@ -510,13 +535,13 @@ func (s *Session) CreateFolder(ctx context.Context, folderSpec *v1alpha1.FolderS
 func (s *Session) DeleteFolder(ctx context.Context, folderName string) error {
 	log.Info("Deleting the Folder", "name", folderName)
 
-	folder, err := s.findFolder(ctx, folderName)
+	folder, err := s.ChildFolder(ctx, folderName)
 	if err != nil {
 		switch err.(type) {
 		case *find.NotFoundError, *find.DefaultNotFoundError:
 			return nil
 		default:
-			log.Error(err, "Error finding the VM folder to delete", "name", folderName, "path", s.ChildFolderPath(folderName))
+			log.Error(err, "Error finding the VM folder to delete", "name", folderName)
 			return err
 		}
 	}
@@ -550,21 +575,21 @@ func (s *Session) GetRPAndFolderFromResourcePolicy(ctx context.Context,
 	}
 
 	resourcePoolName := resourcePolicy.Spec.ResourcePool.Name
-	resourcePoolPath := s.ChildResourcePoolPath(resourcePoolName)
-	resourcePool, err := s.GetResourcePoolByPath(ctx, resourcePoolPath)
+	resourcePool, err := s.ChildResourcePool(ctx, resourcePoolName)
 	if err != nil {
-		log.Error(err, "Unable to find ResourcePool", "name", resourcePoolName, "path", resourcePoolPath)
+		log.Error(err, "Unable to find ResourcePool", "name", resourcePoolName)
 		return nil, nil, err
 	}
-	log.V(4).Info("Found RP:", "name", resourcePoolName, "path", resourcePoolPath, "obj", resourcePool.Reference().Value)
+
+	log.V(4).Info("Found RP:", "name", resourcePoolName, "moRef", resourcePool.Reference().Value)
 
 	folderName := resourcePolicy.Spec.Folder.Name
-	folder, err := s.findFolder(ctx, folderName)
+	folder, err := s.ChildFolder(ctx, folderName)
 	if err != nil {
-		log.Error(err, "Unable to find Folder", "name", folderName, "path", s.ChildFolderPath(folderName))
+		log.Error(err, "Unable to find Folder", "name", folderName)
 		return nil, nil, err
 	}
-	log.V(4).Info("Found Folder:", "name", folderName, "path", s.ChildFolderPath(folderName), "obj", folder.Reference().Value)
+	log.V(4).Info("Found Folder:", "name", folderName, "moRef", folder.Reference().Value)
 
 	return resourcePool, folder, nil
 }
@@ -729,15 +754,18 @@ func (s *Session) GetVirtualMachine(ctx context.Context, vm *v1alpha1.VirtualMac
 			return nil, err
 		}
 
-		folder, err = s.findFolder(ctx, resourcePolicy.Spec.Folder.Name)
+		vmFolderName := resourcePolicy.Spec.Folder.Name
+		vmFolder, err := s.ChildFolder(ctx, vmFolderName)
+
 		if err != nil {
-			log.Error(err, "Failed to find folder", "Folder", resourcePolicy.Spec.Folder.Name)
+			log.Error(err, "Failed to find folder", "folderName", vmFolderName, "moRef", vmFolder.Reference().Value)
 			return nil, err
 		}
 
 	} else {
 		// Developer enablement path: Use the default folder and RP for the session.
-		folder = s.folder
+		// TODO: AKP: If any of the parent objects have been renamed, the cached inventory path will be stale.
+		// 		folder = s.folder
 	}
 
 	vmPath := folder.InventoryPath + "/" + vm.Name
@@ -1233,11 +1261,6 @@ func (s *Session) GetResourcePoolByMoID(ctx context.Context, moID string) (*obje
 	return o.(*object.ResourcePool), nil
 }
 
-// GetResourcePoolByPath returns resource pool for a given inventory path
-func (s *Session) GetResourcePoolByPath(ctx context.Context, path string) (*object.ResourcePool, error) {
-	return s.Finder.ResourcePool(ctx, path)
-}
-
 // GetFolderByMoID returns a folder for a given moref
 func (s *Session) GetFolderByMoID(ctx context.Context, moID string) (*object.Folder, error) {
 	ref := types.ManagedObjectReference{Type: "Folder", Value: moID}
@@ -1246,11 +1269,6 @@ func (s *Session) GetFolderByMoID(ctx context.Context, moID string) (*object.Fol
 		return nil, err
 	}
 	return o.(*object.Folder), nil
-}
-
-// GetFolderByPath returns a folder for a given inventory path
-func (s *Session) GetFolderByPath(ctx context.Context, path string) (*object.Folder, error) {
-	return s.Finder.Folder(ctx, path)
 }
 
 func IsSupportedDeployType(t string) bool {
@@ -1594,6 +1612,46 @@ func (s *Session) DetachTagFromVm(ctx context.Context, tagName string, tagCatNam
 		vmRef := &vimTypes.ManagedObjectReference{Type: "VirtualMachine", Value: resVm.ReferenceValue()}
 		return manager.DetachTag(ctx, tag.ID, vmRef)
 	})
+}
+
+// RenameSessionCluster renames the cluster corresponding to this session. Used only in integration tests for now.
+func (s *Session) RenameSessionCluster(ctx context.Context, name string) error {
+	task, err := s.cluster.Rename(ctx, name)
+	if err != nil {
+		log.Error(err, "Failed to invoke rename for cluster", "clusterMoID", s.cluster.Reference().Value)
+		return err
+	}
+
+	if taskResult, err := task.WaitForResult(ctx, nil); err != nil {
+		msg := ""
+		if taskResult != nil && taskResult.Error != nil {
+			msg = taskResult.Error.LocalizedMessage
+		}
+		log.Error(err, "Error in renaming cluster", "clusterMoID", s.cluster.Reference().Value, "msg", msg)
+		return err
+	}
+
+	return nil
+}
+
+// RenameSessionFolder renames the folder corresponding to this session. Used only by integration tests for now.
+func (s *Session) RenameSessionFolder(ctx context.Context, name string) error {
+	task, err := s.folder.Rename(ctx, name)
+	if err != nil {
+		log.Error(err, "Failed to invoke rename for folder", "folderMoID", s.folder.Reference().Value)
+		return err
+	}
+
+	if taskResult, err := task.WaitForResult(ctx, nil); err != nil {
+		msg := ""
+		if taskResult != nil && taskResult.Error != nil {
+			msg = taskResult.Error.LocalizedMessage
+		}
+		log.Error(err, "Error in renaming cluster", "clusterMoID", s.folder.Reference().Value, "msg", msg)
+		return err
+	}
+
+	return nil
 }
 
 func (s *Session) updateVirtualMachine(ctx context.Context, vm *v1alpha1.VirtualMachine, vmConfigArgs vmprovider.VmConfigArgs) (*res.VirtualMachine, error) {

pkg/vmprovider/integration/session_integration_test.go
@@ -12,7 +12,7 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/vmware/govmomi/object"
+	"github.com/vmware/govmomi/find"
 	"github.com/vmware/govmomi/simulator"
 	"github.com/vmware/govmomi/vapi/rest"
 	"github.com/vmware/govmomi/vapi/tags"
@@ -589,8 +589,27 @@ var _ = Describe("Sessions", func() {
 			})
 
 			Context("Create a ResourcePool, verify it exists and delete it", func() {
-
 				It("Verifies if a ResourcePool exists", func() {
+					exists, err := session.DoesResourcePoolExist(context.TODO(), integration.DefaultNamespace, rpSpec.Name)
+					Expect(exists).To(BeTrue())
+					Expect(err).NotTo(HaveOccurred())
+				})
+			})
+
+			Context("Create a ResourcePool, rename the VC cluster, verify that resource pool is still found", func() {
+				var (
+					defaultVCSimCluster string
+					newClusterName      string
+				)
+				BeforeEach(func() {
+					defaultVCSimCluster = simulator.Map.Any("ClusterComputeResource").Entity().Name
+					newClusterName = "newCluster"
+					Expect(session.RenameSessionCluster(context.TODO(), newClusterName)).To(Succeed())
+				})
+				AfterEach(func() {
+					Expect(session.RenameSessionCluster(context.TODO(), defaultVCSimCluster)).To(Succeed())
+				})
+				It("Verify that the resource pool exists", func() {
 					exists, err := session.DoesResourcePoolExist(context.TODO(), integration.DefaultNamespace, rpSpec.Name)
 					Expect(exists).To(BeTrue())
 					Expect(err).NotTo(HaveOccurred())
@@ -604,6 +623,15 @@ var _ = Describe("Sessions", func() {
 					Expect(err).To(HaveOccurred())
 					Expect(err.Error()).To(Equal("ServerFaultCode: DuplicateName"))
 					Expect(rpMoId).To(BeEmpty())
+				})
+			})
+
+			Context("ChildResourcePool", func() {
+				It("returns NotFoundError for a resource pool that doesn't exist", func() {
+					_, err := session.ChildResourcePool(context.TODO(), "nonExistentResourcePool")
+					Expect(err).To(HaveOccurred())
+					_, ok := err.(*find.NotFoundError)
+					Expect(ok).Should(BeTrue())
 				})
 			})
 
@@ -625,12 +653,12 @@ var _ = Describe("Sessions", func() {
 				}
 			})
 
-			Context("Create a Folder, verify it exists and delete it", func() {
+			Context("Create a folder, verify it exists and delete it", func() {
 				JustBeforeEach(func() {
+
 					folderMoId, err := session.CreateFolder(context.TODO(), folderSpec)
 					Expect(err).NotTo(HaveOccurred())
 					Expect(folderMoId).To(Not(BeEmpty()))
-
 				})
 
 				JustAfterEach(func() {
@@ -657,6 +685,16 @@ var _ = Describe("Sessions", func() {
 					Expect(folderMoId2).To(BeEmpty())
 				})
 			})
+
+			Context("ChildFolder", func() {
+				It("returns NotFoundError for a folder that doesn't exist", func() {
+					_, err := session.ChildFolder(context.TODO(), "nonExistentFolderName")
+					Expect(err).To(HaveOccurred())
+					_, ok := err.(*find.NotFoundError)
+					Expect(ok).Should(BeTrue())
+				})
+			})
+
 			Context("Delete a Folder that doesnt exist", func() {
 				It("should succeed", func() {
 					Expect(session.DeleteFolder(context.TODO(), folderSpec.Name)).To(Succeed())
@@ -731,19 +769,14 @@ var _ = Describe("Sessions", func() {
 			})
 		})
 
-		Context("RP as inventory path", func() {
+		Context("RP as moID", func() {
 			It("returns RP object without error", func() {
 				pools, err := session.Finder.ResourcePoolList(ctx, "*")
 				Expect(err).ShouldNot(HaveOccurred())
 				Expect(len(pools)).ToNot(BeZero())
 
 				existingPool := pools[0]
-				pool, err := session.GetResourcePoolByPath(ctx, existingPool.InventoryPath)
-				Expect(err).ShouldNot(HaveOccurred())
-				Expect(pool.InventoryPath).To(Equal(existingPool.InventoryPath))
-				Expect(pool.Reference().Value).To(Equal(existingPool.Reference().Value))
-
-				pool, err = session.GetResourcePoolByMoID(ctx, existingPool.Reference().Value)
+				pool, err := session.GetResourcePoolByMoID(ctx, existingPool.Reference().Value)
 				Expect(err).ShouldNot(HaveOccurred())
 				Expect(pool.InventoryPath).To(Equal(existingPool.InventoryPath))
 				Expect(pool.Reference().Value).To(Equal(existingPool.Reference().Value))
@@ -751,21 +784,11 @@ var _ = Describe("Sessions", func() {
 		})
 
 		Context("when finding folders", func() {
-			var folders []*object.Folder
-			BeforeEach(func() {
-				folders, err = session.Finder.FolderList(ctx, "*")
+			It("folder as moid returns Folder object without error", func() {
+				folders, err := session.Finder.FolderList(ctx, "*")
 				Expect(err).ShouldNot(HaveOccurred())
 				Expect(len(folders)).ToNot(BeZero())
-			})
 
-			It("folder as inventory path returns Folder object without error", func() {
-				folder, err := session.GetFolderByPath(ctx, folders[0].InventoryPath)
-				Expect(err).ShouldNot(HaveOccurred())
-				Expect(folder.InventoryPath).To(Equal(folders[0].InventoryPath))
-				Expect(folder.Reference().Value).To(Equal(folders[0].Reference().Value))
-			})
-
-			It("folder as moid returns Folder object without error", func() {
 				folder, err := session.GetFolderByMoID(ctx, folders[0].Reference().Value)
 				Expect(err).ShouldNot(HaveOccurred())
 				Expect(folder.InventoryPath).To(Equal(folders[0].InventoryPath))

pkg/vmprovider/integration/vmprovider_integration_test.go
@@ -30,8 +30,8 @@ func createResourcePool(rpName string) (*object.ResourcePool, error) {
 	}
 	_, err := session.CreateResourcePool(context.TODO(), rpSpec)
 	Expect(err).NotTo(HaveOccurred())
-	return session.GetResourcePoolByPath(context.Background(), session.ChildResourcePoolPath(rpSpec.Name))
 
+	return session.ChildResourcePool(context.TODO(), rpSpec.Name)
 }
 
 func createFolder(folderName string) (*object.Folder, error) {
@@ -40,7 +40,8 @@ func createFolder(folderName string) (*object.Folder, error) {
 	}
 	_, err := session.CreateFolder(context.TODO(), folderSpec)
 	Expect(err).NotTo(HaveOccurred())
-	return session.GetFolderByPath(context.Background(), session.ChildFolderPath(folderSpec.Name))
+
+	return session.ChildFolder(context.TODO(), folderSpec.Name)
 }
 
 func getSimpleVirtualMachine(name string) *vmoperatorv1alpha1.VirtualMachine {

commit c4d862085e69a5dbe5b53de7be1c714b5454d3d6

Handle the panic introduced by commit b306a164
Commit b306a164 introduced a bug where the VM folder can potentially
be nil when we try to access the inventory path of a VM. This change
fixes that.

- Added an integration tests for this scenario.
- While testing this, I discovered that the ChildFolder and
  ChildResourcePool functions were not setting the inteventory path of
  the returned objects (since they were not finder methods). Updated
  both the functions to set the inventory path before returning.

pkg/vmprovider/providers/vsphere/session.go
@@ -374,46 +374,56 @@ func (s *Session) ListVirtualMachines(ctx context.Context, path string) ([]*res.
 	return vms, nil
 }
 
-// ChildResourcePool returns a child resource pool by a given name under the session's parent
-// resource pool, returns error if no child resource pool exists with a given name.
-func (s *Session) ChildResourcePool(ctx context.Context, resourcePoolName string) (*object.ResourcePool, error) {
+// findChildEntity finds a child entity by a given name under a parent object
+func (s *Session) findChildEntity(ctx context.Context, parent object.Reference, childName string) (object.Reference, error) {
 	si := object.NewSearchIndex(s.client.VimClient())
-	ref, err := si.FindChild(ctx, s.resourcepool, resourcePoolName)
+	ref, err := si.FindChild(ctx, parent, childName)
 	if err != nil {
 		return nil, err
 	}
 	if ref == nil {
 		// SearchIndex returns nil when child name is not found
-		log.Error(fmt.Errorf("ResourcePool not found"), "resourcePoolName", resourcePoolName)
+		log.Error(fmt.Errorf("entity not found"), "name", childName)
 		return nil, &find.NotFoundError{}
 	}
-	child, ok := ref.(*object.ResourcePool)
-	if ok {
-		// this should always be the case for a ResourcePool child
-		return child, nil
+
+	// We have found a child entity with the given name. Populate the inventory path before returning.
+	child, err := s.Finder.ObjectReference(ctx, ref.Reference())
+	if err != nil {
+		log.Error(err, "error when setting inventory path for the object", "moRef", child.Reference().Value)
+		return nil, err
 	}
-	return nil, fmt.Errorf("ResourcePool '%s' not found. '%s' is a %T", resourcePoolName, resourcePoolName, ref)
+	return child, err
+}
+
+// ChildResourcePool returns a child resource pool by a given name under the session's parent
+// resource pool, returns error if no child resource pool exists with a given name.
+func (s *Session) ChildResourcePool(ctx context.Context, resourcePoolName string) (*object.ResourcePool, error) {
+	resourcePool, err := s.findChildEntity(ctx, s.resourcepool, resourcePoolName)
+	if err != nil {
+		return nil, err
+	}
+
+	rp, ok := resourcePool.(*object.ResourcePool)
+	if !ok {
+		return nil, fmt.Errorf("ResourcePool '%s' not found. '%s' is a %T", resourcePoolName, resourcePoolName, rp)
+	}
+	return resourcePool.(*object.ResourcePool), err
 }
 
 // ChildFolder returns a child resource pool by a given name under the session's parent
 // resource pool, returns error if no child resource pool exists with a given name.
 func (s *Session) ChildFolder(ctx context.Context, folderName string) (*object.Folder, error) {
-	si := object.NewSearchIndex(s.client.VimClient())
-	ref, err := si.FindChild(ctx, s.folder, folderName)
+	folder, err := s.findChildEntity(ctx, s.folder, folderName)
 	if err != nil {
 		return nil, err
 	}
-	if ref == nil {
-		// SearchIndex returns nil when child name is not found
-		log.Error(fmt.Errorf("Folder not found"), "folderName", folderName)
-		return nil, &find.NotFoundError{}
+
+	folder, ok := folder.(*object.Folder)
+	if !ok {
+		return nil, fmt.Errorf("Folder '%s' not found. '%s' is a %T", folderName, folderName, folder)
 	}
-	child, ok := ref.(*object.Folder)
-	if ok {
-		// this should always be the case for a ResourcePool child
-		return child, nil
-	}
-	return nil, fmt.Errorf("Folder '%s' not found. '%s' is a %T", folderName, folderName, ref)
+	return folder.(*object.Folder), err
 }
 
 // DoesResourcePoolExist checks if a ResourcePool with the given name exists.
@@ -754,11 +764,11 @@ func (s *Session) GetVirtualMachine(ctx context.Context, vm *v1alpha1.VirtualMac
 			return nil, err
 		}
 
-		vmFolderName := resourcePolicy.Spec.Folder.Name
-		vmFolder, err := s.ChildFolder(ctx, vmFolderName)
+		folderName := resourcePolicy.Spec.Folder.Name
+		folder, err = s.ChildFolder(ctx, folderName)
 
 		if err != nil {
-			log.Error(err, "Failed to find folder", "folderName", vmFolderName, "moRef", vmFolder.Reference().Value)
+			log.Error(err, "Failed to find folder", "folderName", folderName, "moRef", folder.Reference().Value)
 			return nil, err
 		}
 

pkg/vmprovider/integration/session_integration_test.go
@@ -22,12 +22,13 @@ import (
 
 	"github.com/vmware-tanzu/vm-operator-api/api/v1alpha1"
 	vmoperatorv1alpha1 "github.com/vmware-tanzu/vm-operator-api/api/v1alpha1"
-	ncpfake "gitlab.eng.vmware.com/guest-clusters/ncp-client/pkg/client/clientset/versioned/fake"
 
+	vmopfake "github.com/vmware-tanzu/vm-operator/pkg/client/clientset_generated/clientset/fake"
 	"github.com/vmware-tanzu/vm-operator/pkg/vmprovider"
 	"github.com/vmware-tanzu/vm-operator/pkg/vmprovider/providers/vsphere"
 	"github.com/vmware-tanzu/vm-operator/pkg/vmprovider/providers/vsphere/resources"
 	"github.com/vmware-tanzu/vm-operator/test/integration"
+	ncpfake "gitlab.eng.vmware.com/guest-clusters/ncp-client/pkg/client/clientset/versioned/fake"
 )
 
 var (
@@ -37,13 +38,15 @@ var (
 
 var _ = Describe("Sessions", func() {
 	var (
-		session   *vsphere.Session
-		ncpClient *ncpfake.Clientset
+		session    *vsphere.Session
+		ncpClient  *ncpfake.Clientset
+		vmopClient *vmopfake.Clientset
 	)
 	BeforeEach(func() {
 		ctx = context.Background()
 		ncpClient = ncpfake.NewSimpleClientset()
-		session, err = vsphere.NewSessionAndConfigure(context.TODO(), c, vSphereConfig, clientSet, ncpClient, nil)
+		vmopClient = vmopfake.NewSimpleClientset()
+		session, err = vsphere.NewSessionAndConfigure(context.TODO(), c, vSphereConfig, clientSet, ncpClient, vmopClient)
 		Expect(err).NotTo(HaveOccurred())
 	})
 
@@ -147,26 +150,57 @@ var _ = Describe("Sessions", func() {
 		})
 
 		Context("When MoID is absent", func() {
+			Context("When a resourcepolicy exists", func() {
+				It("should successfully find the VM by path", func() {
+					namespace := integration.DefaultNamespace
+					vmName := "getvm-with-rp-and-without-moID"
+					resourcePolicy := getVirtualMachineSetResourcePolicy(vmName, namespace)
+					_, err := vmopClient.VmoperatorV1alpha1().VirtualMachineSetResourcePolicies(namespace).Create(resourcePolicy)
+					Expect(err).NotTo(HaveOccurred())
 
-			It("should successfully find the VM by path", func() {
-				ctx := context.Background()
+					Expect(vmProvider.CreateOrUpdateVirtualMachineSetResourcePolicy(ctx, resourcePolicy)).To(Succeed())
 
-				imageName := "test-item"
-				vmName := "getvm-without-moID"
+					imageName := "test-item"
+					vmConfigArgs := getVmConfigArgs(namespace, vmName)
+					vm := getVirtualMachineInstance(vmName, namespace, imageName, vmConfigArgs.VmClass.Name)
+					vm.Spec.ResourcePolicyName = resourcePolicy.Name
+					vmConfigArgs.ResourcePolicy = resourcePolicy
 
-				vmConfigArgs := getVmConfigArgs(testNamespace, vmName)
-				vm := getVirtualMachineInstance(vmName, testNamespace, imageName, vmConfigArgs.VmClass.Name)
+					clonedVM, err := session.CloneVirtualMachine(context.TODO(), vm, vmConfigArgs)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(clonedVM.Name).Should(Equal(vmName))
+					moId, err := clonedVM.UniqueID(ctx)
+					Expect(err).NotTo(HaveOccurred())
 
-				clonedVM, err := session.CloneVirtualMachine(context.TODO(), vm, vmConfigArgs)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(clonedVM.Name).Should(Equal(vmName))
-				moId, err := clonedVM.UniqueID(ctx)
-				Expect(err).NotTo(HaveOccurred())
+					vm.Status.UniqueID = ""
 
-				vm.Status.UniqueID = ""
-				vm1, err := session.GetVirtualMachine(ctx, vm)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(vm1.UniqueID(ctx)).To(Equal(moId))
+					vm1, err := session.GetVirtualMachine(ctx, vm)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(vm1.UniqueID(ctx)).To(Equal(moId))
+				})
+			})
+
+			Context("When a resourcepolicy doesn't exist", func() {
+				It("should successfully find the VM by path", func() {
+					ctx := context.Background()
+					imageName := "test-item"
+					vmName := "getvm-without-moID"
+
+					vmConfigArgs := getVmConfigArgs(testNamespace, vmName)
+					vm := getVirtualMachineInstance(vmName, testNamespace, imageName, vmConfigArgs.VmClass.Name)
+
+					clonedVM, err := session.CloneVirtualMachine(context.TODO(), vm, vmConfigArgs)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(clonedVM.Name).Should(Equal(vmName))
+					moId, err := clonedVM.UniqueID(ctx)
+					Expect(err).NotTo(HaveOccurred())
+
+					vm.Status.UniqueID = ""
+
+					vm1, err := session.GetVirtualMachine(ctx, vm)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(vm1.UniqueID(ctx)).To(Equal(moId))
+				})
 			})
 		})
 	})
93c06087a65d50c06f90c60124756de8caf15a72
Arunesh Pandey <parunesh@vmware.com>
2020-05-11 19:31:29 -0700

    Retry installing VM classes until validating webhook is ready
    We cannot install VM classes on the API server if the validating
    webhook is not ready.
    
    Following is an example of the error:
    ```
    [2020-05-10T17:16:15.330Z] KUBECONFIG=/root/.kube/kind-config-kind-it
    kubectl apply -f artifacts/default-vmclasses.yaml
    
    [2020-05-10T17:16:15.330Z] Error from server (InternalError): error
    when creating "artifacts/default-vmclasses.yaml": Internal error
    occurred: failed calling webhook
    "default.validating.virtualmachineclass.vmoperator.vmware.com": Post
    https://vmware-system-vmop-webhook-service.vmware-system-vmop.svc:443/default-validate-vmoperator-vmware-com-v1alpha1-virtualmachineclass?timeout=30s:
    dial tcp 10.103.188.116:443: connect: connection refused
    ```
    
    This change adds a retry wrapper for the VM class installation so we
    retry until the webhook is ready or until the 5 minutes timeout is
    reached. This is similar to what we do in WCP environments.
    
    - Also fix the typo in the make clean target to remove all the .out
    files.
    
    ```
    ~/go/src/github.com/vmware-tanzu/vm-operator $ ls -al *.out
    -rw-r--r--   1 parunesh       staff     7548935 2020-05-05 22:01 cover.out
    -rw-r--r--   1 parunesh       staff     4818121 2020-02-20 14:09 coverage.out
    -rw-r--r--   1 parunesh       staff           0 2019-10-16  2019 final-coverage.out
    -rw-r--r--   1 parunesh       staff           0 2019-10-16  2019 final-coverge.out
    -rw-r--r--   1 parunesh       staff      145483 2019-08-08  2019 int-coverage.out
    -rw-r--r--   1 parunesh       staff      328807 2020-05-09 21:43 integration-cover.out
    -rw-r--r--   1 parunesh       staff      145317 2020-05-09 21:43 integration-tests.out
    -rw-r--r--   1 parunesh       staff       87650 2019-09-21  2019 unit-coverage.out
    -rw-r--r--   1 parunesh       staff      130432 2020-05-05 22:01 unit-tests.out
    ~/go/src/github.com/vmware-tanzu/vm-operator $ make clean
    Remove docker container vmoperator-controller:latest
    Untagged: vmoperator-controller:latest
    rm -rf bin *.out artifacts
    ~/go/src/github.com/vmware-tanzu/vm-operator $ ls -al *.out
    *.out: No such file or directory
    ```

--- hack/deploy-local.sh
+++ hack/deploy-local.sh
@@ -14,7 +14,24 @@ KUBECTL="kubectl"
 VMOP_NAMESPACE="vmware-system-vmop"
 VMOP_DEPLOYMENT="vmware-system-vmop-controller-manager"
 
+ARTIFACTS_DIR="artifacts"
+DEFAULT_VMCLASSES_YAML="${ARTIFACTS_DIR}/default-vmclasses.yaml"
+
 $KUBECTL apply -f "$YAML"
 
 $KUBECTL rollout restart -n ${VMOP_NAMESPACE} deployment ${VMOP_DEPLOYMENT}
 $KUBECTL rollout status -n ${VMOP_NAMESPACE} deployment ${VMOP_DEPLOYMENT}
+
+# Hack that retries applying the default VM Classes until the
+# validating webhook is available.
+VMOP_VMCLASSES_ATTEMPTS=0
+while true ; do
+    kubectl apply -f "${DEFAULT_VMCLASSES_YAML}" && break
+    VMOP_VMCLASSES_ATTEMPTS=$((VMOP_VMCLASSES_ATTEMPTS+1))
+    if [[ $VMOP_VMCLASSES_ATTEMPTS -ge 60 ]] ; then
+	log "Failed to apply default VM Classes"
+	exit 1
+    fi
+    echo "Cannot create default VM Classes. Trying again."
+    sleep "5s"
+done

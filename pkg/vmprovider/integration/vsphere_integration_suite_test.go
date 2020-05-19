a5ed562e79046bf11035a79ea589288b9684dcb7
Arunesh Pandey <parunesh@vmware.com>
2020-05-19 09:04:36 -0700

    Use controller-runtime client instead of client-gen generated client
    VM operator uses typed clients generated via client-gen. Among other
    issues This client uses a cache, which leads to races when different
    clients try to access the same object via same code path.
    
    This change migrates VM operator to use controller-runtime generated
    client. This allows us to perform operations against any reosurce Kind
    that is registered in our scheme. The controller-runtime client also
    supports unstructured types.
    
    Since we are no longer using the typed clients, this commit removes
    the pkg/client directory.

--- pkg/vmprovider/integration/vsphere_integration_suite_test.go
+++ pkg/vmprovider/integration/vsphere_integration_suite_test.go
@@ -16,6 +16,7 @@ import (
 	. "github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 
 	"github.com/vmware-tanzu/vm-operator/pkg/vmprovider/providers/vsphere"
@@ -23,13 +24,14 @@ import (
 )
 
 var (
-	restConfig    *rest.Config
-	vcSim         *integration.VcSimInstance
-	testEnv       *envtest.Environment
-	vSphereConfig *vsphere.VSphereVmProviderConfig
-	session       *vsphere.Session
-	vmProvider    vmprovider.VirtualMachineProviderInterface
-	clientSet     *kubernetes.Clientset
+	restConfig        *rest.Config
+	vcSim             *integration.VcSimInstance
+	testEnv           *envtest.Environment
+	vSphereConfig     *vsphere.VSphereVmProviderConfig
+	session           *vsphere.Session
+	vmProvider        vmprovider.VirtualMachineProviderInterface
+	clientSet         *kubernetes.Clientset
+	ctrlruntimeClient client.Client
 
 	err error
 	ctx context.Context
@@ -44,6 +46,9 @@ func TestVSphereIntegrationProvider(t *testing.T) {
 var _ = BeforeSuite(func() {
 	testEnv, vSphereConfig, restConfig, vcSim, session, vmProvider = integration.SetupIntegrationEnv([]string{integration.DefaultNamespace})
 	clientSet = kubernetes.NewForConfigOrDie(restConfig)
+
+	ctrlruntimeClient, err = integration.GetVmopClient(restConfig)
+	Expect(err).NotTo(HaveOccurred())
 
 	ctx = context.Background()
 

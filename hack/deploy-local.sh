093f5ed15ee0fc6df93ae5f05ea6b1d65378c8a2
Anupam Garg <anupamg@vmware.com>
2020-06-09 13:58:40 -0700

    deploy-local changes
    - add a `revision_history_limit.yaml` for local deployments so that
      frequent local reloads do not accumulate too many replicasets
    - pass vmclasses to `hack/deploy-local.sh` like deployment yaml,
      instead of having `hack/deploy-local.sh` understand artifacts
      or artifactsdir
    - modify local deployment to a single replica if deploying on a
      single node kind cluster
    
    Testing Done:
    - dev-bootstrap's `deploy.sh -c vcsim vmop`
    - `make deploy-local-vcsim`
    - `make test`
    - `make test-integration`

--- hack/deploy-local.sh
+++ hack/deploy-local.sh
@@ -2,35 +2,50 @@
 # Deploy VMOP to the given cluster
 #
 # Usage:
-# $ deploy-local.sh <yaml>
+# $ deploy-local.sh <deploy_yaml> <vmclasses_yaml>
 
 set -o errexit
 set -o pipefail
 set -o nounset
 
 YAML=$1
+VMCLASSES_YAML=$2
+
 KUBECTL="kubectl"
 
 VMOP_NAMESPACE="vmware-system-vmop"
 VMOP_DEPLOYMENT="vmware-system-vmop-controller-manager"
 
-ARTIFACTS_DIR="artifacts"
-DEFAULT_VMCLASSES_YAML="${ARTIFACTS_DIR}/default-vmclasses.yaml"
+DEPLOYMENT_EXISTS=""
+if $KUBECTL get deployment -n ${VMOP_NAMESPACE} ${VMOP_DEPLOYMENT} >/dev/null 2>&1 ; then
+    DEPLOYMENT_EXISTS=1
+fi
+
+# Hack to reduce the number of replicas deployed from 3 to 1
+# when deploying onto a single node kind cluster.
+NODE_COUNT=$(kubectl get node --no-headers 2>/dev/null | wc -l)
+if [ "$NODE_COUNT" -eq 1 ]; then
+  sed -i -e 's/replicas: 3/replicas: 1/g' "$YAML"
+  # remove the generated '-e' file on Mac
+  rm -f "$YAML-e"
+fi
 
 $KUBECTL apply -f "$YAML"
 
-$KUBECTL rollout restart -n ${VMOP_NAMESPACE} deployment ${VMOP_DEPLOYMENT}
-$KUBECTL rollout status -n ${VMOP_NAMESPACE} deployment ${VMOP_DEPLOYMENT}
+if [[ -n $DEPLOYMENT_EXISTS ]] ; then
+    $KUBECTL rollout restart -n ${VMOP_NAMESPACE} deployment ${VMOP_DEPLOYMENT}
+    $KUBECTL rollout status -n ${VMOP_NAMESPACE} deployment ${VMOP_DEPLOYMENT}
+fi
 
 # Hack that retries applying the default VM Classes until the
 # validating webhook is available.
 VMOP_VMCLASSES_ATTEMPTS=0
 while true ; do
-    kubectl apply -f "${DEFAULT_VMCLASSES_YAML}" && break
+    kubectl apply -f "${VMCLASSES_YAML}" && break
     VMOP_VMCLASSES_ATTEMPTS=$((VMOP_VMCLASSES_ATTEMPTS+1))
     if [[ $VMOP_VMCLASSES_ATTEMPTS -ge 60 ]] ; then
-	log "Failed to apply default VM Classes"
-	exit 1
+        echo "Failed to apply default VM Classes"
+        exit 1
     fi
     echo "Cannot create default VM Classes. Trying again."
     sleep "5s"

84b1ee259e7c279e83911074d5dea1d2bdf8d1c6
Varun Jain <jainvarun@vmware.com>
2020-08-10 10:02:12 -0700

    Validating webhook for VM volumes
    This patch adds validating webhook check to ensure
    only one volume source is set for each VM volume.
    
    Testing Done:
    - make test

--- webhooks/virtualmachine/validation/messages/messages.go
+++ webhooks/virtualmachine/validation/messages/messages.go
@@ -10,9 +10,10 @@ const (
 	NetworkNameNotSpecifiedFmt               = "spec.networkInterfaces[%d].networkName must be specified"
 	NetworkTypeNotSupportedFmt               = "spec.networkInterfaces[%d].networkType is not supported"
 	VolumeNameNotSpecifiedFmt                = "spec.volumes[%d].name must be specified"
-	PersistentVolumeClaimNotSpecifiedFmt     = "spec.volumes[%d].persistentVolumeClaim must be specified"
 	PersistentVolumeClaimNameNotSpecifiedFmt = "spec.volumes[%d].persistentVolumeClaim.claimName must be specified"
 	VolumeNameDuplicateFmt                   = "spec.volumes[%d].name must be unique"
 	MetadataTransportNotSupported            = "spec.vmmetadata.transport is not supported"
 	MetadataTransportConfigMapNotSpecified   = "spec.vmmetadata.configmapname must be specified"
+	MultipleVolumeSpecifiedFmt               = "only one of spec.volumes[%d].persistentVolumeClaim/spec.volumes[%d].vsphereVolume must be specified"
+	VolumeNotSpecifiedFmt                    = "one of spec.volumes[%d].persistentVolumeClaim/spec.volumes[%d].vsphereVolume must be specified"
 )

758ca0d7dfe304a7e159496595d40169bb370a70
Arunesh Kumar Pandey <parunesh@vmware.com>
2020-08-26 09:51:39 -0700

    VirtualMachineService controller improvements
    This change improves the VirtualMachineService controller and tests.
    Changes include:
    - Adding function comments and other code comments.
    - Modify the createOrUpdateService function so we are not dealing with a variety
    of Service and VMService objects at a time.
    - Fold the `mergeLabels` function in the parent createOrUpdateService.
    - Modify the tests to remove the nested levels of BeforeEach/AfterEach. We also
    had tests that relied on the objects created by other tests. Tried to untangle
    them as much as I can.

--- controllers/virtualmachineservice/utils/utils.go
+++ controllers/virtualmachineservice/utils/utils.go
@@ -5,48 +5,13 @@
 package utils
 
 import (
-	"encoding/json"
-	"fmt"
 	"reflect"
 
-	corev1 "k8s.io/api/core/v1"
-	apiequality "k8s.io/apimachinery/pkg/api/equality"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/pointer"
 
 	vmoperatorv1alpha1 "github.com/vmware-tanzu/vm-operator-api/api/v1alpha1"
 )
-
-// ServiceEqual compares the new Service's spec with old Service's last applied config
-func ServiceEqual(newService, oldService *corev1.Service) (bool, error) {
-	if lastAppliedConfig, ok := oldService.GetAnnotations()[corev1.LastAppliedConfigAnnotation]; ok {
-		lastAppliedService := corev1.Service{}
-		err := json.Unmarshal([]byte(lastAppliedConfig), &lastAppliedService)
-		if err != nil {
-			return false, err
-		}
-		return apiequality.Semantic.DeepEqual(lastAppliedService.Spec, newService.Spec), nil
-	}
-	return false, fmt.Errorf("old service doesn't have last applied config ")
-}
-
-// VMServiceEqual compares the new vm service's old vm service's last applied config
-func VMServiceEqual(newVMService, oldVMService *vmoperatorv1alpha1.VirtualMachineService) (bool, error) {
-	if lastAppliedConfig, ok := oldVMService.GetAnnotations()[corev1.LastAppliedConfigAnnotation]; ok {
-		return VMServiceCompareToLastApplied(newVMService, lastAppliedConfig)
-	}
-	return false, fmt.Errorf("old vm service doesn't have last applied config ")
-}
-
-// VMServiceCompareToLastApplied compares the new vm service's spec with last applied vm service config
-func VMServiceCompareToLastApplied(newVMService *vmoperatorv1alpha1.VirtualMachineService, lastAppliedAnnotation string) (bool, error) {
-	oldVMService := vmoperatorv1alpha1.VirtualMachineService{}
-	err := json.Unmarshal([]byte(lastAppliedAnnotation), &oldVMService)
-	if err != nil {
-		return false, err
-	}
-	return apiequality.Semantic.DeepEqual(oldVMService.Spec, newVMService.Spec), nil
-}
 
 var (
 	virtualMachineServiceKind       = reflect.TypeOf(vmoperatorv1alpha1.VirtualMachineService{}).Name()

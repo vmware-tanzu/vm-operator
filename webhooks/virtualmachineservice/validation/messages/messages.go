c3f27d09f0c8daec9453bab57701e7a09f513e0a
Sreyas Natarajan <sreyasn@vmware.com>
2020-11-16 13:04:28 -0800

    Append messages from validation.IsDNS1123Label() to make the fix obvious
    Testing:
    make test
    
    "msg"="validation denied"  "code"=422 "reason"="metadata.name is invalid: MyVMService : a DNS-1123 label must consist of lower case alphanumeric characters or '-', and must start and end with an alphanumeric character (e.g. 'my-name',  or '123-abc', regex used for validation is '[a-z0-9]([-a-z0-9]*[a-z0-9])?')"

--- webhooks/virtualmachineservice/validation/messages/messages.go
+++ webhooks/virtualmachineservice/validation/messages/messages.go
@@ -8,5 +8,5 @@ const (
 	TypeNotSpecified                  = "spec.type must be specified"
 	PortsNotSpecified                 = "spec.ports must be specified"
 	SelectorNotSpecified              = "spec.selector must be specified"
-	NameNotDNSComplaint               = "metadata.name %s does not conform to the kubernetes DNS_LABEL rules"
+	NameNotDNSComplaint               = "metadata.name is invalid: %s : %s"
 )

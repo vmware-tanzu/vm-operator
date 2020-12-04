e95971f1d6ec26fa55825242708d02c3159cd098
Varun Jain <jainvarun@vmware.com>
2020-12-04 09:32:34 -0800

    Allow creation of VirtualNetworkInterface without referencing a VirtualNetwork
    To create standalone VMs using VMSVC, we need to allow developers to
    create VM resources that specify a VirtualNetworkInterface(VIF) without referencing a
    VirtualNetwork. i.e., we should have the ability to connect such VMs
    to a default network. NCP supports this, but there
    are validation webhooks in place in VMOP to disallow specifying VIFs
    without specifying a VirtualNetwork. This patch removes those validations for NSX-T.
    
    Note that this patch does not modify the VDS code path since there is
    no concept of default network (yet) in NetOp.
    
    Testing Done:
    - make test
    - NSX Setup: Validated that VM comes up and gets connected to a default network
    - VDS Setup: Validated that validation webhook prevents VM creation with empty NetworkName

--- webhooks/virtualmachine/validation/virtualmachine_validator_unit_test.go
+++ webhooks/virtualmachine/validation/virtualmachine_validator_unit_test.go
@@ -19,6 +19,7 @@ import (
 
 	vmopv1 "github.com/vmware-tanzu/vm-operator-api/api/v1alpha1"
 
+	"github.com/vmware-tanzu/vm-operator/pkg/vmprovider/providers/vsphere"
 	"github.com/vmware-tanzu/vm-operator/test/builder"
 	"github.com/vmware-tanzu/vm-operator/webhooks/virtualmachine/validation/messages"
 )
@@ -101,6 +102,7 @@ func unitTestsValidateCreate() {
 		}
 		if args.invalidNetworkName {
 			ctx.vm.Spec.NetworkInterfaces[0].NetworkName = ""
+			ctx.vm.Spec.NetworkInterfaces[0].NetworkType = vsphere.VdsNetworkType
 		}
 		if args.invalidNetworkType {
 			ctx.vm.Spec.NetworkInterfaces[0].NetworkType = "bogusNetworkType"
@@ -176,10 +178,10 @@ func unitTestsValidateCreate() {
 		Entry("should deny invalid class name", createArgs{invalidClassName: true}, false, messages.ClassNotSpecified, nil),
 		Entry("should deny invalid image name", createArgs{invalidImageName: true}, false, messages.ImageNotSpecified, nil),
 		Entry("should not work for image with an invalid osType", createArgs{invalidGuestOSType: true}, false, fmt.Sprintf(messages.GuestOSCustomizationNotSupported, builder.DummyOSType, builder.DummyImageName), nil),
-		Entry("should deny invalid network name", createArgs{invalidNetworkName: true}, false, fmt.Sprintf(messages.NetworkNameNotSpecifiedFmt, 0), nil),
+		Entry("should deny invalid network name for VDS network type", createArgs{invalidNetworkName: true}, false, fmt.Sprintf(messages.NetworkNameNotSpecifiedFmt, 0), nil),
 		Entry("should deny invalid network type", createArgs{invalidNetworkType: true}, false, fmt.Sprintf(messages.NetworkTypeNotSupportedFmt, 0), nil),
 		Entry("should deny connection of multiple network interfaces of a VM to the same network", createArgs{multipleNetIfToSameNetwork: true},
-			false, fmt.Sprintf(messages.MultipleNetworkInterfacesNotSupportedFmt, builder.DummyNetworkName), nil),
+			false, fmt.Sprintf(messages.MultipleNetworkInterfacesNotSupportedFmt, 1), nil),
 		Entry("should deny invalid volume name", createArgs{invalidVolumeName: true}, false, fmt.Sprintf(messages.VolumeNameNotSpecifiedFmt, 0), nil),
 		Entry("should deny duplicated volume names", createArgs{dupVolumeName: true}, false, fmt.Sprintf(messages.VolumeNameDuplicateFmt, 1), nil),
 		Entry("should deny invalid volume source spec", createArgs{invalidVolumeSource: true}, false, fmt.Sprintf(messages.VolumeNotSpecifiedFmt, 0, 0), nil),

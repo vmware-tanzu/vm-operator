2046ce1da2fd0c9ba759637558db841223a65720
Yi Jiang <yijiang@vmware.com>
2020-12-07 16:06:02 -0800

    Use a fake probe when running probe worker unit tests.
    This change removes the mock_probe file generated by mock-gen from probe interface.
    Add a fake probe for probe worker unit tests.
    
    Test done:
      make test

--- pkg/prober/worker/readiness_worker_test.go
+++ pkg/prober/worker/readiness_worker_test.go
@@ -1,11 +1,10 @@
 // Copyright (c) 2020 VMware, Inc. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-//go:generate mockgen -destination=../../../mocks/mock_probe.go -package=mocks github.com/vmware-tanzu/vm-operator/pkg/prober/probe Probe
-
 package worker
 
 import (
+	goctx "context"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
@@ -13,18 +12,16 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	clientgorecord "k8s.io/client-go/tools/record"
 	"k8s.io/client-go/util/workqueue"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	"github.com/golang/mock/gomock"
 	vmopv1alpha1 "github.com/vmware-tanzu/vm-operator-api/api/v1alpha1"
 
-	"github.com/vmware-tanzu/vm-operator/mocks"
 	"github.com/vmware-tanzu/vm-operator/pkg/conditions"
 	"github.com/vmware-tanzu/vm-operator/pkg/prober/context"
+	fakeprobe "github.com/vmware-tanzu/vm-operator/pkg/prober/fake/probe"
 	"github.com/vmware-tanzu/vm-operator/pkg/prober/probe"
 	"github.com/vmware-tanzu/vm-operator/pkg/record"
 	"github.com/vmware-tanzu/vm-operator/test/builder"
@@ -33,16 +30,15 @@ import (
 var _ = Describe("VirtualMachine readiness probes", func() {
 	var (
 		testWorker Worker
-		vm         *vmopv1alpha1.VirtualMachine
-		vmKey      types.NamespacedName
-		ctx        *context.ProbeContext
+
+		vm    *vmopv1alpha1.VirtualMachine
+		vmKey client.ObjectKey
+		ctx   *context.ProbeContext
 
 		fakeClient   client.Client
 		fakeRecorder record.Recorder
 		fakeEvents   chan string
-
-		mockCtrl  *gomock.Controller
-		mockProbe *mocks.MockProbe
+		fakeProbe    *fakeprobe.FakeProbe
 	)
 
 	BeforeEach(func() {
@@ -63,12 +59,11 @@ var _ = Describe("VirtualMachine readiness probes", func() {
 		fakeRecorder = record.New(eventRecorder)
 		fakeEvents = eventRecorder.Events
 
-		mockCtrl = gomock.NewController(GinkgoT())
-		mockProbe = mocks.NewMockProbe(mockCtrl)
 		queue := workqueue.NewNamedDelayingQueue("test")
 		prober := probe.NewProber()
+		fakeProbe = fakeprobe.NewFakeProbe().(*fakeprobe.FakeProbe)
+		prober.TCPProbe = fakeProbe
 		testWorker = NewReadinessWorker(queue, prober, fakeClient, fakeRecorder)
-		prober.TCPProbe = mockProbe
 	})
 
 	checkReadyCondition := func(c client.Client, objKey client.ObjectKey, expectedCondition corev1.ConditionStatus) {
@@ -85,24 +80,21 @@ var _ = Describe("VirtualMachine readiness probes", func() {
 
 		BeforeEach(func() {
 			vm.Spec.ReadinessProbe = getVirtualMachineReadinessTCPProbe(10001)
-			Expect(fakeClient.Create(ctx, vm)).To(Succeed())
-			Expect(fakeClient.Get(ctx, vmKey, vm)).Should(Succeed())
+			Expect(fakeClient.Create(goctx.Background(), vm)).Should(Succeed())
+			Expect(fakeClient.Get(goctx.Background(), vmKey, vm)).Should(Succeed())
 			var err error
 			ctx, err = testWorker.CreateProbeContext(vm)
 			Expect(err).ShouldNot(HaveOccurred())
 			oldStatus = vm.Status
 		})
 
-		AfterEach(func() {
-			Expect(fakeClient.Delete(ctx, vm)).To(Succeed())
-		})
-
 		When("new ReadyCondition is in a transition", func() {
 			It("Should update ReadyCondition when probe succeeds", func() {
-				mockProbe.EXPECT().Probe(gomock.Any()).Return(probe.Success, nil)
+				fakeProbe.ProbeFn = func(ctx *context.ProbeContext) (probe.Result, error) {
+					return probe.Success, nil
+				}
 
-				err := testWorker.DoProbe(ctx)
-				Expect(err).ShouldNot(HaveOccurred())
+				Expect(testWorker.DoProbe(ctx)).Should(Succeed())
 
 				By("Should set ReadyCondition status as true", func() {
 					checkReadyCondition(fakeClient, vmKey, corev1.ConditionTrue)
@@ -116,10 +108,11 @@ var _ = Describe("VirtualMachine readiness probes", func() {
 			})
 
 			It("Should update ReadyCondition when probe fails", func() {
-				mockProbe.EXPECT().Probe(gomock.Any()).Return(probe.Failure, nil)
+				fakeProbe.ProbeFn = func(ctx *context.ProbeContext) (probe.Result, error) {
+					return probe.Failure, nil
+				}
 
-				err := testWorker.DoProbe(ctx)
-				Expect(err).ShouldNot(HaveOccurred())
+				Expect(testWorker.DoProbe(ctx)).Should(Succeed())
 
 				By("Should set ReadyCondition value as false", func() {
 					checkReadyCondition(fakeClient, vmKey, corev1.ConditionFalse)
@@ -140,10 +133,11 @@ var _ = Describe("VirtualMachine readiness probes", func() {
 					Expect(fakeClient.Get(ctx, vmKey, vm)).Should(Succeed())
 					oldStatus = vm.Status
 
-					mockProbe.EXPECT().Probe(gomock.Any()).Return(probe.Success, nil)
+					fakeProbe.ProbeFn = func(ctx *context.ProbeContext) (probe.Result, error) {
+						return probe.Success, nil
+					}
 
-					err := testWorker.DoProbe(ctx)
-					Expect(err).ShouldNot(HaveOccurred())
+					Expect(testWorker.DoProbe(ctx)).Should(Succeed())
 
 					By("Conditions in VM status should not be updated", func() {
 						Expect(fakeClient.Get(ctx, vmKey, vm)).Should(Succeed())

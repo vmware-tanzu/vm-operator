720ec8e7f5091a7a0e93d972c5750f7191829cbe
Bryan Venteicher <bventeicher@vmware.com>
2021-01-06 17:35:09 -0600

    Generate v1 CustomResourceDefinition
    By generating v1 instead of v1beta1, kubectl explain now works.
    
    preserveUnknownFields is needed for explain to work when upgrading
    from CRD v1beta1 to v1 but controller-gen has a bug [1] that causes
    it not be included in the generated CRD yaml. Use kustomize to add
    it the CRDs.
    
    Keep the webhooks at their prior version until we decide to or must
    update them. The new controller-gen defaults to newer version.
    
    [1] - https://github.com/kubernetes-sigs/controller-tools/issues/476

--- hack/tools/tools.go
+++ hack/tools/tools.go
@@ -29,5 +29,4 @@ import (
 	_ "k8s.io/code-generator"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kustomize/kustomize/v3"
-	_ "sigs.k8s.io/testing_frameworks/integration"
 )

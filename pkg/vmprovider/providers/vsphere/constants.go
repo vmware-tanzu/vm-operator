4b2aa5c4cf668dd8c7bc36b936a35f5ab29aa1a2
Bryan Venteicher <bventeicher@vmware.com>
2021-02-03 17:04:40 -0600

    Support incremental VM Reconfigure
    Compare the current state of the VM from VC with the desired state
    specified by k8s, and reconfigure the VM to move towards the desired
    state.
    
    Start to split apart the big session.go file into various files.
    
    Still needs to be smarter around Network reconfigure by comparing the
    NIC backings.

---
+++ pkg/vmprovider/providers/vsphere/constants.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2021 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package vsphere
+
+const (
+	VCVMAnnotation string = "Virtual Machine managed by the vSphere Virtual Machine service"
+)

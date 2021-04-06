4b766462164c5967488e71e5af3670804dc5acfe
Bryan Venteicher <bventeicher@vmware.com>
2021-04-06 11:39:07 -0500

    Run webhooks and controller integration tests in same command
    All the controller tests have been converted over so these all use the
    same new env test suite.
    
    Remove controllers from the old-style tests since including them there
    meant we were running the unit tests again.

--- hack/test-integration.sh
+++ hack/test-integration.sh
@@ -16,7 +16,6 @@ function join_packages { local IFS=","; echo "$*"; }
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 COVERAGE_FILE="${1-}"
-WEBHOOK_COVERAGE_FILE="$(pwd)/webhook.cover.out"
 INTEGRATION_COVERAGE_FILE="$(pwd)/int.cover.out"
 ENVIRONMENT_COVERAGE_FILE="$(pwd)/env.cover.out"
 
@@ -30,24 +29,18 @@ packages=(
 )
 COV_OPTS=$(join_packages "${packages[@]}")
 
-WEB_GOFLAGS=()
 INT_GOFLAGS=()
 ENV_GOFLAGS=()
 
 # The first argument is the name of the coverage file to use.
 if [[ -n ${COVERAGE_FILE} ]]; then
-    WEB_GOFLAGS=("-coverprofile=${WEBHOOK_COVERAGE_FILE}" "-coverpkg=${COV_OPTS}")
     INT_GOFLAGS=("-coverprofile=${INTEGRATION_COVERAGE_FILE}" "-coverpkg=${COV_OPTS}")
     ENV_GOFLAGS=("-coverprofile=${ENVIRONMENT_COVERAGE_FILE}" "-coverpkg=${COV_OPTS}")
 fi
 
-# Run the webhook tests.
-# go test: -race requires cgo
-CGO_ENABLED=1 go test -v -race -p 1 -count=1 "${WEB_GOFLAGS[@]}" ./webhooks/... -- -enable-integration-tests -enable-unit-tests=false
-
 # Run the package integration tests.
 # go test: -race requires cgo
-CGO_ENABLED=1 go test -v -race -p 1 -count=1 "${INT_GOFLAGS[@]}" -tags=integration ./controllers/... ./pkg/...
+CGO_ENABLED=1 go test -v -race -p 1 -count=1 "${INT_GOFLAGS[@]}" -tags=integration ./pkg/...
 
 # Run integration tests with new framework
 # go test: -race requires cgo
@@ -62,11 +55,12 @@ CGO_ENABLED=1 go test -v -race -p 1 -count=1 "${ENV_GOFLAGS[@]}" \
            ./controllers/virtualmachineservice \
            ./controllers/virtualmachinesetresourcepolicy \
            ./controllers/volume \
+	   ./webhooks/... \
            -- -enable-integration-tests -enable-unit-tests=false
 
 # Merge the coverage files.
 if [[ -n ${COVERAGE_FILE} ]]; then
-    touch "${ENVIRONMENT_COVERAGE_FILE}" "${INTEGRATION_COVERAGE_FILE}" "${WEBHOOK_COVERAGE_FILE}"
-    gocovmerge "${ENVIRONMENT_COVERAGE_FILE}" "${INTEGRATION_COVERAGE_FILE}" "${WEBHOOK_COVERAGE_FILE}" > "${COVERAGE_FILE}"
-    rm -f "${ENVIRONMENT_COVERAGE_FILE}" "${INTEGRATION_COVERAGE_FILE}" "${WEBHOOK_COVERAGE_FILE}"
+    touch "${ENVIRONMENT_COVERAGE_FILE}" "${INTEGRATION_COVERAGE_FILE}"
+    gocovmerge "${ENVIRONMENT_COVERAGE_FILE}" "${INTEGRATION_COVERAGE_FILE}" > "${COVERAGE_FILE}"
+    rm -f "${ENVIRONMENT_COVERAGE_FILE}" "${INTEGRATION_COVERAGE_FILE}"
 fi

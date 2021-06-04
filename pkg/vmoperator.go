4219e51908d4373cfb84cb9e8e0cb7e505a10852
Bryan Venteicher <bventeicher@vmware.com>
2021-06-04 16:16:16 -0500

    VirtualMachineService controller cleanup Part 4
    Clean up most of the reconciliation logic so that it is now similar
    to the other controllers. Fix various bugs around Service and Endpoint
    handling meaning that we'd only update them once, regardless if the
    owning VirtualMachineService changed.
    
    Improve the validation webhook to (hopefully) ensure that the
    VirtualMachineService will result in a valid Service.
    
    Rework most of the test:
     - Don't unit test internal methods. Only export ReconcileNormal and
       ReconcileDelete like we do in other controllers
     - Add a lot of missing assertions
     - Add readiness/probe unit tests. Remove the disabled integration
       tests.

--- pkg/vmoperator.go
+++ pkg/vmoperator.go
@@ -3,16 +3,9 @@
 
 package pkg
 
-import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
-
 const (
 	// Base FQDN for the vmoperator
 	VmOperatorKey string = "vmoperator.vmware.com"
-
-	// VM Operator version key
-	VmOperatorVersionKey string = "vmoperator.vmware.com/version"
 
 	// Annotation Key for VM provider
 	VmOperatorVmProviderKey string = "vmoperator.vmware.com/vmprovider"
@@ -23,15 +16,3 @@ const (
 	// Annotation key for clusterModule group name information at vmoperator
 	ClusterModuleNameKey string = "vsphere-cluster-module-group"
 )
-
-func AddAnnotations(objectMeta *metav1.ObjectMeta) {
-	annotations := objectMeta.GetAnnotations()
-	if annotations == nil {
-		annotations = make(map[string]string)
-	}
-
-	// TODO: Make this version dynamic
-	annotations[VmOperatorVersionKey] = "v1"
-
-	objectMeta.SetAnnotations(annotations)
-}
--- webhooks/virtualmachineservice/validation/virtualmachineservice_validator_unit_test.go
+++ webhooks/virtualmachineservice/validation/virtualmachineservice_validator_unit_test.go
@@ -7,6 +7,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -55,10 +56,15 @@ func unitTestsValidateCreate() {
 	)
 
 	type createArgs struct {
-		invalidDNSName  bool
-		invalidType     bool
-		invalidPorts    bool
-		invalidSelector bool
+		invalidDNSName        bool
+		emptyType             bool
+		invalidType           bool
+		invalidPorts          bool
+		invalidSelector       bool
+		invalidClusterIP      bool
+		invalidLoadBalancerIP bool
+		invalidLBSourceRanges bool
+		invalidExternalName   bool
 	}
 
 	validateCreate := func(args createArgs, expectedAllowed bool, expectedReason string, expectedErr error) {
@@ -68,14 +74,32 @@ func unitTestsValidateCreate() {
 			// Name that doesn't conform to DNS Label format "[a-z0-9]([-a-z0-9]*[a-z0-9])?"
 			ctx.vmService.Name = "MyVMService"
 		}
-		if args.invalidType {
+		if args.emptyType {
 			ctx.vmService.Spec.Type = ""
+		}
+		if args.invalidType {
+			ctx.vmService.Spec.Type = "InvalidLB"
 		}
 		if args.invalidPorts {
 			ctx.vmService.Spec.Ports = nil
 		}
 		if args.invalidSelector {
-			ctx.vmService.Spec.Selector = nil
+			ctx.vmService.Spec.Selector = map[string]string{"THIS_NOT_VALID!": "foo"}
+		}
+		if args.invalidClusterIP {
+			ctx.vmService.Spec.ClusterIP = "100.1000.1.1"
+		}
+		if args.invalidLoadBalancerIP {
+			ctx.vmService.Spec.Type = vmopv1.VirtualMachineServiceTypeLoadBalancer
+			ctx.vmService.Spec.LoadBalancerIP = "500.1.1.1"
+		}
+		if args.invalidLBSourceRanges {
+			ctx.vmService.Spec.Type = vmopv1.VirtualMachineServiceTypeLoadBalancer
+			ctx.vmService.Spec.LoadBalancerSourceRanges = []string{"10.1.1.1/42"}
+		}
+		if args.invalidExternalName {
+			ctx.vmService.Spec.Type = vmopv1.VirtualMachineServiceTypeExternalName
+			ctx.vmService.Spec.ExternalName = "InValid!"
 		}
 
 		ctx.WebhookRequestContext.Obj, err = builder.ToUnstructured(ctx.vmService)
@@ -100,10 +124,105 @@ func unitTestsValidateCreate() {
 
 	DescribeTable("create table", validateCreate,
 		Entry("should allow valid", createArgs{}, true, nil, nil),
-		Entry("should deny invalid type", createArgs{invalidType: true}, false, "spec.type must be specified", nil),
-		Entry("should deny invalid ports", createArgs{invalidPorts: true}, false, "spec.ports must be specified", nil),
-		Entry("should deny invalid selector", createArgs{invalidSelector: true}, false, "spec.selector must be specified", nil),
-		Entry("should deny invalid name", createArgs{invalidDNSName: true}, false, "metadata.name is invalid", nil),
+		Entry("should deny invalid name", createArgs{invalidDNSName: true}, false, "metadata.name: Invalid value: \"MyVMService\": a DNS-1123 label must consist of lower case alphanumeric", nil),
+		Entry("should deny no type", createArgs{emptyType: true}, false, "spec.type: Required value", nil),
+		Entry("should deny invalid type", createArgs{invalidType: true}, false, "spec.type: Unsupported value: \"InvalidLB\":", nil),
+		Entry("should deny invalid ports", createArgs{invalidPorts: true}, false, "spec.ports: Required value", nil),
+		Entry("should deny invalid selector", createArgs{invalidSelector: true}, false, "spec.selector: Invalid value: \"THIS_NOT_VALID!\": name part must consist of alphanumeric characters", nil),
+		Entry("should deny invalid ClusterIP", createArgs{invalidClusterIP: true}, false, "spec.clusterIP: Invalid value: \"100.1000.1.1\": must be a valid IP address", nil),
+		Entry("should deny invalid LoadBalancerIP", createArgs{invalidLoadBalancerIP: true}, false, "spec.loadBalancerIP: Invalid value: \"500.1.1.1\": must be a valid IP address", nil),
+		Entry("should deny invalid LoadBalancerSourceRanges", createArgs{invalidLBSourceRanges: true}, false, "spec.loadBalancerSourceRanges: Invalid value: \"[10.1.1.1/42]", nil),
+		Entry("should deny invalid ExternalName", createArgs{invalidExternalName: true}, false, "spec.externalName: Invalid value: \"InValid!\": a DNS-1123 subdomain", nil),
+	)
+
+	validatePortCreate := func(expectedReason string, ports []vmopv1.VirtualMachineServicePort) {
+		var err error
+
+		ctx.vmService.Spec.Ports = ports
+
+		ctx.WebhookRequestContext.Obj, err = builder.ToUnstructured(ctx.vmService)
+		Expect(err).ToNot(HaveOccurred())
+
+		response := ctx.ValidateCreate(&ctx.WebhookRequestContext)
+		if expectedReason != "" {
+			Expect(response.Allowed).To(BeFalse())
+			Expect(string(response.Result.Reason)).To(ContainSubstring(expectedReason))
+		} else {
+			Expect(response.Allowed).To(BeTrue())
+		}
+	}
+
+	DescribeTable("create service ports", validatePortCreate,
+		Entry("should allow valid port", "",
+			[]vmopv1.VirtualMachineServicePort{
+				{
+					Name:       "http",
+					Protocol:   "TCP",
+					Port:       80,
+					TargetPort: 8080,
+				},
+			},
+		),
+		Entry("should deny invalid name", "spec.ports[0].name: Invalid value: \"INVALID\"",
+			[]vmopv1.VirtualMachineServicePort{
+				{
+					Name: "INVALID",
+				},
+			},
+		),
+		Entry("should deny invalid port", "spec.ports[0].port: Invalid value: 100000:",
+			[]vmopv1.VirtualMachineServicePort{
+				{
+					Port: 100000,
+				},
+			},
+		),
+		Entry("should deny invalid protocol", "spec.ports[0].protocol: Unsupported value: \"INVALID\"",
+			[]vmopv1.VirtualMachineServicePort{
+				{
+					Protocol: "INVALID",
+				},
+			},
+		),
+		Entry("should deny invalid target port", "spec.ports[0].targetPort: Invalid value: 200000:",
+			[]vmopv1.VirtualMachineServicePort{
+				{
+					TargetPort: 200000,
+				},
+			},
+		),
+		Entry("should deny duplicate names", "spec.ports[1].name: Duplicate value: \"port1\"",
+			[]vmopv1.VirtualMachineServicePort{
+				{
+					Name:       "port1",
+					Protocol:   "TCP",
+					Port:       80,
+					TargetPort: 8080,
+				},
+				{
+					Name:       "port1",
+					Protocol:   "TCP",
+					Port:       433,
+					TargetPort: 6443,
+				},
+			},
+		),
+		Entry("should deny duplicate protocol/port", "spec.ports[1]: Duplicate value: v1alpha1.VirtualMachineServicePort",
+			[]vmopv1.VirtualMachineServicePort{
+				{
+					Name:       "port1",
+					Protocol:   "TCP",
+					Port:       80,
+					TargetPort: 8080,
+				},
+				{
+					Name:       "port2",
+					Protocol:   "TCP",
+					Port:       80,
+					TargetPort: 8080,
+				},
+			},
+		),
 	)
 }
 
@@ -114,10 +233,19 @@ func unitTestsValidateUpdate() {
 	)
 
 	type updateArgs struct {
+		updateType      bool
+		updateClusterIP bool
 	}
 
 	validateUpdate := func(args updateArgs, expectedAllowed bool, expectedReason string, expectedErr error) {
 		var err error
+
+		if args.updateType {
+			ctx.vmService.Spec.Type = vmopv1.VirtualMachineServiceTypeClusterIP
+		}
+		if args.updateClusterIP {
+			ctx.vmService.Spec.ClusterIP = "9.9.9.9"
+		}
 
 		ctx.WebhookRequestContext.Obj, err = builder.ToUnstructured(ctx.vmService)
 		Expect(err).ToNot(HaveOccurred())
@@ -141,6 +269,8 @@ func unitTestsValidateUpdate() {
 
 	DescribeTable("update table", validateUpdate,
 		Entry("should allow", updateArgs{}, true, nil, nil),
+		Entry("should deny Type change", updateArgs{updateType: true}, false, "spec.type: Forbidden: field is immutable", nil),
+		Entry("should deny ClusterIP change", updateArgs{updateClusterIP: true}, false, "spec.clusterIP: Forbidden: field is immutable", nil),
 	)
 
 	When("the update is performed while object deletion", func() {

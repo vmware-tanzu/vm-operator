e72405e2a4907150b2bb7d31acd30486ef29b73e
Bryan Venteicher <bventeicher@vmware.com>
2021-09-02 12:13:23 -0500

    Move content library related tests into contentlibrary package

---
+++ pkg/vmprovider/providers/vsphere/contentlibrary/content_library_suite_test.go
@@ -0,0 +1,18 @@
+// +build !integration
+
+// Copyright (c) 2021 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package contentlibrary_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestContentLibrary(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "vSphere Provider Content Library Suite")
+}

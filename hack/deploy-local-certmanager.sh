0175455af0f79960f6a276b1c93d5756fb5c6a2f
Sreyas Natarajan <sreyasn@vmware.com>
2021-10-07 11:38:49 -0700

    Add hack script and Makefile changes for upstream unit/integration testing

---
+++ hack/deploy-local-certmanager.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Deploy cert-manager to the local cluster
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -x
+
+# Exit with a non-zero exit code and an error message if
+# the CERT_MANAGER_URL is not set.
+CERT_MANAGER_URL="${CERT_MANAGER_URL:?}"
+
+mkdir -p artifacts
+
+./hack/tools/bin/kustomize build \
+  --load_restrictor none \
+  "${CERT_MANAGER_URL}" \
+  >artifacts/cert-manager.yaml
+
+kubectl apply -f artifacts/cert-manager.yaml

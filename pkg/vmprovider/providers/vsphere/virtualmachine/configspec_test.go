vmware-tanzu/vm-operator

commit 44cd693d4971f9b2b7afc4ad5588f9847ebfb1e3

Only add PCI passthrough devices from ConfigSpec if FSS is enabled
After VM Class as Config is enabled, a ConfigSpec is always expected
to (mostly) accurately represent the desired set of devices.  This is
done in part, by duplicating the vGPU and DirectPath devices from VM
Class spec into the ConfigSpec.

Currently, we look at ConfigSpec /and/ the VM Class `spec.devices`
field to determine what devices do we need to add to the VM.  This
leads to the vGPU and DirectPath devices being added twice to the
VM (since they are present in VM Class `spec.devices` field and are
duplicated in ConfigSpec).

This change fixes this behavior by only looking at the ConfigSpec when
the FSS is enabled.

Testing Done:
- [X] Existing Unit and Integration tests
- [X] Added new tests
- [ ] e2e tests

pkg/vmprovider/providers/vsphere/session/session_vm_update.go
@@ -511,22 +511,17 @@ func (s *Session) prePowerOnVMConfigSpec(
 	}
 	configSpec.DeviceChange = append(configSpec.DeviceChange, ethCardDeviceChanges...)
 
-	var pciPassthruFromConfigSpec []*vimTypes.VirtualPCIPassthrough
+	var expectedPCIDevices []vimTypes.BaseVirtualDevice
 	if configSpecDevs := util.DevicesFromConfigSpec(updateArgs.ConfigSpec); len(configSpecDevs) > 0 {
 		if lib.IsVMClassAsConfigFSSDaynDateEnabled() {
-			pciPassthruFromConfigSpec = util.SelectVirtualPCIPassthrough(configSpecDevs)
+			pciPassthruFromConfigSpec := util.SelectVirtualPCIPassthrough(configSpecDevs)
+			expectedPCIDevices = virtualmachine.CreatePCIDevicesFromConfigSpec(pciPassthruFromConfigSpec)
 		} else {
-			// Only support vGPUs as passthrough devices from the class config spec
-			// until VM_Class_as_Config_DaynDate FSS is enabled
-			pciPassthruFromConfigSpec = util.SelectVGPUs(configSpecDevs)
+			expectedPCIDevices = virtualmachine.CreatePCIDevicesFromVMClass(updateArgs.VMClass.Spec.Hardware.Devices)
 		}
 	}
 
-	expectedPciDevices := virtualmachine.CreatePCIDevices(
-		updateArgs.VMClass.Spec.Hardware.Devices,
-		pciPassthruFromConfigSpec)
-
-	pciDeviceChanges, err := UpdatePCIDeviceChanges(expectedPciDevices, currentPciDevices)
+	pciDeviceChanges, err := UpdatePCIDeviceChanges(expectedPCIDevices, currentPciDevices)
 	if err != nil {
 		return nil, err
 	}

pkg/vmprovider/providers/vsphere/virtualmachine/configspec.go
@@ -130,7 +130,7 @@ func CreateConfigSpecForPlacement(
 		},
 	})
 
-	for _, dev := range CreatePCIDevices(vmClassSpec.Hardware.Devices, nil) {
+	for _, dev := range CreatePCIDevicesFromVMClass(vmClassSpec.Hardware.Devices) {
 		configSpec.DeviceChange = append(configSpec.DeviceChange, &vimtypes.VirtualDeviceConfigSpec{
 			Operation: vimtypes.VirtualDeviceConfigSpecOperationAdd,
 			Device:    dev,

pkg/vmprovider/providers/vsphere/vmprovider_vm_test.go
@@ -235,9 +235,9 @@ func vmTests() {
 					var o mo.VirtualMachine
 					Expect(vcVM.Properties(ctx, vcVM.Reference(), nil, &o)).To(Succeed())
 					Expect(o.Summary.Config.NumCpu).To(BeEquivalentTo(vmClass.Spec.Hardware.Cpus))
-					Expect(o.Summary.Config.NumCpu).To(Not(BeEquivalentTo(configSpec.NumCPUs)))
+					Expect(o.Summary.Config.NumCpu).ToNot(BeEquivalentTo(configSpec.NumCPUs))
 					Expect(o.Summary.Config.MemorySizeMB).To(BeEquivalentTo(vmClass.Spec.Hardware.Memory.Value() / 1024 / 1024))
-					Expect(o.Summary.Config.MemorySizeMB).To(Not(BeEquivalentTo(configSpec.MemoryMB)))
+					Expect(o.Summary.Config.MemorySizeMB).ToNot(BeEquivalentTo(configSpec.MemoryMB))
 				})
 			})
 
@@ -271,7 +271,7 @@ func vmTests() {
 					reservation := o.Config.CpuAllocation.Reservation
 					vmClassCPURes := virtualmachine.CPUQuantityToMhz(vmClass.Spec.Policies.Resources.Requests.Memory, freq)
 					Expect(reservation).ToNot(BeNil())
-					Expect(reservation).To(Not(BeEquivalentTo(vmClassCPURes)))
+					Expect(reservation).ToNot(BeEquivalentTo(vmClassCPURes))
 					Expect(reservation).To(Equal(configSpec.CpuAllocation.Reservation))
 				})
 			})
@@ -303,7 +303,7 @@ func vmTests() {
 					reservation := o.Config.MemoryAllocation.Reservation
 					vmClassMemoryRes := virtualmachine.MemoryQuantityToMb(vmClass.Spec.Policies.Resources.Requests.Memory)
 					Expect(reservation).ToNot(BeNil())
-					Expect(*reservation).To(Not(Equal(vmClassMemoryRes)))
+					Expect(*reservation).ToNot(Equal(vmClassMemoryRes))
 					Expect(reservation).To(Equal(configSpec.MemoryAllocation.Reservation))
 				})
 			})
@@ -393,6 +393,80 @@ func vmTests() {
 					Expect(vcVM.Properties(ctx, vcVM.Reference(), nil, &o)).To(Succeed())
 					Expect(o.Summary.Config.NumCpu).To(BeEquivalentTo(vmClass.Spec.Hardware.Cpus))
 					Expect(o.Summary.Config.MemorySizeMB).To(BeEquivalentTo(vmClass.Spec.Hardware.Memory.Value() / 1024 / 1024))
+				})
+			})
+
+			Context("VM Class Spec and ConfigSpec both contain GPU and DirectPath devices", func() {
+				BeforeEach(func() {
+					vmClass.Spec.Hardware.Devices = vmopv1alpha1.VirtualDevices{
+						VGPUDevices: []vmopv1alpha1.VGPUDevice{
+							{
+								ProfileName: "profile-from-class",
+							},
+						},
+						DynamicDirectPathIODevices: []vmopv1alpha1.DynamicDirectPathIODevice{
+							{
+								VendorID:    50,
+								DeviceID:    51,
+								CustomLabel: "label-from-class",
+							},
+						},
+					}
+
+					// Create the ConfigSpec with a GPU and a DDPIO device.
+					configSpec = &types.VirtualMachineConfigSpec{
+						Name: "dummy-VM",
+						DeviceChange: []types.BaseVirtualDeviceConfigSpec{
+							&types.VirtualDeviceConfigSpec{
+								Operation: types.VirtualDeviceConfigSpecOperationAdd,
+								Device: &types.VirtualPCIPassthrough{
+									VirtualDevice: types.VirtualDevice{
+										Backing: &types.VirtualPCIPassthroughVmiopBackingInfo{
+											Vgpu: "profile-from-configspec",
+										},
+									},
+								},
+							},
+							&types.VirtualDeviceConfigSpec{
+								Operation: types.VirtualDeviceConfigSpecOperationAdd,
+								Device: &types.VirtualPCIPassthrough{
+									VirtualDevice: types.VirtualDevice{
+										Backing: &types.VirtualPCIPassthroughDynamicBackingInfo{
+											AllowedDevice: []types.VirtualPCIPassthroughAllowedDevice{
+												{
+													VendorId: 52,
+													DeviceId: 53,
+												},
+											},
+											CustomLabel: "label-from-configspec",
+										},
+									},
+								},
+							},
+						},
+					}
+				})
+
+				It("GPU and DirectPath devices from VM Class Spec.Devices are ignored", func() {
+					var o mo.VirtualMachine
+					Expect(vcVM.Properties(ctx, vcVM.Reference(), nil, &o)).To(Succeed())
+
+					devList := object.VirtualDeviceList(o.Config.Hardware.Device)
+					p := devList.SelectByType(&types.VirtualPCIPassthrough{})
+					Expect(p).To(HaveLen(2))
+
+					pciDev1 := p[0].GetVirtualDevice()
+					pciBacking1, ok1 := pciDev1.Backing.(*types.VirtualPCIPassthroughVmiopBackingInfo)
+					Expect(ok1).Should(BeTrue())
+					Expect(pciBacking1.Vgpu).To(Equal("profile-from-configspec"))
+
+					pciDev2 := p[1].GetVirtualDevice()
+					pciBacking2, ok2 := pciDev2.Backing.(*types.VirtualPCIPassthroughDynamicBackingInfo)
+					Expect(ok2).Should(BeTrue())
+					Expect(pciBacking2.AllowedDevice).To(HaveLen(1))
+					Expect(pciBacking2.AllowedDevice[0].VendorId).To(Equal(int32(52)))
+					Expect(pciBacking2.AllowedDevice[0].DeviceId).To(Equal(int32(53)))
+					Expect(pciBacking2.CustomLabel).To(Equal("label-from-configspec"))
 				})
 			})
 

pkg/util/devices.go
@@ -125,17 +125,6 @@ func IsDeviceVGPU(dev vimTypes.BaseVirtualDevice) bool {
 	return false
 }
 
-// SelectVGPUs returns a slice of vGPUs.
-func SelectVGPUs(
-	devices []vimTypes.BaseVirtualDevice,
-) []*vimTypes.VirtualPCIPassthrough {
-
-	return SelectDevicesByDeviceAndBackingType[
-		*vimTypes.VirtualPCIPassthrough,
-		*vimTypes.VirtualPCIPassthroughVmiopBackingInfo,
-	](devices)
-}
-
 // IsDeviceDynamicDirectPathIO returns true if the provided device is a
 // dynamic direct path I/O device..
 func IsDeviceDynamicDirectPathIO(dev vimTypes.BaseVirtualDevice) bool {

pkg/util/devices_test.go
@@ -101,29 +101,6 @@ var _ = Describe("IsDeviceVGPU", func() {
 	})
 })
 
-var _ = Describe("SelectVGPUs", func() {
-	Context("selecting a vGPU", func() {
-		It("will return only the selected device type", func() {
-			devOut := util.SelectVGPUs(
-				[]vimTypes.BaseVirtualDevice{
-					newPCIPassthroughDevice(""),
-					&vimTypes.VirtualVmxnet3{},
-					newPCIPassthroughDevice("profile1"),
-					&vimTypes.VirtualSriovEthernetCard{},
-					newPCIPassthroughDevice(""),
-					newPCIPassthroughDevice("profile2"),
-				},
-			)
-			Expect(devOut).To(BeAssignableToTypeOf([]*vimTypes.VirtualPCIPassthrough{}))
-			Expect(devOut).To(HaveLen(2))
-			Expect(devOut[0].Backing).To(BeAssignableToTypeOf(&vimTypes.VirtualPCIPassthroughVmiopBackingInfo{}))
-			Expect(devOut[0]).To(BeEquivalentTo(newPCIPassthroughDevice("profile1")))
-			Expect(devOut[1].Backing).To(BeAssignableToTypeOf(&vimTypes.VirtualPCIPassthroughVmiopBackingInfo{}))
-			Expect(devOut[1]).To(BeEquivalentTo(newPCIPassthroughDevice("profile2")))
-		})
-	})
-})
-
 var _ = Describe("IsDeviceDynamicDirectPathIO", func() {
 	Context("a VGPU", func() {
 		It("will return false", func() {

pkg/vmprovider/providers/vsphere/session/session_vm_update_test.go
@@ -177,8 +177,10 @@ var _ = Describe("Mutate update args with DNS information", func() {
 
 var _ = Describe("Update ConfigSpec", func() {
 
-	var config *vimTypes.VirtualMachineConfigInfo
-	var configSpec *vimTypes.VirtualMachineConfigSpec
+	var (
+		config     *vimTypes.VirtualMachineConfigInfo
+		configSpec *vimTypes.VirtualMachineConfigSpec
+	)
 
 	BeforeEach(func() {
 		config = &vimTypes.VirtualMachineConfigInfo{}
@@ -1121,28 +1123,28 @@ var _ = Describe("Update ConfigSpec", func() {
 			},
 		}
 		var pciDevices vmopv1alpha1.VirtualDevices
-		Context("For vGPU device", func() {
+		Context("For VM Class Spec vGPU device", func() {
 			BeforeEach(func() {
 				pciDevices = vmopv1alpha1.VirtualDevices{
 					VGPUDevices: vgpuDevices,
 				}
 			})
 			It("should create vSphere device with VmiopBackingInfo", func() {
-				vSphereDevices := virtualmachine.CreatePCIDevices(pciDevices, nil)
+				vSphereDevices := virtualmachine.CreatePCIDevicesFromVMClass(pciDevices)
 				Expect(vSphereDevices).To(HaveLen(1))
 				virtualDevice := vSphereDevices[0].GetVirtualDevice()
 				backing := virtualDevice.Backing.(*vimTypes.VirtualPCIPassthroughVmiopBackingInfo)
 				Expect(backing.Vgpu).To(Equal(pciDevices.VGPUDevices[0].ProfileName))
 			})
 		})
-		Context("For Dynamic DirectPath I/O device", func() {
+		Context("For VM Class Spec Dynamic DirectPath I/O device", func() {
 			BeforeEach(func() {
 				pciDevices = vmopv1alpha1.VirtualDevices{
 					DynamicDirectPathIODevices: ddpioDevices,
 				}
 			})
 			It("should create vSphere device with DynamicBackingInfo", func() {
-				vSphereDevices := virtualmachine.CreatePCIDevices(pciDevices, nil)
+				vSphereDevices := virtualmachine.CreatePCIDevicesFromVMClass(pciDevices)
 				Expect(vSphereDevices).To(HaveLen(1))
 				virtualDevice := vSphereDevices[0].GetVirtualDevice()
 				backing := virtualDevice.Backing.(*vimTypes.VirtualPCIPassthroughDynamicBackingInfo)
@@ -1151,44 +1153,67 @@ var _ = Describe("Update ConfigSpec", func() {
 				Expect(backing.CustomLabel).To(Equal(pciDevices.DynamicDirectPathIODevices[0].CustomLabel))
 			})
 		})
-		Context("with VMClassAsConfig", func() {
-			var (
-				devIn []*vimTypes.VirtualPCIPassthrough
-			)
-			Context("For vGPU device", func() {
+
+		When("PCI devices from ConfigSpec are specified", func() {
+
+			var devIn []*vimTypes.VirtualPCIPassthrough
+
+			Context("For ConfigSpec VGPU device", func() {
 				BeforeEach(func() {
-					pciDevices = vmopv1alpha1.VirtualDevices{
-						VGPUDevices: []vmopv1alpha1.VGPUDevice{
-							{
-								ProfileName: "SampleProfile1",
-							},
-						},
-					}
 					devIn = []*vimTypes.VirtualPCIPassthrough{
 						{
 							VirtualDevice: vimTypes.VirtualDevice{
 								Backing: &vimTypes.VirtualPCIPassthroughVmiopBackingInfo{
-									Vgpu: "SampleProfile2",
+									Vgpu: "configspec-profile",
 								},
 							},
 						},
 					}
 				})
-				It("should create two vGPUs", func() {
-					devList := virtualmachine.CreatePCIDevices(pciDevices, devIn)
-					Expect(devList).To(HaveLen(2))
+				It("should create vSphere device with VmiopBackingInfo", func() {
+					devList := virtualmachine.CreatePCIDevicesFromConfigSpec(devIn)
+					Expect(devList).To(HaveLen(1))
 
 					Expect(devList[0]).ToNot(BeNil())
 					Expect(devList[0]).To(BeAssignableToTypeOf(&vimTypes.VirtualPCIPassthrough{}))
 					Expect(devList[0].(*vimTypes.VirtualPCIPassthrough).Backing).ToNot(BeNil())
 					Expect(devList[0].(*vimTypes.VirtualPCIPassthrough).Backing).To(BeAssignableToTypeOf(&vimTypes.VirtualPCIPassthroughVmiopBackingInfo{}))
-					Expect(devList[0].(*vimTypes.VirtualPCIPassthrough).Backing.(*vimTypes.VirtualPCIPassthroughVmiopBackingInfo).Vgpu).To(Equal("SampleProfile1"))
+					Expect(devList[0].(*vimTypes.VirtualPCIPassthrough).Backing.(*vimTypes.VirtualPCIPassthroughVmiopBackingInfo).Vgpu).To(Equal("configspec-profile"))
+				})
+			})
 
-					Expect(devList[1]).ToNot(BeNil())
-					Expect(devList[1]).To(BeAssignableToTypeOf(&vimTypes.VirtualPCIPassthrough{}))
-					Expect(devList[1].(*vimTypes.VirtualPCIPassthrough).Backing).ToNot(BeNil())
-					Expect(devList[1].(*vimTypes.VirtualPCIPassthrough).Backing).To(BeAssignableToTypeOf(&vimTypes.VirtualPCIPassthroughVmiopBackingInfo{}))
-					Expect(devList[1].(*vimTypes.VirtualPCIPassthrough).Backing.(*vimTypes.VirtualPCIPassthroughVmiopBackingInfo).Vgpu).To(Equal("SampleProfile2"))
+			Context("For ConfigSpec DirectPath I/O device", func() {
+				BeforeEach(func() {
+					devIn = []*vimTypes.VirtualPCIPassthrough{
+						{
+							VirtualDevice: vimTypes.VirtualDevice{
+								Backing: &vimTypes.VirtualPCIPassthroughDynamicBackingInfo{
+									CustomLabel: "configspec-ddpio-label",
+									AllowedDevice: []vimTypes.VirtualPCIPassthroughAllowedDevice{
+										{
+											VendorId: 456,
+											DeviceId: 457,
+										},
+									},
+								},
+							},
+						},
+					}
+				})
+				It("should create vSphere device with DynamicBackingInfo", func() {
+					devList := virtualmachine.CreatePCIDevicesFromConfigSpec(devIn)
+					Expect(devList).To(HaveLen(1))
+
+					Expect(devList[0]).ToNot(BeNil())
+					Expect(devList[0]).To(BeAssignableToTypeOf(&vimTypes.VirtualPCIPassthrough{}))
+
+					Expect(devList[0].(*vimTypes.VirtualPCIPassthrough).Backing).ToNot(BeNil())
+					backing := devList[0].(*vimTypes.VirtualPCIPassthrough).Backing
+					Expect(backing).To(BeAssignableToTypeOf(&vimTypes.VirtualPCIPassthroughDynamicBackingInfo{}))
+
+					Expect(backing.(*vimTypes.VirtualPCIPassthroughDynamicBackingInfo).CustomLabel).To(Equal("configspec-ddpio-label"))
+					Expect(backing.(*vimTypes.VirtualPCIPassthroughDynamicBackingInfo).AllowedDevice[0].VendorId).To(BeEquivalentTo(456))
+					Expect(backing.(*vimTypes.VirtualPCIPassthroughDynamicBackingInfo).AllowedDevice[0].DeviceId).To(BeEquivalentTo(457))
 				})
 			})
 		})

pkg/vmprovider/providers/vsphere/virtualmachine/devices.go
@@ -28,14 +28,25 @@ func CreatePCIPassThroughDevice(deviceKey int32, backingInfo vimTypes.BaseVirtua
 	return device
 }
 
-func CreatePCIDevices(
-	pciDevicesFromVMClass v1alpha1.VirtualDevices,
-	pciDevsFromConfigSpec []*vimTypes.VirtualPCIPassthrough) []vimTypes.BaseVirtualDevice {
+// CreatePCIDevicesFromConfigSpec creates vim25 VirtualDevices from the specified list of PCI devices from the VM Class ConfigSpec.
+func CreatePCIDevicesFromConfigSpec(pciDevsFromConfigSpec []*vimTypes.VirtualPCIPassthrough) []vimTypes.BaseVirtualDevice {
+	devices := make([]vimTypes.BaseVirtualDevice, 0, len(pciDevsFromConfigSpec))
 
-	devices := make([]vimTypes.BaseVirtualDevice, 0,
-		len(pciDevicesFromVMClass.VGPUDevices)+
-			len(pciDevicesFromVMClass.DynamicDirectPathIODevices)+
-			len(pciDevsFromConfigSpec))
+	deviceKey := pciDevicesStartDeviceKey
+
+	for i := range pciDevsFromConfigSpec {
+		dev := pciDevsFromConfigSpec[i]
+		dev.Key = deviceKey
+		devices = append(devices, dev)
+		deviceKey--
+	}
+
+	return devices
+}
+
+// CreatePCIDevicesFromVMClass creates vim25 VirtualDevices from the specified list of PCI devices from VM Class spec.
+func CreatePCIDevicesFromVMClass(pciDevicesFromVMClass v1alpha1.VirtualDevices) []vimTypes.BaseVirtualDevice {
+	devices := make([]vimTypes.BaseVirtualDevice, 0, len(pciDevicesFromVMClass.VGPUDevices)+len(pciDevicesFromVMClass.DynamicDirectPathIODevices))
 
 	deviceKey := pciDevicesStartDeviceKey
 
@@ -58,14 +69,6 @@ func CreatePCIDevices(
 			CustomLabel:   dynamicDirectPath.CustomLabel,
 		}
 		dev := CreatePCIPassThroughDevice(deviceKey, backingInfo)
-		devices = append(devices, dev)
-		deviceKey--
-	}
-
-	// Append PCI devices from the VM Class ConfigSpec.
-	for i := range pciDevsFromConfigSpec {
-		dev := pciDevsFromConfigSpec[i]
-		dev.Key = deviceKey
 		devices = append(devices, dev)
 		deviceKey--
 	}

commit 87b804631564a12c64dd7269e4b3ef196263bbfd

Account for GPU devices when the DaynDate FSS is disabled
Due to a bug introduced by 44cd693, we are not dropping the PCI
passthrough devices when the DaynDate FSS is disabled.  This change
fixes that.

Additionally, when creating a ConfigSpec for placement, we are
considering the PCI passthrough devices _twice_ (both from VM Class
Hardware, and from the ConfigSpec).  This change fixes the behavior by
only looking at the Hardware if the DaynDate FSS is not enabled.

* Testing Done:
- [x] Added unit tests for changes
- [x] Exiting Unit and Integration tests

pkg/vmprovider/providers/vsphere/session/session_vm_update.go
@@ -512,13 +512,13 @@ func (s *Session) prePowerOnVMConfigSpec(
 	configSpec.DeviceChange = append(configSpec.DeviceChange, ethCardDeviceChanges...)
 
 	var expectedPCIDevices []vimTypes.BaseVirtualDevice
-	if configSpecDevs := util.DevicesFromConfigSpec(updateArgs.ConfigSpec); len(configSpecDevs) > 0 {
-		if lib.IsVMClassAsConfigFSSDaynDateEnabled() {
+	if lib.IsVMClassAsConfigFSSDaynDateEnabled() {
+		if configSpecDevs := util.DevicesFromConfigSpec(updateArgs.ConfigSpec); len(configSpecDevs) > 0 {
 			pciPassthruFromConfigSpec := util.SelectVirtualPCIPassthrough(configSpecDevs)
 			expectedPCIDevices = virtualmachine.CreatePCIDevicesFromConfigSpec(pciPassthruFromConfigSpec)
-		} else {
-			expectedPCIDevices = virtualmachine.CreatePCIDevicesFromVMClass(updateArgs.VMClass.Spec.Hardware.Devices)
 		}
+	} else {
+		expectedPCIDevices = virtualmachine.CreatePCIDevicesFromVMClass(updateArgs.VMClass.Spec.Hardware.Devices)
 	}
 
 	pciDeviceChanges, err := UpdatePCIDeviceChanges(expectedPCIDevices, currentPciDevices)

pkg/vmprovider/providers/vsphere/virtualmachine/configspec.go
@@ -130,11 +130,15 @@ func CreateConfigSpecForPlacement(
 		},
 	})
 
-	for _, dev := range CreatePCIDevicesFromVMClass(vmClassSpec.Hardware.Devices) {
-		configSpec.DeviceChange = append(configSpec.DeviceChange, &vimtypes.VirtualDeviceConfigSpec{
-			Operation: vimtypes.VirtualDeviceConfigSpecOperationAdd,
-			Device:    dev,
-		})
+	// With DaynDate FSS, PCI devices are specified via ConfigSpec.  Ignore such devices from the
+	// VM Class Hardware to avoid duplicate devices being added to the placement spec.
+	if !lib.IsVMClassAsConfigFSSDaynDateEnabled() {
+		for _, dev := range CreatePCIDevicesFromVMClass(vmClassSpec.Hardware.Devices) {
+			configSpec.DeviceChange = append(configSpec.DeviceChange, &vimtypes.VirtualDeviceConfigSpec{
+				Operation: vimtypes.VirtualDeviceConfigSpecOperationAdd,
+				Device:    dev,
+			})
+		}
 	}
 
 	if lib.IsInstanceStorageFSSEnabled() {

pkg/vmprovider/providers/vsphere/virtualmachine/configspec_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 VMware, Inc. All Rights Reserved.
+// Copyright (c) 2022-2023 VMware, Inc. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 package virtualmachine_test
@@ -6,15 +6,11 @@ package virtualmachine_test
 import (
 	goctx "context"
 
-	"github.com/go-logr/logr"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	vimtypes "github.com/vmware/govmomi/vim25/types"
 
 	vmopv1 "github.com/vmware-tanzu/vm-operator/api/v1alpha1"
-
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
-
 	"github.com/vmware-tanzu/vm-operator/pkg/context"
 	"github.com/vmware-tanzu/vm-operator/pkg/lib"
 	"github.com/vmware-tanzu/vm-operator/pkg/vmprovider/providers/vsphere/virtualmachine"
@@ -128,7 +124,7 @@ var _ = Describe("CreateConfigSpecForPlacement", func() {
 		vm := builder.DummyVirtualMachine()
 		vmCtx = context.VirtualMachineContext{
 			Context: goctx.Background(),
-			Logger:  logr.New(logf.NullLogSink{}),
+			Logger:  suite.GetLogger().WithValues("vmName", vm.GetName()),
 			VM:      vm,
 		}
 	})
@@ -168,43 +164,106 @@ var _ = Describe("CreateConfigSpecForPlacement", func() {
 		})
 	})
 
-	Context("When class config spec is not nil", func() {
+	Context("when DaynDate FSS is enabled", func() {
+		var oldDaynDateFSSFn func() bool
+
 		BeforeEach(func() {
-			classConfigSpec = &vimtypes.VirtualMachineConfigSpec{
-				Name:       "dummy-VM",
-				Annotation: "test-annotation",
-				DeviceChange: []vimtypes.BaseVirtualDeviceConfigSpec{
-					&vimtypes.VirtualDeviceConfigSpec{
-						Operation: vimtypes.VirtualDeviceConfigSpecOperationAdd,
-						Device: &vimtypes.VirtualPCIPassthrough{
-							VirtualDevice: vimtypes.VirtualDevice{
-								Backing: &vimtypes.VirtualPCIPassthroughVmiopBackingInfo{
-									Vgpu: "SampleProfile2",
+			oldDaynDateFSSFn = lib.IsVMClassAsConfigFSSDaynDateEnabled
+			lib.IsVMClassAsConfigFSSDaynDateEnabled = func() bool { return true }
+		})
+
+		AfterEach(func() {
+			lib.IsVMClassAsConfigFSSDaynDateEnabled = oldDaynDateFSSFn
+		})
+
+		Context("When class config spec is not nil", func() {
+			BeforeEach(func() {
+				classConfigSpec = &vimtypes.VirtualMachineConfigSpec{
+					Name:       "dummy-VM",
+					Annotation: "test-annotation",
+					DeviceChange: []vimtypes.BaseVirtualDeviceConfigSpec{
+						&vimtypes.VirtualDeviceConfigSpec{
+							Operation: vimtypes.VirtualDeviceConfigSpecOperationAdd,
+							Device: &vimtypes.VirtualPCIPassthrough{
+								VirtualDevice: vimtypes.VirtualDevice{
+									Backing: &vimtypes.VirtualPCIPassthroughVmiopBackingInfo{
+										Vgpu: "SampleProfile2",
+									},
 								},
 							},
 						},
 					},
-				},
-			}
-		})
+				}
+			})
 
-		It("Placement ConfigSpec contains expected field set sans ethernet device from class config spec", func() {
-			Expect(configSpec.Annotation).ToNot(BeEmpty())
-			Expect(configSpec.Annotation).To(Equal(classConfigSpec.Annotation))
-			Expect(configSpec.NumCPUs).To(BeEquivalentTo(vmClassSpec.Hardware.Cpus))
-			Expect(configSpec.MemoryMB).To(BeEquivalentTo(4 * 1024))
-			Expect(configSpec.CpuAllocation).ToNot(BeNil())
-			Expect(configSpec.MemoryAllocation).ToNot(BeNil())
-			Expect(configSpec.Firmware).To(Equal(firmware))
-			Expect(configSpec.DeviceChange).To(HaveLen(2))
-			dSpec := configSpec.DeviceChange[0].GetVirtualDeviceConfigSpec()
-			_, ok := dSpec.Device.(*vimtypes.VirtualPCIPassthrough)
-			Expect(ok).To(BeTrue())
-			dSpec1 := configSpec.DeviceChange[1].GetVirtualDeviceConfigSpec()
-			_, ok = dSpec1.Device.(*vimtypes.VirtualDisk)
-			Expect(ok).To(BeTrue())
+			AfterEach(func() {
+				classConfigSpec = nil
+			})
+
+			It("Placement ConfigSpec contains expected field set sans ethernet device from class config spec", func() {
+				Expect(configSpec.Annotation).ToNot(BeEmpty())
+				Expect(configSpec.Annotation).To(Equal(classConfigSpec.Annotation))
+				Expect(configSpec.NumCPUs).To(BeEquivalentTo(vmClassSpec.Hardware.Cpus))
+				Expect(configSpec.MemoryMB).To(BeEquivalentTo(4 * 1024))
+				Expect(configSpec.CpuAllocation).ToNot(BeNil())
+				Expect(configSpec.MemoryAllocation).ToNot(BeNil())
+				Expect(configSpec.Firmware).To(Equal(firmware))
+				Expect(configSpec.DeviceChange).To(HaveLen(2))
+				dSpec := configSpec.DeviceChange[0].GetVirtualDeviceConfigSpec()
+				_, ok := dSpec.Device.(*vimtypes.VirtualPCIPassthrough)
+				Expect(ok).To(BeTrue())
+				dSpec1 := configSpec.DeviceChange[1].GetVirtualDeviceConfigSpec()
+				_, ok = dSpec1.Device.(*vimtypes.VirtualDisk)
+				Expect(ok).To(BeTrue())
+			})
 		})
 	})
+
+	Context("when Class specifies GPUs in Hardware", func() {
+		BeforeEach(func() {
+			vmClassSpec.Hardware.Devices.VGPUDevices = []vmopv1.VGPUDevice{{
+				ProfileName: "createplacementspec-profile",
+			}}
+
+			vmClassSpec.Hardware.Devices.DynamicDirectPathIODevices = []vmopv1.DynamicDirectPathIODevice{{
+				VendorID:    20,
+				DeviceID:    30,
+				CustomLabel: "createplacementspec-label",
+			}}
+		})
+
+		AfterEach(func() {
+			vmClassSpec.Hardware.Devices.VGPUDevices = nil
+			vmClassSpec.Hardware.Devices.DynamicDirectPathIODevices = nil
+		})
+
+		It("Placement ConfigSpec contains the GPU from the VM Class", func() {
+			Expect(configSpec.DeviceChange).To(HaveLen(3)) // one dummy disk + 2 Pass through devices
+
+			dspec := configSpec.DeviceChange[0].GetVirtualDeviceConfigSpec()
+			_, ok := dspec.Device.(*vimtypes.VirtualDisk)
+			Expect(ok).To(BeTrue())
+
+			dSpec1 := configSpec.DeviceChange[1].GetVirtualDeviceConfigSpec()
+			dev1, ok := dSpec1.Device.(*vimtypes.VirtualPCIPassthrough)
+			Expect(ok).To(BeTrue())
+			pciDev1 := dev1.GetVirtualDevice()
+			pciBacking1, ok1 := pciDev1.Backing.(*vimtypes.VirtualPCIPassthroughVmiopBackingInfo)
+			Expect(ok1).To(BeTrue())
+			Expect(pciBacking1.Vgpu).To(Equal(vmClassSpec.Hardware.Devices.VGPUDevices[0].ProfileName))
+
+			dSpec2 := configSpec.DeviceChange[2].GetVirtualDeviceConfigSpec()
+			dev2, ok2 := dSpec2.Device.(*vimtypes.VirtualPCIPassthrough)
+			Expect(ok2).To(BeTrue())
+			pciDev2 := dev2.GetVirtualDevice()
+			pciBacking2, ok2 := pciDev2.Backing.(*vimtypes.VirtualPCIPassthroughDynamicBackingInfo)
+			Expect(ok2).To(BeTrue())
+			Expect(pciBacking2.AllowedDevice[0].DeviceId).To(BeEquivalentTo(vmClassSpec.Hardware.Devices.DynamicDirectPathIODevices[0].DeviceID))
+			Expect(pciBacking2.AllowedDevice[0].VendorId).To(BeEquivalentTo(vmClassSpec.Hardware.Devices.DynamicDirectPathIODevices[0].VendorID))
+			Expect(pciBacking2.CustomLabel).To(Equal(vmClassSpec.Hardware.Devices.DynamicDirectPathIODevices[0].CustomLabel))
+		})
+	})
+
 })
 
 func assertInstanceStorageDeviceChange(

pkg/vmprovider/providers/vsphere/vmprovider_vm_test.go
@@ -760,6 +760,48 @@ func vmTests() {
 				// TODO: More assertions!
 			})
 
+			Context("VM Class with PCI passthrough devices", func() {
+				BeforeEach(func() {
+					vmClass.Spec.Hardware.Devices = vmopv1alpha1.VirtualDevices{
+						VGPUDevices: []vmopv1alpha1.VGPUDevice{
+							{
+								ProfileName: "profile-from-class-without-class-as-config-fss",
+							},
+						},
+						DynamicDirectPathIODevices: []vmopv1alpha1.DynamicDirectPathIODevice{
+							{
+								VendorID:    59,
+								DeviceID:    60,
+								CustomLabel: "label-from-class-without-class-as-config-fss",
+							},
+						},
+					}
+				})
+
+				It("VM should have expected PCI devices from VM Class", func() {
+					vcVM, err := createOrUpdateAndGetVcVM(ctx, vm)
+					Expect(err).ToNot(HaveOccurred())
+
+					var o mo.VirtualMachine
+					Expect(vcVM.Properties(ctx, vcVM.Reference(), nil, &o)).To(Succeed())
+
+					devList := object.VirtualDeviceList(o.Config.Hardware.Device)
+					p := devList.SelectByType(&types.VirtualPCIPassthrough{})
+					Expect(p).To(HaveLen(2))
+					pciDev1 := p[0].GetVirtualDevice()
+					pciBacking1, ok1 := pciDev1.Backing.(*types.VirtualPCIPassthroughVmiopBackingInfo)
+					Expect(ok1).Should(BeTrue())
+					Expect(pciBacking1.Vgpu).To(Equal("profile-from-class-without-class-as-config-fss"))
+					pciDev2 := p[1].GetVirtualDevice()
+					pciBacking2, ok2 := pciDev2.Backing.(*types.VirtualPCIPassthroughDynamicBackingInfo)
+					Expect(ok2).Should(BeTrue())
+					Expect(pciBacking2.AllowedDevice).To(HaveLen(1))
+					Expect(pciBacking2.AllowedDevice[0].VendorId).To(Equal(int32(59)))
+					Expect(pciBacking2.AllowedDevice[0].DeviceId).To(Equal(int32(60)))
+					Expect(pciBacking2.CustomLabel).To(Equal("label-from-class-without-class-as-config-fss"))
+				})
+			})
+
 			Context("Prereq args and Conditions", func() {
 				readyCondition := *conditions.TrueCondition(vmopv1alpha1.VirtualMachinePrereqReadyCondition)
 
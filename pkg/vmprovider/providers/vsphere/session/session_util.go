cdeec0cdffcfeffe9e518e4732e1ffc371cd5983
Arunesh Pandey <parunesh@vmware.com>
2023-09-06 22:07:06 -0700

    Allow non user-configurable properties in VM deployment
    Currently, VM operator ignores any vApp properties that are not user
    configurable when patching with values specified in the bootstrap
    resources (VM metadata ConfigMap/Secret). There are some valid
    use-cases where users might want to patch hidden properties defined in
    an image. This change fixes this behavior.
    
    Fixes #181.

--- pkg/vmprovider/providers/vsphere/session/session_util.go
+++ pkg/vmprovider/providers/vsphere/session/session_util.go
@@ -49,11 +49,8 @@ func MergeExtraConfig(extraConfig []vimTypes.BaseOptionValue, newMap map[string]
 func GetMergedvAppConfigSpec(inProps map[string]string, vmProps []vimTypes.VAppPropertyInfo) *vimTypes.VmConfigSpec {
 	outProps := make([]vimTypes.VAppPropertySpec, 0)
 
+	// Update any vApp properties if they have been specified in VM metadata.
 	for _, vmProp := range vmProps {
-		if vmProp.UserConfigurable == nil || !*vmProp.UserConfigurable {
-			continue
-		}
-
 		inPropValue, found := inProps[vmProp.Id]
 		if !found || vmProp.Value == inPropValue {
 			continue

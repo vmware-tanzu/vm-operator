b57c8bc9622d3249ea8470b9968201267e89bfe0
Sreyas Natarajan <sreyasn@vmware.com>
2023-10-03 14:07:32 -0700

    Add v1a2 webconsolerequest validation webhooks

---
+++ webhooks/virtualmachinewebconsolerequest/v1alpha2/validation/webconsolerequest_validator_suite_test.go
@@ -0,0 +1,29 @@
+// Copyright (c) 2022-2023 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package validation_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+
+	"github.com/vmware-tanzu/vm-operator/pkg/lib"
+	"github.com/vmware-tanzu/vm-operator/test/builder"
+	"github.com/vmware-tanzu/vm-operator/webhooks/virtualmachinewebconsolerequest/v1alpha2/validation"
+)
+
+// suite is used for unit and integration testing this webhook.
+var suite = builder.NewTestSuiteForValidatingWebhookwithFSS(
+	validation.AddToManager,
+	validation.NewValidator,
+	"default.validating.virtualmachinewebconsolerequest.v1alpha2.vmoperator.vmware.com",
+	map[string]bool{lib.VMServiceV1Alpha2FSS: true})
+
+func TestWebhook(t *testing.T) {
+	suite.Register(t, "Validation webhook suite", intgTests, unitTests)
+}
+
+var _ = BeforeSuite(suite.BeforeSuite)
+
+var _ = AfterSuite(suite.AfterSuite)

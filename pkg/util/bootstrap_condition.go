366525f3d0ddfd325856def6ab1e50ebbcb773c3
akutz <akutz@vmware.com>
2024-01-22 09:17:09 -0600

    Guest bootstrap status as condition
    This patch introduces the condition "GuestBootstrap" such
    that its status, reason, and message are all surfaced from reading
    the ExtraConfig key "guestinfo.vmservice.bootstrap.condition". The
    key's value is comma-delimited, split at most three times:
    
        0 - status
        1 - reason  (if present)
        2 - message (if present)
    
    This condition allows users specifying run commands using either
    the CloudInit or Sysprep bootstrap providers to leverage the
    vmware-rpctool or vmtoolsd commands to update the aforementioned
    guestinfo key with a value that is ultimately realized in the
    Kubernetes VM's status as a condition.

---
+++ pkg/util/bootstrap_condition.go
@@ -0,0 +1,57 @@
+// Copyright (c) 2024 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package util
+
+import (
+	"strconv"
+	"strings"
+
+	vimTypes "github.com/vmware/govmomi/vim25/types"
+)
+
+// GuestInfoBootstrapCondition is the ExtraConfig key at which possible info
+// about the bootstrap status may be stored.
+const GuestInfoBootstrapCondition = "guestinfo.vmservice.bootstrap.condition"
+
+// GetBootstrapConditionValues returns the bootstrap condition values from a
+// VM if the data is present.
+func GetBootstrapConditionValues(
+	configInfo *vimTypes.VirtualMachineConfigInfo) (bool, string, string, bool) {
+
+	if configInfo == nil {
+		return false, "", "", false
+	}
+
+	if len(configInfo.ExtraConfig) == 0 {
+		return false, "", "", false
+	}
+
+	for i := range configInfo.ExtraConfig {
+		if ec := configInfo.ExtraConfig[i]; ec != nil {
+			if ov := ec.GetOptionValue(); ov != nil {
+				if ov.Key == GuestInfoBootstrapCondition {
+					if s, ok := ov.Value.(string); ok {
+						v := strings.SplitN(s, ",", 3)
+						status, _ := strconv.ParseBool(strings.TrimSpace(v[0]))
+						switch len(v) {
+						case 1:
+							return status, "", "", true
+						case 2:
+							return status, strings.TrimSpace(v[1]), "", true
+						case 3:
+							return status,
+								strings.TrimSpace(v[1]),
+								strings.TrimSpace(v[2]),
+								true
+						default:
+							return false, "", "", false
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return false, "", "", false
+}

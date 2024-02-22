0a50cd4ce6167fed31ba21aa7850a238ea30d980
Bryan Venteicher <bryanventeicher@gmail.com>
2024-02-22 15:17:33 -0600

    Remove v1a1 controllers
    Also remove the providerconfigmap controller since it depends on v1a1
    types, and it isn't used anymore because of ImageRegistry work.

--- webhooks/virtualmachinepublishrequest/v1alpha2/validation/virtualmachinepublishrequest_validator_unit_test.go
+++ webhooks/virtualmachinepublishrequest/v1alpha2/validation/virtualmachinepublishrequest_validator_unit_test.go
@@ -14,7 +14,7 @@ import (
 	imgregv1a1 "github.com/vmware-tanzu/image-registry-operator-api/api/v1alpha1"
 
 	vmopv1 "github.com/vmware-tanzu/vm-operator/api/v1alpha2"
-	"github.com/vmware-tanzu/vm-operator/controllers/contentlibrary/v1alpha1/utils"
+	"github.com/vmware-tanzu/vm-operator/controllers/contentlibrary/v1alpha2/utils"
 	pkgconfig "github.com/vmware-tanzu/vm-operator/pkg/config"
 	"github.com/vmware-tanzu/vm-operator/test/builder"
 )

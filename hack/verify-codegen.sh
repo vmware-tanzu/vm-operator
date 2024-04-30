5cc54908d36328f415b4fefd266e282b14b1f6e2
akutz <akutz@vmware.com>
2024-04-30 18:27:05 -0500

    GitHub action verify-codegen checks conversions
    This patch updates the GitHub action that verifies codegen
    so that the generate-go-conversions target is checked as
    well.

--- hack/verify-codegen.sh
+++ hack/verify-codegen.sh
@@ -10,7 +10,26 @@ set -o pipefail
 # script is located.
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-make generate
+if ! make generate; then
+  exit_code="${?}"
+
+  # Please note the following heredoc uses leading tabs to allow
+  # the contents to be indented with the if/fi statement. For
+  # more information on indenting heredocs, please see the section
+  # entitled "Mutli-line message, with tabs suppressed" from The
+  # Linux Documentation Project (TLDP) at
+  # https://tldp.org/LDP/abs/html/here-docs.html.
+  cat <<-EOF
+	Please investigate why the target is failing by running the
+	following command locally from the root of the project:
+
+	    make generate
+
+	Thank you!
+	EOF
+
+  exit "${exit_code}"
+fi
 
 if git diff --exit-code; then
   printf '\nCongratulations! Generated assets are up-to-date!\n'
@@ -29,6 +48,51 @@ else
 	following command from the root of the project:
 
 	    make generate
+
+	Thank you!
+	EOF
+
+  exit "${exit_code}"
+fi
+
+if ! make generate-go-conversions; then
+  exit_code="${?}"
+
+  # Please note the following heredoc uses leading tabs to allow
+  # the contents to be indented with the if/fi statement. For
+  # more information on indenting heredocs, please see the section
+  # entitled "Mutli-line message, with tabs suppressed" from The
+  # Linux Documentation Project (TLDP) at
+  # https://tldp.org/LDP/abs/html/here-docs.html.
+  cat <<-EOF
+	Please investigate why the target is failing by running the
+	following command locally from the root of the project:
+
+	    make generate-go-conversions
+
+	Thank you!
+	EOF
+
+  exit "${exit_code}"
+fi
+
+if git diff --exit-code; then
+  printf '\nCongratulations! Generated conversions are up-to-date!\n'
+else
+  exit_code="${?}"
+
+  # Please note the following heredoc uses leading tabs to allow
+  # the contents to be indented with the if/fi statement. For
+  # more information on indenting heredocs, please see the section
+  # entitled "Mutli-line message, with tabs suppressed" from The
+  # Linux Documentation Project (TLDP) at
+  # https://tldp.org/LDP/abs/html/here-docs.html.
+  cat <<-EOF
+	Please update generated conversions before opening a pull request or
+	pushing new changes. To generate the conversions, please run the
+	following command from the root of the project:
+
+	    make generate-go-conversions
 
 	Thank you!
 	EOF

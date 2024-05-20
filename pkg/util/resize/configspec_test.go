89eb24f98d22583ce22ab311f75c52e80284a828
Bryan Venteicher <bryanventeicher@gmail.com>
2024-05-20 10:30:41 -0500

    Compare basic ConfigSpec.Hardware fields for resize
    For the fields that are either comparable or bool pointer and have
    an equivalent field in the ConfigInfo, compare then during resize.
    This is mostly just a start on how to best start comparing all the
    fields in the ConfigSpec/ConfigInfo.

--- pkg/util/resize/configspec_test.go
+++ pkg/util/resize/configspec_test.go
@@ -22,8 +22,9 @@ type ConfigInfo = vimtypes.VirtualMachineConfigInfo
 var _ = Describe("CreateResizeConfigSpec", func() {
 
 	ctx := context.Background()
+	truePtr, falsePtr := vimtypes.NewBool(true), vimtypes.NewBool(false)
 
-	DescribeTable("Simple fields",
+	DescribeTable("ConfigInfo.Hardware",
 		func(
 			ci vimtypes.VirtualMachineConfigInfo,
 			cs, expectedCS vimtypes.VirtualMachineConfigSpec) {
@@ -32,6 +33,11 @@ var _ = Describe("CreateResizeConfigSpec", func() {
 			Expect(err).ToNot(HaveOccurred())
 			Expect(reflect.DeepEqual(actualCS, expectedCS)).To(BeTrue(), cmp.Diff(actualCS, expectedCS))
 		},
+
+		Entry("Empty Hardware needs no updating",
+			ConfigInfo{Hardware: vimtypes.VirtualHardware{}},
+			ConfigSpec{},
+			ConfigSpec{}),
 
 		Entry("NumCPUs needs updating",
 			ConfigInfo{Hardware: vimtypes.VirtualHardware{NumCPU: 2}},
@@ -42,6 +48,15 @@ var _ = Describe("CreateResizeConfigSpec", func() {
 			ConfigSpec{NumCPUs: 4},
 			ConfigSpec{}),
 
+		Entry("NumCoresPerSocket needs updating",
+			ConfigInfo{Hardware: vimtypes.VirtualHardware{NumCoresPerSocket: 2}},
+			ConfigSpec{NumCoresPerSocket: 4},
+			ConfigSpec{NumCoresPerSocket: 4}),
+		Entry("NumCoresPerSocket does not need updating",
+			ConfigInfo{Hardware: vimtypes.VirtualHardware{NumCoresPerSocket: 4}},
+			ConfigSpec{NumCoresPerSocket: 4},
+			ConfigSpec{}),
+
 		Entry("MemoryMB needs updating",
 			ConfigInfo{Hardware: vimtypes.VirtualHardware{MemoryMB: 512}},
 			ConfigSpec{NumCPUs: 1024},
@@ -49,6 +64,50 @@ var _ = Describe("CreateResizeConfigSpec", func() {
 		Entry("MemoryMB does not need updating",
 			ConfigInfo{Hardware: vimtypes.VirtualHardware{MemoryMB: 1024}},
 			ConfigSpec{MemoryMB: 1024},
+			ConfigSpec{}),
+
+		Entry("VirtualICH7MPresent needs updating",
+			ConfigInfo{Hardware: vimtypes.VirtualHardware{VirtualICH7MPresent: truePtr}},
+			ConfigSpec{VirtualICH7MPresent: falsePtr},
+			ConfigSpec{VirtualICH7MPresent: falsePtr}),
+		Entry("VirtualICH7MPresent needs updating",
+			ConfigInfo{Hardware: vimtypes.VirtualHardware{VirtualICH7MPresent: nil}},
+			ConfigSpec{VirtualICH7MPresent: falsePtr},
+			ConfigSpec{VirtualICH7MPresent: falsePtr}),
+		Entry("VirtualICH7MPresent does not need updating",
+			ConfigInfo{Hardware: vimtypes.VirtualHardware{VirtualICH7MPresent: truePtr}},
+			ConfigSpec{VirtualICH7MPresent: truePtr},
+			ConfigSpec{}),
+
+		Entry("VirtualSMCPresent needs updating",
+			ConfigInfo{Hardware: vimtypes.VirtualHardware{VirtualSMCPresent: truePtr}},
+			ConfigSpec{VirtualSMCPresent: falsePtr},
+			ConfigSpec{VirtualSMCPresent: falsePtr}),
+		Entry("VirtualSMCPresent needs updating",
+			ConfigInfo{Hardware: vimtypes.VirtualHardware{VirtualSMCPresent: nil}},
+			ConfigSpec{VirtualSMCPresent: falsePtr},
+			ConfigSpec{VirtualSMCPresent: falsePtr}),
+		Entry("VirtualSMCPresent does not need updating",
+			ConfigInfo{Hardware: vimtypes.VirtualHardware{VirtualSMCPresent: truePtr}},
+			ConfigSpec{VirtualSMCPresent: truePtr},
+			ConfigSpec{}),
+
+		Entry("MotherboardLayout needs updating",
+			ConfigInfo{Hardware: vimtypes.VirtualHardware{MotherboardLayout: "foo"}},
+			ConfigSpec{MotherboardLayout: "i440bxHostBridge"},
+			ConfigSpec{MotherboardLayout: "i440bxHostBridge"}),
+		Entry("MotherboardLayout does not needs updating",
+			ConfigInfo{Hardware: vimtypes.VirtualHardware{MotherboardLayout: "i440bxHostBridge"}},
+			ConfigSpec{MotherboardLayout: "i440bxHostBridge"},
+			ConfigSpec{}),
+
+		Entry("SimultaneousThreads needs updating",
+			ConfigInfo{Hardware: vimtypes.VirtualHardware{SimultaneousThreads: 8}},
+			ConfigSpec{SimultaneousThreads: 16},
+			ConfigSpec{SimultaneousThreads: 16}),
+		Entry("SimultaneousThreads does not need updating",
+			ConfigInfo{Hardware: vimtypes.VirtualHardware{SimultaneousThreads: 8}},
+			ConfigSpec{SimultaneousThreads: 8},
 			ConfigSpec{}),
 	)
 })

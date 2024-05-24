45c6561affe8b7d5b01bbd9adb02fc94d894a205
Sreyas Natarajan <sreyasn@vmware.com>
2024-05-24 15:52:08 -0700

    Resize fields: CPU affinity, hot add, hot remove, latency sensitivity, perf counter, resource allocation
    This change adds support for resize when some of the fields in the configSpec are modified.
    Namely,
    - CPU affinity
    - CPU hot add and remove
    - CPU latency sensitivity
    - CPU perf counter (VPMC)
    - CPU resource allocation

--- pkg/util/resize/configspec_test.go
+++ pkg/util/resize/configspec_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 	vimtypes "github.com/vmware/govmomi/vim25/types"
 
+	"github.com/vmware-tanzu/vm-operator/pkg/util/ptr"
 	"github.com/vmware-tanzu/vm-operator/pkg/util/resize"
 )
 
@@ -108,6 +109,99 @@ var _ = Describe("CreateResizeConfigSpec", func() {
 		Entry("SimultaneousThreads does not need updating",
 			ConfigInfo{Hardware: vimtypes.VirtualHardware{SimultaneousThreads: 8}},
 			ConfigSpec{SimultaneousThreads: 8},
+			ConfigSpec{}),
+
+		Entry("CPU allocation (reservation, limit, shares) settings needs updating",
+			ConfigInfo{
+				CpuAllocation: &vimtypes.ResourceAllocationInfo{
+					Reservation: ptr.To(int64(100)),
+					Limit:       ptr.To(int64(100)),
+					Shares:      &vimtypes.SharesInfo{Level: vimtypes.SharesLevelNormal},
+				}},
+			ConfigSpec{
+				CpuAllocation: &vimtypes.ResourceAllocationInfo{
+					Reservation: ptr.To(int64(200)),
+					Limit:       ptr.To(int64(200)),
+					Shares:      &vimtypes.SharesInfo{Level: vimtypes.SharesLevelCustom, Shares: 50},
+				}},
+			ConfigSpec{
+				CpuAllocation: &vimtypes.ResourceAllocationInfo{
+					Reservation: ptr.To(int64(200)),
+					Limit:       ptr.To(int64(200)),
+					Shares:      &vimtypes.SharesInfo{Level: vimtypes.SharesLevelCustom, Shares: 50},
+				}}),
+		Entry("CPU allocation (reservation, limit, shares) settings needs updating - empty to values set ",
+			ConfigInfo{
+				CpuAllocation: &vimtypes.ResourceAllocationInfo{}},
+			ConfigSpec{
+				CpuAllocation: &vimtypes.ResourceAllocationInfo{
+					Reservation: ptr.To(int64(200)),
+					Limit:       ptr.To(int64(200)),
+					Shares:      &vimtypes.SharesInfo{Level: vimtypes.SharesLevelNormal},
+				}},
+			ConfigSpec{
+				CpuAllocation: &vimtypes.ResourceAllocationInfo{
+					Reservation: ptr.To(int64(200)),
+					Limit:       ptr.To(int64(200)),
+					Shares:      &vimtypes.SharesInfo{Level: vimtypes.SharesLevelNormal},
+				}}),
+		Entry("CPU allocation (reservation,limit,shares) settings does not need updating",
+			ConfigInfo{
+				CpuAllocation: &vimtypes.ResourceAllocationInfo{
+					Reservation: ptr.To(int64(100)),
+					Limit:       ptr.To(int64(100)),
+					Shares:      &vimtypes.SharesInfo{Level: vimtypes.SharesLevelNormal},
+				}},
+			ConfigSpec{
+				CpuAllocation: &vimtypes.ResourceAllocationInfo{
+					Reservation: ptr.To(int64(100)),
+					Limit:       ptr.To(int64(100)),
+					Shares:      &vimtypes.SharesInfo{Level: vimtypes.SharesLevelNormal},
+				}},
+			ConfigSpec{}),
+
+		Entry("CPU Hot Add/Remove needs updating false to true",
+			ConfigInfo{CpuHotAddEnabled: falsePtr, CpuHotRemoveEnabled: falsePtr},
+			ConfigSpec{CpuHotAddEnabled: truePtr, CpuHotRemoveEnabled: truePtr},
+			ConfigSpec{CpuHotAddEnabled: truePtr, CpuHotRemoveEnabled: truePtr}),
+		Entry("CPU Hot Add/Remove needs updating nil to true",
+			ConfigInfo{},
+			ConfigSpec{CpuHotAddEnabled: truePtr, CpuHotRemoveEnabled: truePtr},
+			ConfigSpec{CpuHotAddEnabled: truePtr, CpuHotRemoveEnabled: truePtr}),
+		Entry("CPU Hot Add/Remove does not need updating",
+			ConfigInfo{CpuHotAddEnabled: falsePtr, CpuHotRemoveEnabled: falsePtr},
+			ConfigSpec{CpuHotAddEnabled: falsePtr, CpuHotRemoveEnabled: falsePtr},
+			ConfigSpec{}),
+
+		Entry("CPU affinity settings needs updating value change",
+			ConfigInfo{CpuAffinity: &vimtypes.VirtualMachineAffinityInfo{AffinitySet: []int32{2, 3}}},
+			ConfigSpec{CpuAffinity: &vimtypes.VirtualMachineAffinityInfo{AffinitySet: []int32{1, 3}}},
+			ConfigSpec{CpuAffinity: &vimtypes.VirtualMachineAffinityInfo{AffinitySet: []int32{1, 3}}}),
+		Entry("CPU affinity settings needs updating - remove existing",
+			ConfigInfo{CpuAffinity: &vimtypes.VirtualMachineAffinityInfo{AffinitySet: []int32{2, 3}}},
+			ConfigSpec{CpuAffinity: &vimtypes.VirtualMachineAffinityInfo{AffinitySet: []int32{}}},
+			ConfigSpec{CpuAffinity: &vimtypes.VirtualMachineAffinityInfo{AffinitySet: []int32{}}}),
+		Entry("CPU affinity settings does not need updating",
+			ConfigInfo{CpuAffinity: &vimtypes.VirtualMachineAffinityInfo{AffinitySet: []int32{1, 2, 3}}},
+			ConfigSpec{CpuAffinity: &vimtypes.VirtualMachineAffinityInfo{AffinitySet: []int32{3, 1, 2}}},
+			ConfigSpec{}),
+
+		Entry("CPU perf counter settings does not need updating",
+			ConfigInfo{VPMCEnabled: falsePtr},
+			ConfigSpec{VPMCEnabled: falsePtr},
+			ConfigSpec{}),
+		Entry("CPU perf counter settings needs updating",
+			ConfigInfo{VPMCEnabled: falsePtr},
+			ConfigSpec{VPMCEnabled: truePtr},
+			ConfigSpec{VPMCEnabled: truePtr}),
+
+		Entry("Latency sensitivity settings needs updating",
+			ConfigInfo{LatencySensitivity: &vimtypes.LatencySensitivity{Level: vimtypes.LatencySensitivitySensitivityLevelLow}},
+			ConfigSpec{LatencySensitivity: &vimtypes.LatencySensitivity{Level: vimtypes.LatencySensitivitySensitivityLevelMedium}},
+			ConfigSpec{LatencySensitivity: &vimtypes.LatencySensitivity{Level: vimtypes.LatencySensitivitySensitivityLevelMedium}}),
+		Entry("Latency sensitivity settings does not need updating",
+			ConfigInfo{LatencySensitivity: &vimtypes.LatencySensitivity{Level: vimtypes.LatencySensitivitySensitivityLevelLow}},
+			ConfigSpec{LatencySensitivity: &vimtypes.LatencySensitivity{Level: vimtypes.LatencySensitivitySensitivityLevelLow}},
 			ConfigSpec{}),
 	)
 })

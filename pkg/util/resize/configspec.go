131793c5d7c05b57d2266ba04bc83a696ccb3c77
Sreyas Natarajan <53065832+sreyasn@users.noreply.github.com>
2024-06-05 15:36:41 -0700

    Resize fields: CPU affinity, hot add, hot remove, latency sensitivity, perf counter, resource allocation (#559)
    This change adds support for resize when some of the fields in the configSpec are modified.
    Namely,
    - CPU affinity
    - CPU hot add and remove
    - CPU latency sensitivity
    - CPU perf counter (VPMC)
    - CPU resource allocation

--- pkg/util/resize/configspec.go
+++ pkg/util/resize/configspec.go
@@ -5,6 +5,8 @@ package resize
 
 import (
 	"context"
+	"reflect"
+	"slices"
 
 	vimtypes "github.com/vmware/govmomi/vim25/types"
 )
@@ -20,6 +22,11 @@ func CreateResizeConfigSpec(
 	outCS := vimtypes.VirtualMachineConfigSpec{}
 
 	compareHardware(ci, cs, &outCS)
+	compareCPUAllocation(ci, cs, &outCS)
+	compareCPUHotAddOrRemove(ci, cs, &outCS)
+	compareCPUAffinity(ci, cs, &outCS)
+	compareCPUPerfCounter(ci, cs, &outCS)
+	compareLatencySensitivity(ci, cs, &outCS)
 
 	return outCS, nil
 }
@@ -39,6 +46,120 @@ func compareHardware(
 	// outCS.Device = ...
 	cmp(ci.Hardware.MotherboardLayout, cs.MotherboardLayout, &outCS.MotherboardLayout)
 	cmp(ci.Hardware.SimultaneousThreads, cs.SimultaneousThreads, &outCS.SimultaneousThreads)
+}
+
+// compareCPUAllocation compares CPU resource allocation.
+func compareCPUAllocation(
+	ci vimtypes.VirtualMachineConfigInfo,
+	cs vimtypes.VirtualMachineConfigSpec,
+	outCS *vimtypes.VirtualMachineConfigSpec) {
+	// nothing to change
+	if cs.CpuAllocation == nil {
+		return
+	}
+
+	ciCPUAllocation := ci.CpuAllocation
+	csCPUAllocation := cs.CpuAllocation
+
+	var cpuReservation *int64
+	if csCPUAllocation.Reservation != nil {
+		if ciCPUAllocation == nil || ciCPUAllocation.Reservation == nil || *ciCPUAllocation.Reservation != *csCPUAllocation.Reservation {
+			cpuReservation = csCPUAllocation.Reservation
+		}
+	}
+
+	var cpuLimit *int64
+	if csCPUAllocation.Limit != nil {
+		if ciCPUAllocation == nil || ciCPUAllocation.Limit == nil || *ciCPUAllocation.Limit != *csCPUAllocation.Limit {
+			cpuLimit = csCPUAllocation.Limit
+		}
+	}
+
+	var cpuShares *vimtypes.SharesInfo
+	if csCPUAllocation.Shares != nil {
+		if ciCPUAllocation == nil || ciCPUAllocation.Shares == nil ||
+			ciCPUAllocation.Shares.Level != csCPUAllocation.Shares.Level ||
+			(csCPUAllocation.Shares.Level == vimtypes.SharesLevelCustom && ciCPUAllocation.Shares.Shares != csCPUAllocation.Shares.Shares) {
+			cpuShares = csCPUAllocation.Shares
+		}
+	}
+
+	if cpuReservation != nil || cpuLimit != nil || cpuShares != nil {
+		outCS.CpuAllocation = &vimtypes.ResourceAllocationInfo{}
+
+		if cpuReservation != nil {
+			outCS.CpuAllocation.Reservation = cpuReservation
+		}
+
+		if cpuLimit != nil {
+			outCS.CpuAllocation.Limit = cpuLimit
+		}
+
+		if cpuShares != nil {
+			outCS.CpuAllocation.Shares = cpuShares
+		}
+	}
+}
+
+// compareCPUHotAddOrRemove compares CPU hot add and remove enabled.
+func compareCPUHotAddOrRemove(
+	ci vimtypes.VirtualMachineConfigInfo,
+	cs vimtypes.VirtualMachineConfigSpec,
+	outCS *vimtypes.VirtualMachineConfigSpec) {
+	cmpPtr(ci.CpuHotAddEnabled, cs.CpuHotAddEnabled, &outCS.CpuHotAddEnabled)
+	cmpPtr(ci.CpuHotRemoveEnabled, cs.CpuHotRemoveEnabled, &outCS.CpuHotRemoveEnabled)
+}
+
+// compareCPUPerfCounter compares virtual CPU performance counter enablement.
+func compareCPUPerfCounter(
+	ci vimtypes.VirtualMachineConfigInfo,
+	cs vimtypes.VirtualMachineConfigSpec,
+	outCS *vimtypes.VirtualMachineConfigSpec) {
+	cmpPtr(ci.VPMCEnabled, cs.VPMCEnabled, &outCS.VPMCEnabled)
+}
+
+// compareCPUAffinity compares CPU affinity settings in ConfigSpec.
+func compareCPUAffinity(
+	ci vimtypes.VirtualMachineConfigInfo,
+	cs vimtypes.VirtualMachineConfigSpec,
+	outCS *vimtypes.VirtualMachineConfigSpec) {
+
+	if cs.CpuAffinity == nil {
+		return
+	}
+
+	if ci.CpuAffinity == nil {
+		outCS.CpuAffinity = cs.CpuAffinity
+	}
+
+	if ci.CpuAffinity != nil {
+		slices.Sort(ci.CpuAffinity.AffinitySet)
+		slices.Sort(cs.CpuAffinity.AffinitySet)
+		if !reflect.DeepEqual(ci.CpuAffinity.AffinitySet, cs.CpuAffinity.AffinitySet) {
+			outCS.CpuAffinity = cs.CpuAffinity
+		}
+	}
+}
+
+// compareLatencySensitivity compares the latency-sensitivity of the virtual machine.
+func compareLatencySensitivity(
+	ci vimtypes.VirtualMachineConfigInfo,
+	cs vimtypes.VirtualMachineConfigSpec,
+	outCS *vimtypes.VirtualMachineConfigSpec) {
+
+	if cs.LatencySensitivity == nil {
+		return
+	}
+
+	if ci.LatencySensitivity == nil ||
+		ci.LatencySensitivity.Sensitivity != cs.LatencySensitivity.Sensitivity ||
+		ci.LatencySensitivity.Level != cs.LatencySensitivity.Level {
+		outCS.LatencySensitivity = &vimtypes.LatencySensitivity{
+			Level: cs.LatencySensitivity.Level,
+			// deprecated since vsphere 5.5
+			//Sensitivity: cs.LatencySensitivity.Sensitivity,
+		}
+	}
 }
 
 func cmp[T comparable](a, b T, c *T) {

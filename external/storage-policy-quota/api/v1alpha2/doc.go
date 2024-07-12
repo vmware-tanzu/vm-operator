9628af3f03fa874c762d00b9130f47bfb22a4ce4
Sreyas Natarajan <sreyasn@vmware.com>
2024-07-12 13:47:41 -0700

    Revert "Storage Policy Quota v1alpha2 APIs"
    This reverts commit 621991ec7887b5cbd8cbd9fc100a49844861aabf.

--- external/storage-policy-quota/api/v1alpha2/doc.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024 VMware, Inc. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// +kubebuilder:object:generate=true
-// +groupName=cns.vmware.com
-
-// Package v1alpha2 contains API Schema definitions for the cns v1alpha2 API
-// group.
-package v1alpha2

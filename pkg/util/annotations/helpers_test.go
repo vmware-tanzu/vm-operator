6b2271974b9aaf7f187d6b3aa3ac8e137dc379c6
Arunesh Pandey <aruneshpa@gmail.com>
2024-07-26 09:46:32 -0700

    Introduce an annotation for unconditional opt into backup (#637)
    Today, we have certain exclusion rules in place that govern whether or
    not we persist the configuration of VirtualMachine in the ExtraConfig
    for use by backup/restore or disaster recovery solutions.  However,
    there might be cases where an entity wants to explicitly opt into
    backup.
    
    This change introduces an annotation that can be placed on the
    VirtualMachine resource that lets VM operator skip the exclusion
    checks when determining whether or not to store the VM's config in
    ExtraConfig.

---
+++ pkg/util/annotations/helpers_test.go
@@ -0,0 +1,31 @@
+// Copyright (c) 2024 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package annotations_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	vmopv1 "github.com/vmware-tanzu/vm-operator/api/v1alpha3"
+	"github.com/vmware-tanzu/vm-operator/pkg/util/annotations"
+)
+
+var _ = DescribeTable(
+	"HasForceEnableBackup",
+	func(in map[string]string, out bool) {
+		vm := &vmopv1.VirtualMachine{
+			ObjectMeta: metav1.ObjectMeta{
+				Annotations: in,
+			},
+		}
+		actual := annotations.HasForceEnableBackup(vm)
+		Expect(actual).To(Equal(out))
+	},
+	Entry("nil", nil, false),
+	Entry("not present", map[string]string{"foo": "bar"}, false),
+	Entry("present but empty ", map[string]string{vmopv1.ForceEnableBackupAnnotation: ""}, true),
+	Entry("present and not empty ", map[string]string{vmopv1.ForceEnableBackupAnnotation: "true"}, true),
+)

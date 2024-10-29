c138f8b2411aba42b20119fffe196699cc2df201
akutz <akutz@vmware.com>
2024-10-29 14:39:08 -0500

    Support building cross-arch images
    This patch adds support for building images targeting an
    architecture different than the host's by specifying
    GOARCH with the desired architecture. For example, the
    following command will build a linux/amd64 image, even
    when executed on an Apple silicon mac:
    
        GOARCH=amd64 make image-build
    
    Please note, this feature requires either Podman or a
    version of Docker that supports buildx.
    
    This patch also:
    
    - Renames all the "docker-*" targets to "image-*". The docker
      aliases are retained for backwards-compatibility.
    
    - Saves any images built with the "image-build" target to the
      file "artifacts/vmoperator-controller-GOOS_GOARCH.tar.

--- hack/build-container.sh
+++ hack/build-container.sh
@@ -15,27 +15,87 @@ IMAGE=
 IMAGE_TAG=
 BUILD_NUMBER=
 
+if [ -z "${GOHOSTOS:-}" ] || [ -z "${GOHOSTARCH:-}" ]; then
+    if ! command -v go >/dev/null 2>&1; then
+        echo "GOHOSTOS and/or GOHOSTARCH are unset and golang is not detected" 1>&2
+        exit 1
+    fi
+fi
+
+GOHOSTOS="${GOHOSTOS:-$(go env GOHOSTOS)}"
+GOHOSTARCH="${GOHOSTARCH:-$(go env GOHOSTARCH)}"
+
+GOOS=linux
+GOARCH="${GOARCH:-${GOHOSTARCH}}"
+
 usage() {
-    echo "Usage: $(basename "${0}") -i image -t imageTag [-n buildNumber] [-c commit] [-b branch] [-v version]"
+    echo "Usage: $(basename "${0}") -i image -t imageTag [-n buildNumber] [-c commit] [-b branch] [-v version] [-o outFile]"
     exit 1
 }
 
-build() {
-    GOOS=linux
-    echo "GOOS=${GOOS} GOARCH=${GOARCH}"
+build_docker() {
+    if [ "${GOOS}" = "${GOHOSTOS}" ] && [ "${GOARCH}" = "${GOHOSTARCH}" ]; then
+
+        "${CRI_BIN}" build . \
+            -t "${IMAGE}":"${IMAGE_TAG}" \
+            -t "${IMAGE}":"${BUILD_NUMBER}" \
+            -t "${IMAGE}":"${BUILD_VERSION}" \
+            --build-arg BUILD_BRANCH="${BUILD_BRANCH}" \
+            --build-arg BUILD_COMMIT="${BUILD_COMMIT}" \
+            --build-arg BUILD_NUMBER="${BUILD_NUMBER}" \
+            --build-arg BUILD_VERSION="${BUILD_VERSION}"
+
+    elif docker buildx version >/dev/null 2>&1; then
+
+        "${CRI_BIN}" buildx build . \
+            -t "${IMAGE}":"${IMAGE_TAG}" \
+            -t "${IMAGE}":"${BUILD_NUMBER}" \
+            -t "${IMAGE}":"${BUILD_VERSION}" \
+            --platform "${GOOS}/${GOARCH}" \
+            --build-arg BUILD_BRANCH="${BUILD_BRANCH}" \
+            --build-arg BUILD_COMMIT="${BUILD_COMMIT}" \
+            --build-arg BUILD_NUMBER="${BUILD_NUMBER}" \
+            --build-arg BUILD_VERSION="${BUILD_VERSION}"
+
+    else
+
+        echo "docker buildx is not installed" 1>&2
+        exit 1
+
+    fi
+}
+
+build_podman() {
     "${CRI_BIN}" build . \
         -t "${IMAGE}":"${IMAGE_TAG}" \
         -t "${IMAGE}":"${BUILD_NUMBER}" \
         -t "${IMAGE}":"${BUILD_VERSION}" \
+        --os "${GOOS}" --arch "${GOARCH}" \
         --build-arg BUILD_BRANCH="${BUILD_BRANCH}" \
         --build-arg BUILD_COMMIT="${BUILD_COMMIT}" \
         --build-arg BUILD_NUMBER="${BUILD_NUMBER}" \
-        --build-arg BUILD_VERSION="${BUILD_VERSION}" \
-        --build-arg TARGETOS="${GOOS}" \
-        --build-arg TARGETARCH="${GOARCH}"
+        --build-arg BUILD_VERSION="${BUILD_VERSION}"
 }
 
-while getopts ":i:t:n:c:b:v:" opt ; do
+build() {
+    echo "GOOS=${GOOS} GOARCH=${GOARCH}"
+
+    if [[ "${CRI_BIN}" == *"podman"* ]]; then
+        build_podman
+    elif [[ "${CRI_BIN}" == *"docker"* ]]; then
+        build_docker
+    else
+        echo "unsupported cri: '${CRI_BIN}'" 1>&2
+        exit 1
+    fi
+
+    if [ -n "${OUT_FILE:-}" ]; then
+        mkdir -p "$(dirname "${OUT_FILE}")"
+        "${CRI_BIN}" save "${IMAGE}":"${IMAGE_TAG}" -o "${OUT_FILE}"
+    fi
+}
+
+while getopts ":i:t:n:c:b:v:o:" opt ; do
     case $opt in
         "i" ) IMAGE="${OPTARG}" ;;
         "t" ) IMAGE_TAG="${OPTARG}" ;;
@@ -43,6 +103,7 @@ while getopts ":i:t:n:c:b:v:" opt ; do
         "c" ) BUILD_COMMIT="${OPTARG}" ;;
         "n" ) BUILD_NUMBER="${OPTARG}" ;;
         "v" ) BUILD_VERSION="${OPTARG}" ;;
+        "o" ) OUT_FILE="${OPTARG}" ;;
         \? ) usage ;;
     esac
 done

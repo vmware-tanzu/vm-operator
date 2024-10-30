264458809df7923822640638c7730351e5a88b43
akutz <akutz@vmware.com>
2024-10-30 12:27:50 -0500

    Unifiy build process up and downstream
    This patch changes the upstream image-build target and Dockerfile
    to no longer do a staged build, building VM Op inside of the
    container. Instead, the binaries are now built prior to building
    the container.
    
    This, as well as a few other changes to the version variables in
    the Makefile, allow the up and downstream build processes for VM
    Op to be unified. Now the internal process can invoke the following
    to build VM Op from the upstream Makefile:
    
        ADDITIONAL_CRI_BUILD_FLAGS="--network=host" \
        BASE_IMAGE="<INTERNAL_CONTAINER_IMAGE>" \
        BUILD_BRANCH="$(BUILD_BRANCH)" \
        BUILD_COMMIT="$(BUILD_COMMIT)" \
        BUILD_NUMBER="$(BUILD_NUMBER)" \
        BUILD_VERSION="$(BUILD_VERSION)" \
        IMAGE="$(IMAGE)" \
        IMAGE_TAG="$(IMAGE_TAG)" \
        IMAGE_VERSION="$(IMAGE_VERSION)" \
        PRDCT_VERSION="$(PRDCT_VERSION)" \
        make image-build-amd64

--- Dockerfile
+++ Dockerfile
@@ -1,77 +1,7 @@
-# Go version used to build the binaries.
-ARG GO_VERSION=1.23
-
-## Docker image used to build the binaries.
-FROM golang:${GO_VERSION} as builder
-
-
-## --------------------------------------
-## Multi-platform support
-## --------------------------------------
-
-ARG TARGETOS
-ARG TARGETARCH
-
-
-## --------------------------------------
-## Environment variables
-## --------------------------------------
-
-ENV GOOS=${TARGETOS}
-ENV GOARCH=${TARGETARCH}
-
-
-## --------------------------------------
-## Build information
-## --------------------------------------
-
-ARG BUILD_COMMIT
-ARG BUILD_NUMBER
-ARG BUILD_VERSION
-
-ENV BUILD_COMMIT=${BUILD_COMMIT}
-ENV BUILD_NUMBER=${BUILD_NUMBER}
-ENV BUILD_VERSION=${BUILD_VERSION}
-
-
-## --------------------------------------
-## Configure the working directory
-## --------------------------------------
-
-WORKDIR /workspace
-
-
-## --------------------------------------
-## Copy in local sources
-## --------------------------------------
-
-# Copy the sources
-COPY ./ ./
-
-
-## --------------------------------------
-## Build the binaries
-## --------------------------------------
-
-# Build
-RUN make manager-only
-RUN make web-console-validator-only
-
-
-## --------------------------------------
-## Create the manager image
-## --------------------------------------
+ARG BASE_IMAGE=gcr.io/distroless/base-debian12
 
 # Copy the controller-manager into a thin image
-FROM gcr.io/distroless/static-debian11
-
-
-## --------------------------------------
-## Multi-platform support
-## --------------------------------------
-
-ARG TARGETOS
-ARG TARGETARCH
+FROM ${BASE_IMAGE}
 
 
 ## --------------------------------------
@@ -96,7 +26,12 @@ ARG BUILD_VERSION
 ## Image labels
 ## --------------------------------------
 
-LABEL buildNumber=$BUILD_NUMBER commit=$BUILD_COMMIT branch=$BUILD_BRANCH version=$BUILD_VERSION
+LABEL branch="${BUILD_BRANCH}" \
+      buildNumber="${BUILD_NUMBER}" \
+      commit="${BUILD_COMMIT}" \
+      name="VM Operator" \
+      vendor="Broadcom" \
+      version="${BUILD_VERSION}"
 
 
 ## --------------------------------------
@@ -104,7 +39,7 @@ LABEL buildNumber=$BUILD_NUMBER commit=$BUILD_COMMIT branch=$BUILD_BRANCH versio
 ## --------------------------------------
 
 WORKDIR /
-COPY --from=builder /workspace/bin/manager .
-COPY --from=builder /workspace/bin/web-console-validator .
+COPY ./bin/manager .
+COPY ./bin/web-console-validator .
 USER nobody
 ENTRYPOINT ["/manager"]
--- hack/build-container.sh
+++ hack/build-container.sh
@@ -6,88 +6,68 @@ set -o nounset
 set -x
 
 
-CRI_BIN="${CRI_BIN:-}"
-if [ -z "${CRI_BIN:-}" ]; then
-  CRI_BIN="$(command -v podman 2>/dev/null || command -v docker 2>/dev/null)"
-fi
+################################################################################
+##                                     functions
+################################################################################
 
-IMAGE=
-IMAGE_TAG=
-BUILD_NUMBER=
-
-if [ -z "${GOHOSTOS:-}" ] || [ -z "${GOHOSTARCH:-}" ]; then
-    if ! command -v go >/dev/null 2>&1; then
-        echo "GOHOSTOS and/or GOHOSTARCH are unset and golang is not detected" 1>&2
-        exit 1
-    fi
-fi
-
-GOHOSTOS="${GOHOSTOS:-$(go env GOHOSTOS)}"
-GOHOSTARCH="${GOHOSTARCH:-$(go env GOHOSTARCH)}"
-
-GOOS=linux
-GOARCH="${GOARCH:-${GOHOSTARCH}}"
 
 usage() {
-    echo "Usage: $(basename "${0}") -i image -t imageTag [-n buildNumber] [-c commit] [-b branch] [-v version] [-o outFile]"
+    echo "Usage: $(basename "${0}") -i image -t imageTag -v version [-n buildNumber] [-c commit] [-b branch] [-V imageVersion] [-o outFile] [-B baseImage] [-C cri]"
     exit 1
 }
 
-build_docker() {
-    if [ "${GOOS}" = "${GOHOSTOS}" ] && [ "${GOARCH}" = "${GOHOSTARCH}" ]; then
+init_docker_build_flags() {
+    if [ "${GOARCH}" = "${GOHOSTARCH}" ]; then
+        return 0
+    fi
 
-        "${CRI_BIN}" build . \
-            -t "${IMAGE}":"${IMAGE_TAG}" \
-            -t "${IMAGE}":"${BUILD_NUMBER}" \
-            -t "${IMAGE}":"${BUILD_VERSION}" \
-            --build-arg BUILD_BRANCH="${BUILD_BRANCH}" \
-            --build-arg BUILD_COMMIT="${BUILD_COMMIT}" \
-            --build-arg BUILD_NUMBER="${BUILD_NUMBER}" \
-            --build-arg BUILD_VERSION="${BUILD_VERSION}"
-
-    elif docker buildx version >/dev/null 2>&1; then
-
-        "${CRI_BIN}" buildx build . \
-            -t "${IMAGE}":"${IMAGE_TAG}" \
-            -t "${IMAGE}":"${BUILD_NUMBER}" \
-            -t "${IMAGE}":"${BUILD_VERSION}" \
-            --platform "${GOOS}/${GOARCH}" \
-            --build-arg BUILD_BRANCH="${BUILD_BRANCH}" \
-            --build-arg BUILD_COMMIT="${BUILD_COMMIT}" \
-            --build-arg BUILD_NUMBER="${BUILD_NUMBER}" \
-            --build-arg BUILD_VERSION="${BUILD_VERSION}"
-
+    if docker buildx version >/dev/null 2>&1; then
+        BUILD_SUB_COMMAND="buildx build"
+        BUILD_FLAGS+=(--platform "${GOOS}/${GOARCH}")
     else
-
         echo "docker buildx is not installed" 1>&2
         exit 1
-
     fi
 }
 
-build_podman() {
-    "${CRI_BIN}" build . \
-        -t "${IMAGE}":"${IMAGE_TAG}" \
-        -t "${IMAGE}":"${BUILD_NUMBER}" \
-        -t "${IMAGE}":"${BUILD_VERSION}" \
-        --os "${GOOS}" --arch "${GOARCH}" \
-        --build-arg BUILD_BRANCH="${BUILD_BRANCH}" \
-        --build-arg BUILD_COMMIT="${BUILD_COMMIT}" \
-        --build-arg BUILD_NUMBER="${BUILD_NUMBER}" \
-        --build-arg BUILD_VERSION="${BUILD_VERSION}"
+init_podman_build_flags() {
+    BUILD_FLAGS+=(--os "${GOOS}")
+    BUILD_FLAGS+=(--arch "${GOARCH}")
+}
+
+init_build_flags() {
+    BUILD_FLAGS+=(-t "${IMAGE}":"${IMAGE_TAG}")
+    BUILD_FLAGS+=(-t "${IMAGE}":"${BUILD_NUMBER}")
+    BUILD_FLAGS+=(-t "${IMAGE}":"${IMAGE_VERSION}")
+    BUILD_FLAGS+=(--build-arg BUILD_BRANCH="${BUILD_BRANCH}")
+    BUILD_FLAGS+=(--build-arg BUILD_COMMIT="${BUILD_COMMIT}")
+    BUILD_FLAGS+=(--build-arg BUILD_NUMBER="${BUILD_NUMBER}")
+    BUILD_FLAGS+=(--build-arg BUILD_VERSION="${BUILD_VERSION}")
+
+    if [ -n "${BASE_IMAGE:-}" ]; then
+        BUILD_FLAGS+=(--build-arg BASE_IMAGE="${BASE_IMAGE}")
+    fi
+
+    if [[ "${CRI_BIN}" == *"podman"* ]]; then
+        init_podman_build_flags
+    elif [[ "${CRI_BIN}" == *"docker"* ]]; then
+        init_docker_build_flags
+    else
+        echo "unsupported cri: '${CRI_BIN}'" 1>&2
+        exit 1
+    fi
+
+    if [ -n "${ADDITIONAL_CRI_BUILD_FLAGS:-}" ]; then
+        BUILD_FLAGS+=(${ADDITIONAL_CRI_BUILD_FLAGS:-})
+    fi
 }
 
 build() {
     echo "GOOS=${GOOS} GOARCH=${GOARCH}"
 
-    if [[ "${CRI_BIN}" == *"podman"* ]]; then
-        build_podman
-    elif [[ "${CRI_BIN}" == *"docker"* ]]; then
-        build_docker
-    else
-        echo "unsupported cri: '${CRI_BIN}'" 1>&2
-        exit 1
-    fi
+    init_build_flags
+
+    "${CRI_BIN}" ${BUILD_SUB_COMMAND:-build} . "${BUILD_FLAGS[@]+"${BUILD_FLAGS[@]}"}"
 
     if [ -n "${OUT_FILE:-}" ]; then
         mkdir -p "$(dirname "${OUT_FILE}")"
@@ -95,7 +75,23 @@ build() {
     fi
 }
 
-while getopts ":i:t:n:c:b:v:o:" opt ; do
+
+################################################################################
+##                                      main
+################################################################################
+
+if [ -z "${GOHOSTARCH:-}" ]; then
+    if ! command -v go >/dev/null 2>&1; then
+        echo "GOHOSTARCH is unset and golang is not detected" 1>&2
+        exit 1
+    fi
+    GOHOSTARCH="$(go env GOHOSTARCH)"
+fi
+
+GOOS=linux
+GOARCH="${GOARCH:-${GOHOSTARCH}}"
+
+while getopts ":i:t:n:c:b:v:V:o:B:C:" opt ; do
     case $opt in
         "i" ) IMAGE="${OPTARG}" ;;
         "t" ) IMAGE_TAG="${OPTARG}" ;;
@@ -103,7 +99,10 @@ while getopts ":i:t:n:c:b:v:o:" opt ; do
         "c" ) BUILD_COMMIT="${OPTARG}" ;;
         "n" ) BUILD_NUMBER="${OPTARG}" ;;
         "v" ) BUILD_VERSION="${OPTARG}" ;;
+        "V" ) IMAGE_VERSION="${OPTARG}" ;;
         "o" ) OUT_FILE="${OPTARG}" ;;
+        "B" ) BASE_IMAGE="${OPTARG}" ;;
+        "C" ) CRI_BIN="${OPTARG}" ;;
         \? ) usage ;;
     esac
 done
@@ -114,12 +113,17 @@ if [[ $# -ne 0 ]] ; then
     usage
 fi
 
-if [[ -z "${IMAGE:-}" || -z "${IMAGE_TAG:-}" ]] ; then
+if [[ -z "${IMAGE:-}" || -z "${IMAGE_TAG:-}" || -z "${BUILD_VERSION}" ]] ; then
     usage
+fi
+
+if [ -z "${CRI_BIN:-}" ]; then
+    CRI_BIN="$(command -v podman 2>/dev/null || command -v docker 2>/dev/null)"
 fi
 
 BUILD_NUMBER="${BUILD_NUMBER:-0}"
 BUILD_COMMIT="${BUILD_COMMIT:-$(git rev-parse HEAD)}"
 BUILD_BRANCH="${BUILD_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}"
+IMAGE_VERSION="${IMAGE_VERSION:-${BUILD_VERSION//+/-}}"
 
 build

vmware-tanzu/vm-operator

commit 14c1ac32bf3e98eb54ccbcaa4b55cf19823482c4

Do not CBLO if VM watcher service fails
This patch updates the behavior of the VM watcher service to no
longer return an error if there is an issue starting. While this
is the correct behavior, it also results in CBLO for the VM Op
pod if the reason for the error is external and not resolved.

For example, if a user configures the host name of vCenter to be
invalid, the VM Op pod will be in CBLO until this is resolved.
Again, this is technically correct, and resolves itself once the
user fixes the misconfigured setting.

However, the VM Op pod becoming unavailable on a single-node
Supervisor also means no webhooks are running, and this would be
bad.

services/vm-watcher/vm_watcher_service.go
@@ -84,10 +84,18 @@ func (s Service) Start(ctx context.Context) error {
 			// error, then do not treat the error as fatal. This allows the
 			// loop to run again, kicking off another watcher with what should
 			// be updated credentials.
-			if !vsphereclient.IsInvalidLogin(err) &&
-				!vsphereclient.IsNotAuthenticatedError(err) {
+			if vsphereclient.IsInvalidLogin(err) ||
+				vsphereclient.IsNotAuthenticatedError(err) {
 
-				return err
+				logger.V(4).Error(
+					err,
+					"Authn/authz issue trying start vm watcher service")
+
+			} else {
+				// Log unexpected error.
+				logger.Error(
+					err,
+					"Unexpected error trying to start vm watcher service")
 			}
 		}
 	}

services/vm-watcher/vm_watcher_service_test.go
@@ -221,8 +221,12 @@ var _ = Describe(
 		})
 
 		When("the port is invalid", func() {
+			var (
+				oldPort string
+			)
+
 			JustBeforeEach(func() {
-				vcSimCtx.Suite.StartErrExpected = true
+				vcSimCtx.Suite.StartErrExpected = false
 
 				Eventually(func(g Gomega) {
 					vsClientMu.RLock()
@@ -236,6 +240,7 @@ var _ = Describe(
 					vsClientMu.Lock()
 					defer vsClientMu.Unlock()
 
+					oldPort = vcSimCtx.VCClientConfig.Port
 					vcSimCtx.VCClientConfig.Port = "1"
 				})
 
@@ -247,10 +252,31 @@ var _ = Describe(
 				})
 			})
 
-			Specify("the service should fail", func() {
+			Specify("the service should be restarted once the port is fixed", func() {
 				Eventually(func(g Gomega) {
-					g.Expect(vcSimCtx.Suite.StartErr()).To(HaveOccurred())
-					g.Expect(atomic.LoadInt32(&numNewClientCalls)).To(Equal(int32(2)))
+					vsClientMu.RLock()
+					defer vsClientMu.RUnlock()
+
+					g.Expect(vsClient.Valid()).To(BeFalse())
+					g.Expect(atomic.LoadInt32(&numNewClientCalls)).To(BeNumerically(">=", int32(2)))
+				}).Should(Succeed())
+
+				By("fix the port", func() {
+					vsClientMu.Lock()
+					defer vsClientMu.Unlock()
+
+					vcSimCtx.VCClientConfig.Port = oldPort
+				})
+
+				Eventually(func(g Gomega) {
+					vsClientMu.RLock()
+					defer vsClientMu.RUnlock()
+
+					g.Expect(atomic.LoadInt32(&numNewClientCalls)).To(BeNumerically(">=", int32(3)))
+				}).Should(Succeed())
+
+				Consistently(func(g Gomega) {
+					g.Expect(vcSimCtx.Suite.StartErr()).ToNot(HaveOccurred())
 				}).Should(Succeed())
 			})
 		})

commit d03b453ba6824f51da11d307fa25763c8580573d

VM Watcher Service improvements
When building the initial container MoID list, include the IDs so
that we can match the Add() and Remove() of the same ID. We had a
bug here in that if the Zone was initially added to a NS when at
start up time, any later removal of the Zone from a NS would remove
the watch of that Zone across all namespaces. This allows the watcher
Add() and Remove() to be idempotent which is needed for some infra
Zone controller described later.

Tweak how the infra Zone controller handles the finalizer. The VC
watcher itself is ephemeral since it will go away when the VC client
is closed. But we need the Zone finalizer so that we can remove it
from the watcher when the Zone goes away. Therefore, call Add() or
Remove() on the Zone regardless if the finalizer was already present
or not.

Better handle a Zone with a stale FolderMoID: if a folder does not
exist on VC, the watcher will fail to start. We filter out invalid
container MoIDs to the watcher; the Zone controller will later keep
retrying to add that FolderMoID to the watcher.

Much of change is fallout from 14c1ac32b when we changed the VM
watcher service to not os.Exit() whenever the watcher could not
be started.

controllers/infra/zone/zone_controller.go
@@ -123,23 +123,26 @@ func (r *Reconciler) ReconcileDelete(
 	ctx context.Context,
 	obj *topologyv1.Zone) (ctrl.Result, error) {
 
-	if controllerutil.ContainsFinalizer(obj, Finalizer) {
-		if val := obj.Spec.ManagedVMs.FolderMoID; val != "" {
-			if err := watcher.Remove(
-				ctx,
-				vimtypes.ManagedObjectReference{
-					Type:  "Folder",
-					Value: val,
-				},
-				fmt.Sprintf("%s/%s", obj.Namespace, obj.Name)); err != nil {
+	if val := obj.Spec.ManagedVMs.FolderMoID; val != "" {
+		if err := watcher.Remove(
+			ctx,
+			vimtypes.ManagedObjectReference{
+				Type:  "Folder",
+				Value: val,
+			},
+			fmt.Sprintf("%s/%s", obj.Namespace, obj.Name)); err != nil {
 
-				if !errors.Is(err, watcher.ErrAsyncSignalDisabled) {
-					return ctrl.Result{}, err
-				}
+			if !errors.Is(err, watcher.ErrAsyncSignalDisabled) {
+				// We don't ignore watcher.ErrNoWatcher here to interlock with the vm watcher
+				// service that is in the process of restarting the watcher. This does mean
+				// that if watcher cannot start like because of invalid VC creds the finalizer
+				// won't be removed.
+				return ctrl.Result{}, err
 			}
 		}
-		controllerutil.RemoveFinalizer(obj, Finalizer)
 	}
+	controllerutil.RemoveFinalizer(obj, Finalizer)
+
 	return ctrl.Result{}, nil
 }
 
@@ -147,24 +150,25 @@ func (r *Reconciler) ReconcileNormal(
 	ctx context.Context,
 	obj *topologyv1.Zone) (ctrl.Result, error) {
 
-	if !controllerutil.ContainsFinalizer(obj, Finalizer) {
-		// The finalizer is not added until we are able to successfully
-		// add a watch to the zone's VM Service folder.
-		if val := obj.Spec.ManagedVMs.FolderMoID; val != "" {
-			if err := watcher.Add(
-				ctx,
-				vimtypes.ManagedObjectReference{
-					Type:  "Folder",
-					Value: val,
-				},
-				fmt.Sprintf("%s/%s", obj.Namespace, obj.Name)); err != nil {
+	if controllerutil.AddFinalizer(obj, Finalizer) {
+		// Ensure the finalizer is present before watching this zone.
+		return ctrl.Result{}, nil
+	}
 
-				if !errors.Is(err, watcher.ErrAsyncSignalDisabled) {
-					return ctrl.Result{}, err
-				}
+	if val := obj.Spec.ManagedVMs.FolderMoID; val != "" {
+		if err := watcher.Add(
+			ctx,
+			vimtypes.ManagedObjectReference{
+				Type:  "Folder",
+				Value: val,
+			},
+			fmt.Sprintf("%s/%s", obj.Namespace, obj.Name)); err != nil {
+
+			if !errors.Is(err, watcher.ErrAsyncSignalDisabled) {
+				return ctrl.Result{}, err
 			}
-			controllerutil.AddFinalizer(obj, Finalizer)
 		}
 	}
+
 	return ctrl.Result{}, nil
 }

pkg/util/vsphere/watcher/watcher.go
@@ -118,10 +118,12 @@ type Watcher struct {
 	lv *view.ListView
 	cv map[moRef]*view.ContainerView
 
-	// cvr is used to count the number of times a container has been added to
-	// the list view. If the Remove function is called on a container with a ref
-	// count of one, then the container will be removed from the list view and
+	// cvr is used to keep track of what opaque IDs are using the list view of
+	// each container. If the Remove function is called on a container with the
+	// last ID, then the container will be removed from the list view and
 	// destroyed.
+	// cvr will have a container moref only if cv does, so cv should be checked
+	// first.
 	cvr map[moRef]map[string]struct{}
 
 	ignoredExtraConfigKeys map[string]struct{}
@@ -158,7 +160,7 @@ func newWatcher(
 	watchedPropertyPaths []string,
 	additionalIgnoredExtraConfigKeys []string,
 	lookupNamespacedName lookupNamespacedNameFn,
-	containerRefs ...moRef) (*Watcher, error) {
+	containerRefsWithIDs map[moRef][]string) (*Watcher, error) {
 
 	if watchedPropertyPaths == nil {
 		watchedPropertyPaths = DefaultWatchedPropertyPaths()
@@ -172,7 +174,7 @@ func newWatcher(
 
 	// For each container reference, create a container view and add it to
 	// the list view's initial list of members.
-	cvs, cvr, err := toContainerViewMap(ctx, vm, containerRefs...)
+	cvs, cvr, err := toContainerViewMap(ctx, vm, containerRefsWithIDs)
 	if err != nil {
 		return nil, err
 	}
@@ -227,15 +229,15 @@ func (w *Watcher) close() {
 
 // Start begins watching a vSphere server for updates to VM Service managed VMs.
 // If watchedPropertyPaths is nil, DefaultWatchedPropertyPaths will be used.
-// The containerRefs parameter may be used to start the watcher with an initial
-// list of entities to watch.
+// The containerRefsWithIDs parameter may be used to start the watcher with an
+// initial list of entities to watch.
 func Start(
 	ctx context.Context,
 	client *vim25.Client,
 	watchedPropertyPaths []string,
 	additionalIgnoredExtraConfigKeys []string,
 	lookupNamespacedName lookupNamespacedNameFn,
-	containerRefs ...moRef) (*Watcher, error) {
+	containerRefsWithIDs map[moRef][]string) (*Watcher, error) {
 
 	logger := logr.FromContextOrDiscard(ctx).WithName("vSphereWatcher")
 
@@ -247,7 +249,7 @@ func Start(
 		watchedPropertyPaths,
 		additionalIgnoredExtraConfigKeys,
 		lookupNamespacedName,
-		containerRefs...)
+		containerRefsWithIDs)
 	if err != nil {
 		return nil, err
 	}
@@ -512,9 +514,9 @@ func (w *Watcher) remove(_ context.Context, ref moRef, id string) error {
 		return nil
 	}
 
-	// Only remove the container from the list view if it has a ref count of
-	// one.
-	if len(w.cvr[ref]) > 1 {
+	// Only remove the container from the list view if this ref is the
+	// last user, and make sure that this ID is actually in use.
+	if _, ok := w.cvr[ref][id]; !ok || len(w.cvr[ref]) > 1 {
 		delete(w.cvr[ref], id)
 		return nil
 	}
@@ -529,7 +531,7 @@ func (w *Watcher) remove(_ context.Context, ref moRef, id string) error {
 	}
 
 	delete(w.cv, ref)
-	delete(w.cvr[ref], id)
+	delete(w.cvr, ref)
 
 	return nil
 }
@@ -537,34 +539,35 @@ func (w *Watcher) remove(_ context.Context, ref moRef, id string) error {
 func toContainerViewMap(
 	ctx context.Context,
 	vm *view.Manager,
-	containerRefs ...moRef) (map[moRef]*view.ContainerView, map[moRef]map[string]struct{}, error) {
+	containerRefsWithIDs map[moRef][]string) (map[moRef]*view.ContainerView, map[moRef]map[string]struct{}, error) {
 
 	var (
 		cvMap     = map[moRef]*view.ContainerView{}
 		cvRefsMap = map[moRef]map[string]struct{}{}
 	)
 
-	if len(containerRefs) == 0 {
+	if len(containerRefsWithIDs) == 0 {
 		return cvMap, cvRefsMap, nil
 	}
 
 	var resultErr error
-	for i := range containerRefs {
-		if _, ok := cvMap[containerRefs[i]]; ok {
-			// Ignore duplicates.
-			continue
-		}
+	for moref, ids := range containerRefsWithIDs {
 		cv, err := vm.CreateContainerView(
 			ctx,
-			containerRefs[i],
+			moref,
 			[]string{virtualMachineType},
 			true)
 		if err != nil {
 			resultErr = err
 			break
 		}
-		cvMap[containerRefs[i]] = cv
-		cvRefsMap[containerRefs[i]] = map[string]struct{}{}
+		cvMap[moref] = cv
+
+		idSet := make(map[string]struct{}, len(ids))
+		for _, id := range ids {
+			idSet[id] = struct{}{}
+		}
+		cvRefsMap[moref] = idSet
 	}
 
 	if resultErr != nil {

services/vm-watcher/vm_watcher_service.go
@@ -6,15 +6,21 @@ package vmwatcher
 
 import (
 	"context"
+	"fmt"
+	"maps"
+	"slices"
 
 	"github.com/go-logr/logr"
+	"github.com/vmware/govmomi/property"
 	vimtypes "github.com/vmware/govmomi/vim25/types"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	vmopv1 "github.com/vmware-tanzu/vm-operator/api/v1alpha3"
+	zonectrl "github.com/vmware-tanzu/vm-operator/controllers/infra/zone"
 	topologyv1 "github.com/vmware-tanzu/vm-operator/external/tanzu-topology/api/v1alpha1"
 	pkgcfg "github.com/vmware-tanzu/vm-operator/pkg/config"
 	pkgctx "github.com/vmware-tanzu/vm-operator/pkg/context"
@@ -98,38 +104,103 @@ func (s Service) Start(ctx context.Context) error {
 					err,
 					"Unexpected error trying to start vm watcher service")
 			}
+
+			// TODO: We might want to sleep a bit here so we don't spin if like we
+			// cannot get a VC client.
 		}
 	}
 	return ctx.Err()
 }
 
-var emptyResult watcher.Result
+func (s Service) vmFolderMoRefWithIDs(
+	ctx context.Context,
+	vcClient *vsphereclient.Client) (map[vimtypes.ManagedObjectReference][]string, error) {
 
-func (s Service) vmFolderRefs(ctx context.Context) ([]vimtypes.ManagedObjectReference, error) {
 	// Get a list of all the folders that can contain VM Service VMs.
 	var (
-		zones topologyv1.ZoneList
-		frefs []vimtypes.ManagedObjectReference
-		moids = map[string]struct{}{}
+		zones                 topologyv1.ZoneList
+		objSet                []vimtypes.ObjectSpec
+		moids                 = map[string][]string{}
+		zonesWithoutFinalizer []topologyv1.Zone
 	)
+
 	if err := s.Client.List(ctx, &zones); err != nil {
 		return nil, err
 	}
+
 	for i := range zones.Items {
 		z := zones.Items[i]
+
 		if v := z.Spec.ManagedVMs.FolderMoID; v != "" {
 			if _, ok := moids[v]; !ok {
-				moids[v] = struct{}{}
-				frefs = append(frefs, vimtypes.ManagedObjectReference{
-					Type:  "Folder",
-					Value: v,
+				moids[v] = make([]string, 0, 1)
+
+				objSet = append(objSet, vimtypes.ObjectSpec{
+					Obj: vimtypes.ManagedObjectReference{
+						Type:  "Folder",
+						Value: v,
+					},
 				})
+			}
+			moids[v] = append(moids[v], fmt.Sprintf("%s/%s", z.Namespace, z.Name))
+
+			if !controllerutil.ContainsFinalizer(&z, zonectrl.Finalizer) {
+				zonesWithoutFinalizer = append(zonesWithoutFinalizer, z)
 			}
 		}
 	}
 
-	return frefs, nil
+	if len(objSet) == 0 {
+		return nil, nil
+	}
+
+	// Filter the folder MoIDs for ones that exist on VC. The watcher will fail to start if
+	// any of the initial MoIDs don't exist but need to watcher to start. For any stale Zones
+	// the infra Zone controller will keep retrying to add their folder to the watcher.
+	pc := property.DefaultCollector(vcClient.VimClient())
+	res, err := pc.RetrieveProperties(ctx, vimtypes.RetrieveProperties{
+		SpecSet: []vimtypes.PropertyFilterSpec{
+			{
+				PropSet: []vimtypes.PropertySpec{
+					{
+						Type: "Folder",
+					},
+				},
+				ObjectSet:                     objSet,
+				ReportMissingObjectsInResults: vimtypes.NewBool(true),
+			},
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get managed VMs folder properties: %w", err)
+	}
+
+	moRefWithIDs := make(map[vimtypes.ManagedObjectReference][]string, len(res.Returnval))
+	for _, o := range res.Returnval {
+		moRefWithIDs[o.Obj] = moids[o.Obj.Value]
+	}
+
+	// For Zones with a valid FolderMoID but without the finalizer, add it before starting the
+	// watcher so the infra Zone controller will remove it from the watcher.
+	for _, z := range zonesWithoutFinalizer {
+		moref := vimtypes.ManagedObjectReference{
+			Type:  "Folder",
+			Value: z.Spec.ManagedVMs.FolderMoID,
+		}
+
+		if _, ok := moRefWithIDs[moref]; ok {
+			zPatch := ctrlclient.MergeFromWithOptions(z.DeepCopy(), ctrlclient.MergeFromWithOptimisticLock{})
+			z.Finalizers = append(z.Finalizers, zonectrl.Finalizer)
+			if err := s.Client.Patch(ctx, &z, zPatch); err != nil {
+				return nil, fmt.Errorf("failed to add finalizer: %w", err)
+			}
+		}
+	}
+
+	return moRefWithIDs, nil
 }
+
+var emptyResult watcher.Result
 
 func (s Service) waitForChanges(ctx context.Context) error {
 	var (
@@ -143,11 +214,11 @@ func (s Service) waitForChanges(ctx context.Context) error {
 	}
 	logger.Info("Got vsphere client")
 
-	refs, err := s.vmFolderRefs(ctx)
+	moRefWithIDs, err := s.vmFolderMoRefWithIDs(ctx, vcClient)
 	if err != nil {
 		return err
 	}
-	logger.Info("Got vm service folders", "refs", refs)
+	logger.Info("Got vm service folders", "refs", slices.Collect(maps.Keys(moRefWithIDs)))
 
 	// Start the watcher.
 	w, err := watcher.Start(
@@ -156,7 +227,7 @@ func (s Service) waitForChanges(ctx context.Context) error {
 		nil,
 		nil,
 		s.lookupNamespacedName,
-		refs...)
+		moRefWithIDs)
 	if err != nil {
 		return err
 	}

controllers/infra/zone/zone_controller_test.go
@@ -122,8 +122,7 @@ var _ = Describe(
 			When("no vms exist in the zone's vm service folder", func() {
 
 				const (
-					vmName     = "my-vm-1"
-					fakeString = "fake"
+					vmName = "my-vm-1"
 				)
 
 				var (
@@ -212,7 +211,6 @@ var _ = Describe(
 							for i := range list.Items {
 								z := &list.Items[i]
 								if z.Spec.ManagedVMs.FolderMoID == nsInfo.Folder.Reference().Value {
-									Expect(z).ToNot(BeNil())
 									Expect(vcSimCtx.Client.Delete(ctx, z)).To(Succeed())
 									Eventually(func(g Gomega) {
 										key := ctrlclient.ObjectKeyFromObject(z)
@@ -267,7 +265,6 @@ var _ = Describe(
 							for i := range list.Items {
 								z := &list.Items[i]
 								if z.Spec.ManagedVMs.FolderMoID == nsInfo.Folder.Reference().Value {
-									Expect(z).ToNot(BeNil())
 									Expect(vcSimCtx.Client.Delete(ctx, z)).To(Succeed())
 									Eventually(func(g Gomega) {
 										key := ctrlclient.ObjectKeyFromObject(z)

pkg/util/vsphere/watcher/watcher_test.go
@@ -11,6 +11,7 @@ import (
 	"net"
 	"net/url"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 
@@ -32,6 +33,10 @@ import (
 const (
 	fakeStr = "fake"
 )
+
+func idStr(i int) string {
+	return "id-" + strconv.Itoa(i)
+}
 
 var _ = Describe("Start", func() {
 	var (
@@ -173,7 +178,10 @@ var _ = Describe("Start", func() {
 				}
 				return watcher.LookupNamespacedNameResult{}
 			},
-			cluster1.Reference())
+			map[vimtypes.ManagedObjectReference][]string{
+				cluster1.Reference(): {idStr(0)},
+			},
+		)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(w).ToNot(BeNil())
 	})
@@ -273,6 +281,16 @@ var _ = Describe("Start", func() {
 
 	When("no vms have namespace/name information", func() {
 		Specify("no error or results", func() {
+			assertNoError()
+			assertNoResult()
+		})
+	})
+
+	When("removal of container that does not exist", func() {
+		Specify("no error or results", func() {
+			moRef := cluster1vm1.Reference()
+			moRef.Value = "bogus"
+			Expect(watcher.Remove(ctx, moRef, idStr(0))).To(Succeed())
 			assertNoError()
 			assertNoResult()
 		})
@@ -410,7 +428,7 @@ var _ = Describe("Start", func() {
 				assertNoResult()
 
 				// Remove the cluster from the scope of the watcher.
-				Expect(watcher.Remove(ctx, cluster1.Reference(), fakeStr)).To(Succeed())
+				Expect(watcher.Remove(ctx, cluster1.Reference(), idStr(0))).To(Succeed())
 
 				// Add a non-ignored ExtraConfig key.
 				t, err := cluster1vm1.Reconfigure(
@@ -436,14 +454,16 @@ var _ = Describe("Start", func() {
 
 				// Assert no more results are signaled.
 				assertNoResult()
+
+				// Duplicate remove should noop.
+				Expect(watcher.Remove(ctx, cluster1.Reference(), idStr(0))).To(Succeed())
 			})
 		})
 
 		When("the container has multiple adds", func() {
 			JustBeforeEach(func() {
-				Expect(watcher.Add(ctx, cluster1.Reference(), fakeStr+"1")).To(Succeed())
-				Expect(watcher.Add(ctx, cluster1.Reference(), fakeStr+"2")).To(Succeed())
-				Expect(watcher.Add(ctx, cluster1.Reference(), fakeStr+"3")).To(Succeed())
+				Expect(watcher.Add(ctx, cluster1.Reference(), idStr(1))).To(Succeed())
+				Expect(watcher.Add(ctx, cluster1.Reference(), idStr(2))).To(Succeed())
 			})
 			Specify("it should need an equal number of removes before it stops being watched", func() {
 				// Assert that a result is signaled due to the VM entering the
@@ -454,7 +474,7 @@ var _ = Describe("Start", func() {
 				assertNoResult()
 
 				// Remove the cluster from the scope of the watcher.
-				Expect(watcher.Remove(ctx, cluster1.Reference(), fakeStr+"1")).To(Succeed())
+				Expect(watcher.Remove(ctx, cluster1.Reference(), idStr(0))).To(Succeed())
 
 				// Add a non-ignored ExtraConfig key.
 				t, err := cluster1vm1.Reconfigure(
@@ -462,8 +482,8 @@ var _ = Describe("Start", func() {
 					vimtypes.VirtualMachineConfigSpec{
 						ExtraConfig: []vimtypes.BaseOptionValue{
 							&vimtypes.OptionValue{
-								Key:   "guestinfo.1",
-								Value: "1",
+								Key:   "guestinfo.0",
+								Value: "0",
 							},
 						},
 					},
@@ -482,15 +502,15 @@ var _ = Describe("Start", func() {
 				assertNoResult()
 
 				//
-				// Do this again.
+				// Do this again with next ID.
 				//
-				Expect(watcher.Remove(ctx, cluster1.Reference(), fakeStr+"2")).To(Succeed())
+				Expect(watcher.Remove(ctx, cluster1.Reference(), idStr(1))).To(Succeed())
 				t, err = cluster1vm1.Reconfigure(
 					ctx,
 					vimtypes.VirtualMachineConfigSpec{
 						ExtraConfig: []vimtypes.BaseOptionValue{
 							&vimtypes.OptionValue{
-								Key:   "guestinfo.2",
+								Key:   "guestinfo.1",
 								Value: "1",
 							},
 						},
@@ -503,16 +523,37 @@ var _ = Describe("Start", func() {
 				assertNoResult()
 
 				//
-				// Remove the container a third time, and it should finally be
-				// be removed from the watch.
+				// Do it again with the same ID. This should be a noop so we still expect events.
 				//
-				Expect(watcher.Remove(ctx, cluster1.Reference(), fakeStr+"3")).To(Succeed())
+				Expect(watcher.Remove(ctx, cluster1.Reference(), idStr(1))).To(Succeed())
 				t, err = cluster1vm1.Reconfigure(
 					ctx,
 					vimtypes.VirtualMachineConfigSpec{
 						ExtraConfig: []vimtypes.BaseOptionValue{
 							&vimtypes.OptionValue{
-								Key:   "guestinfo.3",
+								Key:   "guestinfo.1",
+								Value: "2",
+							},
+						},
+					},
+				)
+				ExpectWithOffset(1, err).ShouldNot(HaveOccurred())
+				ExpectWithOffset(1, t.WaitEx(ctx)).To(Succeed())
+				assertNoError()
+				assertResult(cluster1vm1, "my-namespace-1", "my-name-1")
+				assertNoResult()
+
+				//
+				// Remove the container with the final ID, and it should finally be
+				// removed from the watch.
+				//
+				Expect(watcher.Remove(ctx, cluster1.Reference(), idStr(2))).To(Succeed())
+				t, err = cluster1vm1.Reconfigure(
+					ctx,
+					vimtypes.VirtualMachineConfigSpec{
+						ExtraConfig: []vimtypes.BaseOptionValue{
+							&vimtypes.OptionValue{
+								Key:   "guestinfo.2",
 								Value: "1",
 							},
 						},
@@ -597,7 +638,7 @@ var _ = Describe("Start", func() {
 
 		When("the container is added to the watcher", func() {
 			JustBeforeEach(func() {
-				Expect(watcher.Add(ctx, cluster2.Reference(), fakeStr)).To(Succeed())
+				Expect(watcher.Add(ctx, cluster2.Reference(), idStr(0))).To(Succeed())
 			})
 			When("the lookup function returns verified=false", func() {
 				Specify("the result channel should receive a result", func() {

services/vm-watcher/vm_watcher_service_test.go
@@ -18,10 +18,13 @@ import (
 
 	"github.com/vmware/govmomi/object"
 	vimtypes "github.com/vmware/govmomi/vim25/types"
+	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	ctrlmgr "sigs.k8s.io/controller-runtime/pkg/manager"
 
 	vmopv1 "github.com/vmware-tanzu/vm-operator/api/v1alpha3"
+	zonectrl "github.com/vmware-tanzu/vm-operator/controllers/infra/zone"
+	topologyv1 "github.com/vmware-tanzu/vm-operator/external/tanzu-topology/api/v1alpha1"
 	pkgcfg "github.com/vmware-tanzu/vm-operator/pkg/config"
 	"github.com/vmware-tanzu/vm-operator/pkg/constants/testlabels"
 	pkgctx "github.com/vmware-tanzu/vm-operator/pkg/context"
@@ -282,6 +285,20 @@ var _ = Describe(
 			})
 		})
 
+		When("the zones do not have the finalizer", func() {
+			Specify("the finalizer gets added", func() {
+				Eventually(func(g Gomega) {
+					zoneList := topologyv1.ZoneList{}
+					err := vcSimCtx.Client.List(vcSimCtx, &zoneList, ctrlclient.InNamespace(vcSimCtx.NSInfo.Namespace))
+					g.Expect(err).NotTo(HaveOccurred())
+					g.Expect(zoneList.Items).ToNot(BeEmpty())
+					for _, zone := range zoneList.Items {
+						g.Expect(zone.Finalizers).To(ConsistOf(zonectrl.Finalizer))
+					}
+				})
+			})
+		})
+
 		When("there is a vm in the zone's folder", func() {
 
 			const (
@@ -345,6 +362,53 @@ var _ = Describe(
 							},
 						},
 					})))
+				})
+
+				When("a bogus Zone Folder MoID", func() {
+					BeforeEach(func() {
+						outerInitEnvFn := initEnvFn
+
+						initEnvFn = func(ctx *builder.IntegrationTestContextForVCSim) {
+							outerInitEnvFn(ctx)
+
+							zone := &topologyv1.Zone{
+								ObjectMeta: metav1.ObjectMeta{
+									Name:      "bogus",
+									Namespace: vcSimCtx.NSInfo.Namespace,
+								},
+								Spec: topologyv1.ZoneSpec{
+									ManagedVMs: topologyv1.VSphereEntityInfo{
+										FolderMoID: "group-4242424242",
+									},
+								},
+							}
+							Expect(ctx.Client.Create(ctx, zone)).To(Succeed())
+						}
+					})
+
+					Specify("a reconcile request should still be received", func() {
+						chanSource := cource.FromContext(ctx, "VirtualMachine")
+						var e event.GenericEvent
+						Eventually(chanSource).Should(Receive(&e, Equal(event.GenericEvent{
+							Object: &vmopv1.VirtualMachine{
+								ObjectMeta: metav1.ObjectMeta{
+									Namespace: vcSimCtx.NSInfo.Namespace,
+									Name:      vmName,
+								},
+							},
+						})))
+
+						By("bogus zone should not have finalizer applied", func() {
+							zone := topologyv1.Zone{
+								ObjectMeta: metav1.ObjectMeta{
+									Name:      "bogus",
+									Namespace: vcSimCtx.NSInfo.Namespace,
+								},
+							}
+							Expect(vcSimCtx.Client.Get(vcSimCtx, ctrlclient.ObjectKeyFromObject(&zone), &zone)).To(Succeed())
+							Expect(zone.Finalizers).To(BeEmpty())
+						})
+					})
 				})
 
 				When("the vm is reconfigured", func() {
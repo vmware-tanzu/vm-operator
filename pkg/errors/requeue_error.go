2f2ee053a1788b1ded5efad74ccc5da6f4a3fbb9
Bryan Venteicher <bryan.venteicher@broadcom.com>
2025-01-28 11:48:09 -0600

    Do not requeue VMIC for empty Spec fields
    If these empty Spec fields are updated, the Watch() will cause the
    controller to reconcile the object.
    
    Add NoRequeueError that returns the controller-runtime TerminalError
    that does not do a requeue but is logged and counted as an error.
    
    While here, update the VMIC ReconcileNormal() to use the logger
    from the context instead of parent reconciler so the object name
    is still there.

--- pkg/errors/requeue_error.go
+++ pkg/errors/requeue_error.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 // RequeueError may be returned from any part of a reconcile call stack and the
@@ -27,16 +28,46 @@ func (e RequeueError) Error() string {
 	return fmt.Sprintf("requeue after %s", e.After)
 }
 
+// NoRequeueError may be returned from any part of a reconcile call stack and the
+// controller will not requeue the request. This can be used to return an error
+// but not cause the back-off retry to occur.
+type NoRequeueError struct {
+	Message string
+}
+
+func (e NoRequeueError) Error() string {
+	if e.Message == "" {
+		return "no requeue"
+	}
+	return e.Message
+}
+
 // ResultFromError returns a ReconcileResult based on the provided error. If
-// the error contains an embedded RequeueError, then it is used to influence
-// the result.
+// the error contains an embedded RequeueError or NoRequeueError, then it is
+// used to influence the result. An embedded RequeueError is favored in an
+// error that also contains a NoRequeueError.
+// An embedded NoRequeueError will return a controller-runtime TerminalError
+// that will be logged and counted as an error but does not retry.
 func ResultFromError(err error) (ctrl.Result, error) {
-	var dst RequeueError
-	if err != nil && errors.As(err, &dst) {
-		if dst.After == 0 {
+	if err == nil {
+		return ctrl.Result{}, nil
+	}
+
+	var requeue RequeueError
+	if errors.As(err, &requeue) {
+		if requeue.After == 0 {
 			return ctrl.Result{Requeue: true}, nil
 		}
-		return ctrl.Result{RequeueAfter: dst.After}, nil
+		return ctrl.Result{RequeueAfter: requeue.After}, nil
 	}
+
+	var noRequeue NoRequeueError
+	if errors.As(err, &noRequeue) {
+		// TerminalError is confusingly named: it won't cause an error retry to
+		// be enqueued but later events from like a watch will still be queued.
+		// Wrap the original error so any other wrapped errors are still there.
+		return ctrl.Result{}, reconcile.TerminalError(err)
+	}
+
 	return ctrl.Result{}, err
 }

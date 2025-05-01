4dee39d079793a51556e80a34d8f917cd87ad1bb
Doug MacEachern <dougm@broadcom.com>
2025-05-01 09:14:11 -0700

    FastDeploy: Reconcile disk promotion task state and conditions

--- pkg/vmconfig/diskpromo/diskpromo_reconciler.go
+++ pkg/vmconfig/diskpromo/diskpromo_reconciler.go
@@ -8,9 +8,11 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/go-logr/logr"
+	"github.com/vmware/govmomi/fault"
 	"github.com/vmware/govmomi/object"
+	"github.com/vmware/govmomi/property"
 	"github.com/vmware/govmomi/vim25"
-	"github.com/vmware/govmomi/vim25/methods"
 	"github.com/vmware/govmomi/vim25/mo"
 	vimtypes "github.com/vmware/govmomi/vim25/types"
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
@@ -24,6 +26,13 @@ import (
 type reconciler struct{}
 
 var _ vmconfig.Reconciler = reconciler{}
+
+const (
+	ReasonTaskError = "DiskPromotionTaskError"
+	ReasonPending   = "DiskPromotionPending"
+
+	promoteDisksTaskKey = "VirtualMachine.promoteDisks"
+)
 
 // New returns a new Reconciler for a VM's disk promotion state.
 func New() vmconfig.Reconciler {
@@ -44,8 +53,6 @@ func (r reconciler) OnResult(
 	return nil
 }
 
-var doPromote bool
-
 // Reconcile performs an online promotion of any of a VM's linked-clone disks to
 // full clones. This reconciler is a no-op for VMs that do not yet exists and/or
 // are not powered on.
@@ -57,15 +64,83 @@ func (r reconciler) Reconcile(
 	moVM mo.VirtualMachine,
 	_ *vimtypes.VirtualMachineConfigSpec) error {
 
+	if ctx == nil {
+		panic("context is nil")
+	}
+	if vimClient == nil {
+		panic("vimClient is nil")
+	}
+	if vm == nil {
+		panic("vm is nil")
+	}
+
 	if moVM.Config == nil {
 		// Skip VMs that are not yet created.
 		return nil
 	}
 
+	logger := logr.FromContextOrDiscard(ctx)
+
 	if vm.Status.TaskID != "" {
-		// Skip VMs with outstanding tasks.
-		return nil
+		ref := vimtypes.ManagedObjectReference{
+			Type:  "Task",
+			Value: vm.Status.TaskID,
+		}
+
+		var task mo.Task
+		pc := property.DefaultCollector(vimClient)
+
+		if err := pc.RetrieveOne(ctx, ref, []string{"info"}, &task); err != nil {
+			if fault.Is(err, &vimtypes.ManagedObjectNotFound{}) {
+				// Tasks go away after 10m of completion
+				vm.Status.TaskID = ""
+			}
+			return err
+		}
+
+		logger.Info("Pending task",
+			"ID", vm.Status.TaskID,
+			"description", task.Info.DescriptionId,
+			"state", task.Info.State)
+
+		// From the API doc:
+		//   An identifier for this operation. This includes publicly visible internal tasks and
+		//   is a lookup in the TaskDescription methodInfo data object.
+		// See also:
+		//   govc collect -s -json TaskManager:TaskManager description.methodInfo | \
+		//     jq '.[] | select(.key == "VirtualMachine.promoteDisks") | .'
+
+		if task.Info.DescriptionId != promoteDisksTaskKey {
+			return nil
+		}
+
+		switch task.Info.State {
+		case vimtypes.TaskInfoStateSuccess:
+			vm.Status.TaskID = ""
+
+			pkgcond.MarkTrue(
+				vm,
+				vmopv1.VirtualMachineDiskPromotionSynced)
+
+			return nil
+		case vimtypes.TaskInfoStateError:
+			vm.Status.TaskID = ""
+
+			pkgcond.MarkFalse(
+				vm,
+				vmopv1.VirtualMachineDiskPromotionSynced,
+				ReasonTaskError,
+				"%s",
+				task.Info.Error.LocalizedMessage)
+
+			return nil
+		default:
+			// Skip VMs with outstanding tasks.
+			return nil
+		}
 	}
+
+	logger.Info("Promote disks", "mode", vm.Spec.PromoteDisksMode)
 
 	if vm.Spec.PromoteDisksMode == vmopv1.VirtualMachinePromoteDisksModeDisabled {
 		// Skip VMs that do not request promotion.
@@ -101,6 +176,8 @@ func (r reconciler) Reconcile(
 		}
 	}
 
+	logger.Info("Promote disks", "total", len(allDisks), "child", len(childDisks))
+
 	if len(childDisks) == 0 {
 		// Skip VMs that do not have any child disks to promote.
 		return nil
@@ -113,36 +190,38 @@ func (r reconciler) Reconcile(
 			pkgcond.MarkFalse(
 				vm,
 				vmopv1.VirtualMachineDiskPromotionSynced,
-				"")
-			// TODO(akutz) Set error condition.
+				ReasonPending,
+				"Cannot online promote disks when VM has snapshot")
 			return nil
 		}
 		if moVM.Runtime.PowerState != vimtypes.VirtualMachinePowerStatePoweredOn {
-			// TODO(akutz) Set pending condition.
+			pkgcond.MarkFalse(
+				vm,
+				vmopv1.VirtualMachineDiskPromotionSynced,
+				ReasonPending,
+				"Pending VM powered on")
 			return nil
 		}
 	case vmopv1.VirtualMachinePromoteDisksModeOffline:
 		if moVM.Runtime.PowerState != vimtypes.VirtualMachinePowerStatePoweredOff {
 			// Skip VMs that are not powered off.
-			// TODO(akutz) Set pending condition.
+			pkgcond.MarkFalse(
+				vm,
+				vmopv1.VirtualMachineDiskPromotionSynced,
+				ReasonPending,
+				"Pending VM powered off")
 			return nil
 		}
 	}
 
-	req := vimtypes.PromoteDisks_Task{
-		This:   moVM.Reference(),
-		Unlink: true,
-		Disks:  childDisks,
-	}
-	res, err := methods.PromoteDisks_Task(ctx, vimClient, &req)
+	obj := object.NewVirtualMachine(vimClient, moVM.Self)
+	task, err := obj.PromoteDisks(ctx, true, childDisks)
 	if err != nil {
 		return fmt.Errorf("failed to call promote disks task: %w", err)
 	}
 
-	promoteTask := object.NewTask(vimClient, res.Returnval)
-
 	// Track the task ID.
-	vm.Status.TaskID = promoteTask.Reference().Value
+	vm.Status.TaskID = task.Reference().Value
 
 	return pkgerr.NoRequeueError{
 		Message: "doing online disk promotion",

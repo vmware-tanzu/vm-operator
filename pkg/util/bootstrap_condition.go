721982ef931cc40b1b152f160c5e2a63b9dcdff5
Bryan Venteicher <bryan.venteicher@broadcom.com>
2025-05-22 13:46:23 -0500

    Prefer cloudinit local-ipv4/6 for VM's Primary IPs
    The cloudinit VMware datasource sets these values in the ExtraConfig, so
    use those fields if set as the VM's primary IPv4/6 over what is reported
    by VM Tools. Apply the same validity checks on EC fields as what we were
    already doing.
    
    Since we were already both walking and making a map of the ExtraConfig
    convert it to a map once and pass that down.

--- pkg/util/bootstrap_condition.go
+++ pkg/util/bootstrap_condition.go
@@ -7,8 +7,6 @@ package util
 import (
 	"strconv"
 	"strings"
-
-	vimtypes "github.com/vmware/govmomi/vim25/types"
 )
 
 // GuestInfoBootstrapCondition is the ExtraConfig key at which possible info
@@ -18,40 +16,25 @@ const GuestInfoBootstrapCondition = "guestinfo.vmservice.bootstrap.condition"
 // GetBootstrapConditionValues returns the bootstrap condition values from a
 // VM if the data is present.
 func GetBootstrapConditionValues(
-	configInfo *vimtypes.VirtualMachineConfigInfo) (bool, string, string, bool) {
+	extraConfig map[string]string) (bool, string, string, bool) {
 
-	if configInfo == nil {
+	val, ok := extraConfig[GuestInfoBootstrapCondition]
+	if !ok {
 		return false, "", "", false
 	}
 
-	if len(configInfo.ExtraConfig) == 0 {
-		return false, "", "", false
-	}
-
-	for i := range configInfo.ExtraConfig {
-		if ec := configInfo.ExtraConfig[i]; ec != nil {
-			if ov := ec.GetOptionValue(); ov != nil {
-				if ov.Key == GuestInfoBootstrapCondition {
-					if s, ok := ov.Value.(string); ok {
-						v := strings.SplitN(s, ",", 3)
-						status, _ := strconv.ParseBool(strings.TrimSpace(v[0]))
-						switch len(v) {
-						case 1:
-							return status, "", "", true
-						case 2:
-							return status, strings.TrimSpace(v[1]), "", true
-						case 3:
-							return status,
-								strings.TrimSpace(v[1]),
-								strings.TrimSpace(v[2]),
-								true
-						default:
-							return false, "", "", false
-						}
-					}
-				}
-			}
-		}
+	v := strings.SplitN(val, ",", 3)
+	status, _ := strconv.ParseBool(strings.TrimSpace(v[0]))
+	switch len(v) {
+	case 1:
+		return status, "", "", true
+	case 2:
+		return status, strings.TrimSpace(v[1]), "", true
+	case 3:
+		return status,
+			strings.TrimSpace(v[1]),
+			strings.TrimSpace(v[2]),
+			true
 	}
 
 	return false, "", "", false

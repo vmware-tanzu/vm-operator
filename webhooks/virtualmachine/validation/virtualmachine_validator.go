d50a58fc92e5f85b18824c6f7bb0efcf3b239949
Adnan Baruni <adnan.baruni@broadcom.com>
2025-06-04 10:22:24 -0500

    add controller logic for bootoptions

--- webhooks/virtualmachine/validation/virtualmachine_validator.go
+++ webhooks/virtualmachine/validation/virtualmachine_validator.go
@@ -156,6 +156,7 @@ func (v validator) ValidateCreate(ctx *pkgctx.WebhookRequestContext) admission.R
 	fieldErrs = append(fieldErrs, v.validateMinHardwareVersion(ctx, vm, nil)...)
 	fieldErrs = append(fieldErrs, v.validateCdrom(ctx, vm)...)
 	fieldErrs = append(fieldErrs, v.validateChecks(ctx, vm, nil)...)
+	fieldErrs = append(fieldErrs, v.validateBootOptions(ctx, vm)...)
 
 	validationErrs := make([]string, 0, len(fieldErrs))
 	for _, fieldErr := range fieldErrs {
@@ -251,6 +252,7 @@ func (v validator) ValidateUpdate(ctx *pkgctx.WebhookRequestContext) admission.R
 	fieldErrs = append(fieldErrs, v.validateNetworkHostAndDomainName(ctx, vm, oldVM)...)
 	fieldErrs = append(fieldErrs, v.validateCdrom(ctx, vm)...)
 	fieldErrs = append(fieldErrs, v.validateChecks(ctx, vm, oldVM)...)
+	fieldErrs = append(fieldErrs, v.validateBootOptions(ctx, vm)...)
 
 	validationErrs := make([]string, 0, len(fieldErrs))
 	for _, fieldErr := range fieldErrs {
@@ -1659,6 +1661,29 @@ func (v validator) validateChecksAnnotation(
 					allErrs = append(allErrs, field.Forbidden(f, modRestrictedAnnotation))
 				}
 			}
+		}
+	}
+
+	return allErrs
+}
+
+func (v validator) validateBootOptions(
+	ctx *pkgctx.WebhookRequestContext,
+	vm *vmopv1.VirtualMachine) field.ErrorList {
+
+	var allErrs field.ErrorList
+
+	if vm.Spec.BootOptions != nil {
+		fieldPath := field.NewPath("spec", "bootOptions")
+
+		if vm.Spec.BootOptions.BootRetryDelay != nil && vm.Spec.BootOptions.BootRetry != vmopv1.VirtualMachineBootOptionsBootRetryEnabled {
+			allErrs = append(allErrs, field.Required(fieldPath.Child("bootRetry"), "when setting bootRetryDelay"))
+		}
+
+		if vm.Spec.BootOptions.EFISecureBoot == vmopv1.VirtualMachineBootOptionsEFISecureBootEnabled &&
+			vm.Spec.BootOptions.Firmware != vmopv1.VirtualMachineBootOptionsFirmwareTypeEFI {
+
+			allErrs = append(allErrs, field.Forbidden(fieldPath.Child("efiSecureBoot"), "when image firmware is not EFI"))
 		}
 	}
 

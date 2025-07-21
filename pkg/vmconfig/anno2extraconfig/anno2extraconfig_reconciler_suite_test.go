a6a2eae0cdbdb329e1e112edfea4f2aef87bc0a5
akutz <andrew.kutz@broadcom.com>
2025-07-21 18:03:14 -0500

    Annotations-to-ExtraConfig Support
    This patch supports updating a VM's ExtraConfig from a set of
    approved annotation keys. If the VM does not have the annotation,
    its corresponding ExtraConfig value will be removed.

---
+++ pkg/vmconfig/anno2extraconfig/anno2extraconfig_reconciler_suite_test.go
@@ -0,0 +1,25 @@
+// © Broadcom. All Rights Reserved.
+// The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: Apache-2.0
+
+package anno2extraconfig_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"k8s.io/klog/v2"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+func init() {
+	klog.SetOutput(GinkgoWriter)
+	logf.SetLogger(klog.Background())
+}
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "AnnotationsToExtraConfig Reconciler Test Suite")
+}

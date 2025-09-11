ee278e43b808b1b2942806ec8a4c69980abf77eb
Arunesh Pandey <aruneshpa@gmail.com>
2025-09-11 16:49:51 -0700

    :seedling: Support explicit zone specification from group members
    Placement of a VM is driven via the VirtualMachineGroup if the VM is
    part of a group (specified by spec.groupName).  This means we don't
    send in the list of placement candidate resource pools to DRS when
    asking for placement recommendations.  DRS returns recommendations
    based on the hardware and the placement policies specified with the
    VM.
    
    However, if a group member wants to explicitly be placed in a
    particular zone, they should be able to specify the zone label on the
    member.  This change adds support for that.  This will be used by VKS
    to support scale-outs of node pools.

--- pkg/providers/vsphere/vmprovider_vm.go
+++ pkg/providers/vsphere/vmprovider_vm.go
@@ -1396,12 +1396,33 @@ func (vs *vSphereVMProvider) vmCreateDoPlacement(
 
 	if pkgcfg.FromContext(vmCtx).Features.VMGroups &&
 		vmCtx.VM.Spec.GroupName != "" {
-		vmCtx.Logger.Info(
-			"Getting VM placement result from its group",
-			"groupName", vmCtx.VM.Spec.GroupName,
-		)
 
-		return vs.vmCreateDoPlacementByGroup(vmCtx, vcClient, createArgs)
+		// First update and check if the VM is linked to its group.
+		if err := vmopv1util.UpdateGroupLinkedCondition(
+			vmCtx,
+			vmCtx.VM,
+			vs.k8sClient,
+		); err != nil {
+			return fmt.Errorf("failed to update VM group linked condition: %w", err)
+		}
+
+		if !pkgcnd.IsTrue(
+			vmCtx.VM,
+			vmopv1.VirtualMachineGroupMemberConditionGroupLinked,
+		) {
+			return fmt.Errorf("VM is not linked to its group")
+		}
+
+		// If the VM has an explicit zone label, skip group placement
+		// and use the regular placement flow to respect the zone override.
+		if zoneName := vmCtx.VM.Labels[corev1.LabelTopologyZone]; zoneName == "" {
+			vmCtx.Logger.Info(
+				"Getting VM placement result from its group",
+				"groupName", vmCtx.VM.Spec.GroupName,
+			)
+
+			return vs.vmCreateDoPlacementByGroup(vmCtx, vcClient, createArgs)
+		}
 	}
 
 	placementConfigSpec, err := virtualmachine.CreateConfigSpecForPlacement(
@@ -1447,22 +1468,6 @@ func (vs *vSphereVMProvider) vmCreateDoPlacementByGroup(
 	// Should never happen when this function is called, check just in case.
 	if vmCtx.VM.Spec.GroupName == "" {
 		return fmt.Errorf("VM.Spec.GroupName is empty")
-	}
-
-	// First update and check if the VM is linked to its group.
-	if err := vmopv1util.UpdateGroupLinkedCondition(
-		vmCtx,
-		vmCtx.VM,
-		vs.k8sClient,
-	); err != nil {
-		return fmt.Errorf("failed to update VM group linked condition: %w", err)
-	}
-
-	if !pkgcnd.IsTrue(
-		vmCtx.VM,
-		vmopv1.VirtualMachineGroupMemberConditionGroupLinked,
-	) {
-		return fmt.Errorf("VM is not linked to its group")
 	}
 
 	var vmg vmopv1.VirtualMachineGroup

d5e504b2718abf846fbaa18efbb4abfd894f53b3
Bryan Venteicher <bryan.venteicher@broadcom.com>
2025-11-18 10:46:08 -0600

    Handle vSphere Policy Evaluations during group placement
    Group placement uses much of the code with the non-group
    VM create code path, but where policies were done was
    not a part of the group uses, since that is done closer
    to single VM placement.
    
    Since the policy eval object will need to be reconciled
    before it is ready, stop failing at the first error, so
    all the needed objects will be created.
    
    Set the VM group member PlacementReady condition when
    things fail to give more info. Note that since when
    the regular VM reconcile bails for not yet ready grouped
    VMs, the VM itself will also have that info but add to the
    group for convenience.
    
    Update the PolicyEval to also check for Ready condition

--- pkg/providers/vsphere/vmprovider_vm_group_test.go
+++ pkg/providers/vsphere/vmprovider_vm_group_test.go
@@ -13,6 +13,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	vmopv1 "github.com/vmware-tanzu/vm-operator/api/v1alpha5"
+	vspherepolv1 "github.com/vmware-tanzu/vm-operator/external/vsphere-policy/api/v1alpha1"
 	pkgcond "github.com/vmware-tanzu/vm-operator/pkg/conditions"
 	pkgcfg "github.com/vmware-tanzu/vm-operator/pkg/config"
 	"github.com/vmware-tanzu/vm-operator/pkg/providers"
@@ -31,6 +32,7 @@ func vmGroupTests() {
 		nsInfo      builder.WorkloadNamespaceInfo
 
 		vm1     *vmopv1.VirtualMachine
+		vm2     *vmopv1.VirtualMachine
 		vmClass *vmopv1.VirtualMachineClass
 		vmGroup *vmopv1.VirtualMachineGroup
 	)
@@ -41,13 +43,20 @@ func vmGroupTests() {
 		}
 
 		vm1 = builder.DummyBasicVirtualMachine("group-placement-vm-1", "")
+		vm2 = builder.DummyBasicVirtualMachine("group-placement-vm-2", "")
 		vmClass = builder.DummyVirtualMachineClassGenName()
 
 		vmGroup = &vmopv1.VirtualMachineGroup{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "vm-group-test",
 			},
+			Spec: vmopv1.VirtualMachineGroupSpec{
+				BootOrder: make([]vmopv1.VirtualMachineGroupBootOrderGroup, 1),
+			},
 		}
+		vmGroup.Spec.BootOrder[0].Members = append(vmGroup.Spec.BootOrder[0].Members,
+			vmopv1.GroupMember{Kind: "VirtualMachine", Name: vm1.Name},
+			vmopv1.GroupMember{Kind: "VirtualMachine", Name: vm2.Name})
 	})
 
 	JustBeforeEach(func() {
@@ -62,13 +71,17 @@ func vmGroupTests() {
 		Expect(ctx.Client.Create(ctx, vmClass)).To(Succeed())
 		Expect(ctx.Client.Status().Update(ctx, vmClass)).To(Succeed())
 
-		vm1.Namespace = nsInfo.Namespace
-		vm1.Spec.ClassName = vmClass.Name
-		vm1.Spec.ImageName = ctx.ContentLibraryImageName
-		vm1.Spec.Image.Kind = cvmiKind
-		vm1.Spec.Image.Name = ctx.ContentLibraryImageName
-		vm1.Spec.StorageClass = ctx.StorageClassName
-		vm1.Spec.GroupName = vmGroup.Name
+		initVM := func(vm *vmopv1.VirtualMachine) {
+			vm.Namespace = nsInfo.Namespace
+			vm.Spec.ClassName = vmClass.Name
+			vm.Spec.ImageName = ctx.ContentLibraryImageName
+			vm.Spec.Image.Kind = cvmiKind
+			vm.Spec.Image.Name = ctx.ContentLibraryImageName
+			vm.Spec.StorageClass = ctx.StorageClassName
+			vm.Spec.GroupName = vmGroup.Name
+		}
+		initVM(vm1)
+		initVM(vm2)
 
 		vmGroup.Namespace = nsInfo.Namespace
 
@@ -146,24 +159,44 @@ func vmGroupTests() {
 		nsInfo = builder.WorkloadNamespaceInfo{}
 
 		vm1 = nil
+		vm2 = nil
 		vmClass = nil
 		vmGroup = nil
 	})
 
 	assertMemberStatusForVM := func(vm *vmopv1.VirtualMachine, ms vmopv1.VirtualMachineGroupMemberStatus) {
-		ExpectWithOffset(1, ms.Name).To(Equal(vm.Name), "Unexpected Name")
-		ExpectWithOffset(1, ms.Kind).To(Equal("VirtualMachine"), "Unexpected Kind")
-		ExpectWithOffset(1, ms.Placement).ToNot(BeNil(), "Missing Placement")
-		ExpectWithOffset(1, pkgcond.IsTrue(&ms, vmopv1.VirtualMachineGroupMemberConditionPlacementReady)).To(BeTrue(), "No placement ready condition")
-		ExpectWithOffset(1, ms.Placement.Zone).ToNot(BeEmpty(), "Missing Placement Zone")
-		ExpectWithOffset(1, ms.Placement.Pool).ToNot(BeEmpty(), "Missing Placement Pool")
-		ExpectWithOffset(1, ms.Placement.Node).To(BeEmpty(), "Has Placement Node")
+		GinkgoHelper()
+
+		Expect(ms.Name).To(Equal(vm.Name), "Unexpected Name")
+		Expect(ms.Kind).To(Equal("VirtualMachine"), "Unexpected Kind")
+		Expect(ms.Placement).ToNot(BeNil(), "Missing Placement")
+		Expect(pkgcond.IsTrue(&ms, vmopv1.VirtualMachineGroupMemberConditionPlacementReady)).To(BeTrue(), "No placement ready condition")
+		Expect(ms.Placement.Zone).ToNot(BeEmpty(), "Missing Placement Zone")
+		Expect(ms.Placement.Pool).ToNot(BeEmpty(), "Missing Placement Pool")
+		Expect(ms.Placement.Node).To(BeEmpty(), "Has Placement Node")
 		if pkgcfg.FromContext(ctx).Features.FastDeploy {
-			ExpectWithOffset(1, ms.Placement.Datastores).ToNot(BeEmpty(), "Missing Placement Datastores")
+			Expect(ms.Placement.Datastores).ToNot(BeEmpty(), "Missing Placement Datastores")
 			// Verify against VirtualMachineImageCache.Status
 		} else {
-			ExpectWithOffset(1, ms.Placement.Datastores).To(BeEmpty(), "Has Placement Datastores")
+			Expect(ms.Placement.Datastores).To(BeEmpty(), "Has Placement Datastores")
 		}
+	}
+
+	assertNotReadyMemberStatusForVM := func(
+		vm *vmopv1.VirtualMachine,
+		ms vmopv1.VirtualMachineGroupMemberStatus,
+		reason string) {
+
+		GinkgoHelper()
+
+		Expect(ms.Name).To(Equal(vm.Name), "Unexpected Name")
+		Expect(ms.Kind).To(Equal("VirtualMachine"), "Unexpected Kind")
+		Expect(ms.Placement).To(BeNil(), "Has Placement")
+
+		c := pkgcond.Get(ms, vmopv1.VirtualMachineGroupMemberConditionPlacementReady)
+		Expect(c).ToNot(BeNil(), "Condition missing")
+		Expect(c.Status).To(Equal(metav1.ConditionFalse))
+		Expect(c.Reason).To(Equal(reason))
 	}
 
 	Context("Group placement with VMs specifying affinity policies", func() {
@@ -189,14 +222,16 @@ func vmGroupTests() {
 					VMGroup: vmGroup,
 					VMMembers: []*vmopv1.VirtualMachine{
 						vm1,
+						vm2,
 					},
 				},
 			}
 
 			err := vmProvider.PlaceVirtualMachineGroup(ctx, vmGroup, groupPlacements)
 			Expect(err).ToNot(HaveOccurred())
-			Expect(vmGroup.Status.Members).To(HaveLen(1))
+			Expect(vmGroup.Status.Members).To(HaveLen(2))
 			assertMemberStatusForVM(vm1, vmGroup.Status.Members[0])
+			assertMemberStatusForVM(vm2, vmGroup.Status.Members[1])
 		})
 
 		It("should process required VM affinity policies during group placement", func() {
@@ -221,33 +256,71 @@ func vmGroupTests() {
 					VMGroup: vmGroup,
 					VMMembers: []*vmopv1.VirtualMachine{
 						vm1,
+						vm2,
 					},
 				},
 			}
 
 			err := vmProvider.PlaceVirtualMachineGroup(ctx, vmGroup, groupPlacements)
 			Expect(err).ToNot(HaveOccurred())
-			Expect(vmGroup.Status.Members).To(HaveLen(1))
+			Expect(vmGroup.Status.Members).To(HaveLen(2))
 			assertMemberStatusForVM(vm1, vmGroup.Status.Members[0])
+			assertMemberStatusForVM(vm2, vmGroup.Status.Members[1])
 		})
 	})
 
-	Context("Group Placement", func() {
-		// vcsim PlaceVmsXCluster() only allows one ConfigSpec at the moment.
-		It("VM Group with one VM member", func() {
+	Context("VSpherePolicies is enabled", func() {
+		JustBeforeEach(func() {
+			pkgcfg.SetContext(ctx, func(config *pkgcfg.Config) {
+				config.Features.VSpherePolicies = true
+			})
+		})
+
+		It("should process VM with PolicyEval during group placement", func() {
 			groupPlacements := []providers.VMGroupPlacement{
 				{
 					VMGroup: vmGroup,
 					VMMembers: []*vmopv1.VirtualMachine{
 						vm1,
+						vm2,
 					},
 				},
 			}
 
 			err := vmProvider.PlaceVirtualMachineGroup(ctx, vmGroup, groupPlacements)
+			Expect(err).To(HaveOccurred())
+
+			Expect(vmGroup.Status.Members).To(HaveLen(2))
+			assertNotReadyMemberStatusForVM(vm1, vmGroup.Status.Members[0], "NotReady")
+			assertNotReadyMemberStatusForVM(vm2, vmGroup.Status.Members[1], "NotReady")
+
+			markPolicyEvalReady := func(vm *vmopv1.VirtualMachine) {
+				policyEval := &vspherepolv1.PolicyEvaluation{}
+				Expect(ctx.Client.Get(ctx, client.ObjectKey{
+					Namespace: vm.Namespace,
+					Name:      "vm-" + vm.Name},
+					policyEval)).To(Succeed())
+				policyEval.Status.ObservedGeneration = policyEval.Generation
+				pkgcond.MarkTrue(policyEval, vspherepolv1.ReadyConditionType)
+				Expect(ctx.Client.Status().Update(ctx, policyEval)).To(Succeed())
+			}
+
+			markPolicyEvalReady(vm1)
+			err = vmProvider.PlaceVirtualMachineGroup(ctx, vmGroup, groupPlacements)
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError(vsphere.ErrVMGroupPlacementConfigSpec))
+
+			Expect(vmGroup.Status.Members).To(HaveLen(2))
+			assertNotReadyMemberStatusForVM(vm1, vmGroup.Status.Members[0], "PendingPlacement")
+			assertNotReadyMemberStatusForVM(vm2, vmGroup.Status.Members[1], "NotReady")
+
+			markPolicyEvalReady(vm2)
+			err = vmProvider.PlaceVirtualMachineGroup(ctx, vmGroup, groupPlacements)
 			Expect(err).ToNot(HaveOccurred())
-			Expect(vmGroup.Status.Members).To(HaveLen(1))
+
+			Expect(vmGroup.Status.Members).To(HaveLen(2))
 			assertMemberStatusForVM(vm1, vmGroup.Status.Members[0])
+			assertMemberStatusForVM(vm2, vmGroup.Status.Members[1])
 		})
 	})
 }
--- pkg/providers/vsphere/vmprovider_vm_test.go
+++ pkg/providers/vsphere/vmprovider_vm_test.go
@@ -5423,6 +5423,9 @@ func vmTests() {
 												Tags: []string{policyTag1ID, policyTag2ID},
 											},
 										},
+										Conditions: []metav1.Condition{
+											*conditions.TrueCondition(vspherepolv1.ReadyConditionType),
+										},
 									},
 								}
 
@@ -5462,7 +5465,7 @@ func vmTests() {
 							})
 						})
 
-						It("should successfully create VM and call vmconfpolicy.Reconcile during placement", func() {
+						It("should successfully create VM and call vmconfig policy.Reconcile during placement", func() {
 							By("Setting up VM with policy evaluation objects", func() {
 								// Set VM UID for proper PolicyEvaluation naming
 								vm.UID = "test-vm-sync-policy-uid"
@@ -5501,6 +5504,9 @@ func vmTests() {
 												Tags: []string{policyTag1ID, policyTag2ID},
 											},
 										},
+										Conditions: []metav1.Condition{
+											*conditions.TrueCondition(vspherepolv1.ReadyConditionType),
+										},
 									},
 								}
 
@@ -5536,6 +5542,47 @@ func vmTests() {
 
 				When("updating a VM", func() {
 					It("should update VM with policy tags during reconfiguration", func() {
+						By("Setting up VM with policy evaluation objects", func() {
+							// Set VM UID for proper PolicyEvaluation naming
+							vm.UID = "test-vm-policy-uid"
+
+							// Create a PolicyEvaluation object that will be found during policy reconciliation
+							policyEval := &vspherepolv1.PolicyEvaluation{
+								ObjectMeta: metav1.ObjectMeta{
+									Namespace:  vm.Namespace,
+									Name:       "vm-" + vm.Name,
+									Generation: 1,
+									OwnerReferences: []metav1.OwnerReference{
+										{
+											APIVersion:         vmopv1.GroupVersion.String(),
+											Kind:               "VirtualMachine",
+											Name:               vm.Name,
+											UID:                vm.UID,
+											Controller:         ptr.To(true),
+											BlockOwnerDeletion: ptr.To(true),
+										},
+									},
+								},
+								Spec: vspherepolv1.PolicyEvaluationSpec{
+									Workload: &vspherepolv1.PolicyEvaluationWorkloadSpec{
+										Guest: &vspherepolv1.PolicyEvaluationGuestSpec{
+											GuestID:     "ubuntu64Guest",
+											GuestFamily: vspherepolv1.GuestFamilyTypeLinux,
+										},
+									},
+								},
+								Status: vspherepolv1.PolicyEvaluationStatus{
+									ObservedGeneration: 1,
+									Conditions: []metav1.Condition{
+										*conditions.TrueCondition(vspherepolv1.ReadyConditionType),
+									},
+								},
+							}
+
+							// Create the PolicyEvaluation in the fake Kubernetes client
+							Expect(ctx.Client.Create(ctx, policyEval)).To(Succeed())
+						})
+
 						// First create the VM
 						vcVM, err := createOrUpdateAndGetVcVM(ctx, vmProvider, vm)
 						Expect(err).ToNot(HaveOccurred())
@@ -5567,13 +5614,16 @@ func vmTests() {
 
 							// Create a PolicyEvaluation object with updated tags
 							policyEval.Status = vspherepolv1.PolicyEvaluationStatus{
-								ObservedGeneration: 0,
+								ObservedGeneration: policyEval.Generation,
 								Policies: []vspherepolv1.PolicyEvaluationResult{
 									{
 										Name: "test-updated-active-policy",
 										Kind: "ComputePolicy",
 										Tags: []string{policyTag1ID, policyTag2ID, policyTag3ID},
 									},
+								},
+								Conditions: []metav1.Condition{
+									*conditions.TrueCondition(vspherepolv1.ReadyConditionType),
 								},
 							}
 

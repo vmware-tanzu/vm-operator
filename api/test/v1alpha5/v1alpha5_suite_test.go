906d5cffcf120812ec9a8af3abcc49e57896fd7a
Bryan Venteicher <bryan.venteicher@broadcom.com>
2026-03-05 12:33:28 -0600

    Remove VM Spec.Bootstrap.Disabled from v1alpha5
    A decision was made not to include this field in the shipping
    v1a5, so remove it.
    
    Now that there is a difference between v1a5 and v1a6, set up
    the conversion scaffolding.

---
+++ api/test/v1alpha5/v1alpha5_suite_test.go
@@ -0,0 +1,17 @@
+// © Broadcom. All Rights Reserved.
+// The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha5_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestV1Alpha5(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "v1alpha5 Suite")
+}

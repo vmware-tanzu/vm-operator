d60627409b4c04ff3eb260a7ad7e839147bd43d2
Bryan Venteicher <bryan.venteicher@broadcom.com>
2026-03-05 12:33:28 -0600

    Remove VM Spec.Bootstrap.Disabled from v1alpha5
    A decision was made not to include this field in the shipping
    v1a5, so remove it.
    
    Now that there is a difference between v1a5 and v1a6, set up
    the conversion scaffolding.

--- api/v1alpha5/virtualmachine_conversion.go
+++ api/v1alpha5/virtualmachine_conversion.go
@@ -5,21 +5,64 @@
 package v1alpha5
 
 import (
+	apiconversion "k8s.io/apimachinery/pkg/conversion"
 	ctrlconversion "sigs.k8s.io/controller-runtime/pkg/conversion"
+
+	"github.com/vmware-tanzu/vm-operator/api/utilconversion"
 
 	vmopv1 "github.com/vmware-tanzu/vm-operator/api/v1alpha6"
 )
 
+func Convert_v1alpha6_VirtualMachineBootstrapSpec_To_v1alpha5_VirtualMachineBootstrapSpec(
+	in *vmopv1.VirtualMachineBootstrapSpec, out *VirtualMachineBootstrapSpec, s apiconversion.Scope) error {
+
+	return autoConvert_v1alpha6_VirtualMachineBootstrapSpec_To_v1alpha5_VirtualMachineBootstrapSpec(in, out, s)
+}
+
+func restore_v1alpha6_VirtualMachineBootstrapDisabled(dst, src *vmopv1.VirtualMachine) {
+	if bs := src.Spec.Bootstrap; bs != nil {
+		if bs.Disabled {
+			if dst.Spec.Bootstrap == nil {
+				dst.Spec.Bootstrap = &vmopv1.VirtualMachineBootstrapSpec{}
+			}
+			dst.Spec.Bootstrap.Disabled = true
+		}
+	}
+}
+
 // ConvertTo converts this VirtualMachine to the Hub version.
 func (src *VirtualMachine) ConvertTo(dstRaw ctrlconversion.Hub) error {
 	dst := dstRaw.(*vmopv1.VirtualMachine)
-	return Convert_v1alpha5_VirtualMachine_To_v1alpha6_VirtualMachine(src, dst, nil)
+	if err := Convert_v1alpha5_VirtualMachine_To_v1alpha6_VirtualMachine(src, dst, nil); err != nil {
+		return err
+	}
+
+	// Manually restore data.
+	restored := &vmopv1.VirtualMachine{}
+	if ok, err := utilconversion.UnmarshalData(src, restored); err != nil || !ok {
+		return err
+	}
+
+	// BEGIN RESTORE
+
+	restore_v1alpha6_VirtualMachineBootstrapDisabled(dst, restored)
+
+	// END RESTORE
+
+	dst.Status = restored.Status
+
+	return nil
 }
 
 // ConvertFrom converts the hub version to this VirtualMachine.
 func (dst *VirtualMachine) ConvertFrom(srcRaw ctrlconversion.Hub) error {
 	src := srcRaw.(*vmopv1.VirtualMachine)
-	return Convert_v1alpha6_VirtualMachine_To_v1alpha5_VirtualMachine(src, dst, nil)
+	if err := Convert_v1alpha6_VirtualMachine_To_v1alpha5_VirtualMachine(src, dst, nil); err != nil {
+		return err
+	}
+
+	// Preserve Hub data on down-conversion except for metadata
+	return utilconversion.MarshalData(src, dst)
 }
 
 // ConvertTo converts this VirtualMachineList to the Hub version.

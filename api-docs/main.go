c481a6369a0b80697cca57d95ecc4f24385f9bcb
Arunesh Pandey <aruneshpa@gmail.com>
2026-06-12 13:24:41 -0700

    [x-port] [9.1.1] API doc generation changes for VCF 9.1.1 (#1658)
    * :sparkles: Generate API documentation from CRDs (#1602)
    
    Add `api-docs/` infrastructure to generate HTML documentation from CRD
    type definitions using the crd-ref-docs tool.
    
    A separate Go module (api-docs/go.mod) with a stub main.go vendors the
    API packages into a local vendor/ directory, which crd-ref-docs then
    scans. This vendoring approach was chosen over scanning api/ directly
    because crd-ref-docs recurses into all subdirectories, including
    api/test/ whose go.mod requires a newer Go version than the Docker
    image provides. Vendoring naturally includes only the imported packages,
    avoiding this incompatibility without fragile directory filtering.
    
    The Makefile supports both local toolchains (crd-ref-docs, asciidoctor)
    and fallback to a Docker image for CI. A `package` target assembles the
    generated HTML into publish/ for consumption by cayman_vm-operator's
    gobuild pipeline.
    
    * Remove v1a6 references since it is unavailable in 9.1
    
    * :bug: Use updated api-tools to generate API docs (#1656)

---
+++ api-docs/main.go
@@ -0,0 +1,33 @@
+// © Broadcom. All Rights Reserved.
+// The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: Apache-2.0
+
+// Package main is a stub module whose imports determine which API packages are
+// vendored and therefore included in the generated CRD reference documentation.
+package main
+
+import (
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha1"
+
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha2"
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha2/cloudinit"
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha2/common"
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha2/sysprep"
+
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha3"
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha3/cloudinit"
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha3/common"
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha3/sysprep"
+
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha4"
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha4/cloudinit"
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha4/common"
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha4/sysprep"
+
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha5"
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha5/cloudinit"
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha5/common"
+	_ "github.com/vmware-tanzu/vm-operator/api/v1alpha5/sysprep"
+)
+
+func main() {}

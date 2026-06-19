8533387861b5360be7fe80a5db8eca356bf421dd
Bryan Venteicher <bryan.venteicher@broadcom.com>
2026-06-19 14:14:10 -0500

    Update network e2e tests for mixed providers

--- pkg/providers/vsphere/network/list_interfaces_test.go
+++ pkg/providers/vsphere/network/list_interfaces_test.go
@@ -196,20 +196,33 @@ var _ = Describe("ListNetworkInterfaces", func() {
 		initObjects = nil
 	})
 
-	// newNetworkSettings creates a NetworkSettings/default CR for the VM's
-	// namespace that maps to the given provider.
-	newNetworkSettings := func(provider netopv1alpha1.NetworkProvider) *netopv1alpha1.NetworkSettings {
+	newNetworkSettings := func(provider, legacyProvider netopv1alpha1.NetworkProvider) *netopv1alpha1.NetworkSettings {
 		return &netopv1alpha1.NetworkSettings{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "default",
 				Namespace: vm.Namespace,
 			},
-			Provider: provider,
+			Provider:       provider,
+			LegacyProvider: legacyProvider,
 		}
 	}
 
 	makeNetOPIf := func(name string, makeOwner bool) *netopv1alpha1.NetworkInterface {
 		obj := &netopv1alpha1.NetworkInterface{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: vm.Namespace,
+				Labels:    map[string]string{network.VMNameLabel: vm.Name},
+			},
+		}
+		if makeOwner {
+			Expect(ctrlutil.SetOwnerReference(vm, obj, ctx.Client.Scheme())).To(Succeed())
+		}
+		return obj
+	}
+
+	makeNCPIf := func(name string, makeOwner bool) *ncpv1alpha1.VirtualNetworkInterface {
+		obj := &ncpv1alpha1.VirtualNetworkInterface{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      name,
 				Namespace: vm.Namespace,
@@ -259,7 +272,7 @@ var _ = Describe("ListNetworkInterfaces", func() {
 		Context("with VDS NetworkSettings", func() {
 
 			JustBeforeEach(func() {
-				ns := newNetworkSettings(netopv1alpha1.NetworkProviderVSphereDistributed)
+				ns := newNetworkSettings(netopv1alpha1.NetworkProviderVSphereDistributed, "")
 				Expect(ctx.Client.Create(ctx, ns)).To(Succeed())
 			})
 
@@ -313,8 +326,284 @@ var _ = Describe("ListNetworkInterfaces", func() {
 						},
 					}
 
-					// Since the namespace's provider is VDS, only NetOP CRs are
-					// listed; the VPC CR is not eligible even though it exists.
+					err := network.ListOrphanedNetworkInterfaces(vmCtx, ctx.Client, &results)
+					Expect(err).ToNot(HaveOccurred())
+					Expect(results.OrphanedNetworkInterfaces).To(BeEmpty())
+				})
+			})
+		})
+
+		Context("with NSXT NetworkSettings", func() {
+
+			JustBeforeEach(func() {
+				ns := newNetworkSettings(netopv1alpha1.NetworkProviderNSXTier1, "")
+				Expect(ctx.Client.Create(ctx, ns)).To(Succeed())
+			})
+
+			It("returns empty list when no interface CRs exist", func() {
+				ifaces, err := network.ListNetworkInterfaces(vmCtx, ctx.Client)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(ifaces).To(BeEmpty())
+			})
+
+			It("lists owned NCP VirtualNetworkInterface CRs", func() {
+				owned := makeNCPIf(vm.Name+"-eth0", true)
+				Expect(ctx.Client.Create(ctx, owned)).To(Succeed())
+
+				ifaces, err := network.ListNetworkInterfaces(vmCtx, ctx.Client)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(ifaces).To(HaveLen(1))
+				Expect(ifaces[0].GetName()).To(Equal(owned.GetName()))
+			})
+
+			It("excludes unowned NCP VirtualNetworkInterface CRs", func() {
+				owned := makeNCPIf(vm.Name+"-owned", true)
+				Expect(ctx.Client.Create(ctx, owned)).To(Succeed())
+				unowned := makeNCPIf(vm.Name+"-unowned", false)
+				Expect(ctx.Client.Create(ctx, unowned)).To(Succeed())
+
+				ifaces, err := network.ListNetworkInterfaces(vmCtx, ctx.Client)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(ifaces).To(HaveLen(1))
+				Expect(ifaces[0].GetName()).To(Equal(owned.GetName()))
+			})
+
+			It("does not list NetOP or VPC CRs even if they exist", func() {
+				ncpOwned := makeNCPIf(vm.Name+"-eth0", true)
+				Expect(ctx.Client.Create(ctx, ncpOwned)).To(Succeed())
+				// A NetOP CR with the same VM-name label should be invisible.
+				netopIf := makeNetOPIf(vm.Name+"-eth0", true)
+				Expect(ctx.Client.Create(ctx, netopIf)).To(Succeed())
+
+				ifaces, err := network.ListNetworkInterfaces(vmCtx, ctx.Client)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(ifaces).To(HaveLen(1))
+				Expect(ifaces[0].GetName()).To(Equal(ncpOwned.GetName()))
+			})
+
+			Context("ListOrphanedNetworkInterfaces", func() {
+
+				It("same-named NetOP CR from previous provider is invisible when not tracked as a legacy provider", func() {
+					// A leftover NetOP CR with the same name as the active NCP CR.
+					netopIf := makeNetOPIf(vm.Name+"-eth0", true)
+					Expect(ctx.Client.Create(ctx, netopIf)).To(Succeed())
+
+					// Active NCP CR.
+					ncpIf := makeNCPIf(vm.Name+"-eth0", true)
+					Expect(ctx.Client.Create(ctx, ncpIf)).To(Succeed())
+
+					results := network.NetworkInterfaceResults{
+						Results: []network.NetworkInterfaceResult{
+							{
+								ObjectName:         ncpIf.GetName(),
+								ObjectProviderType: pkgcfg.NetworkProviderTypeNSXT,
+							},
+						},
+					}
+
+					err := network.ListOrphanedNetworkInterfaces(vmCtx, ctx.Client, &results)
+					Expect(err).ToNot(HaveOccurred())
+					Expect(results.OrphanedNetworkInterfaces).To(BeEmpty())
+				})
+			})
+		})
+
+		Context("with VPC NetworkSettings", func() {
+
+			JustBeforeEach(func() {
+				ns := newNetworkSettings(netopv1alpha1.NetworkProviderVPC, "")
+				Expect(ctx.Client.Create(ctx, ns)).To(Succeed())
+			})
+
+			It("returns empty list when no interface CRs exist", func() {
+				ifaces, err := network.ListNetworkInterfaces(vmCtx, ctx.Client)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(ifaces).To(BeEmpty())
+			})
+
+			It("lists owned SubnetPort CRs", func() {
+				owned := makeVPCIf(vm.Name+"-eth0", true)
+				Expect(ctx.Client.Create(ctx, owned)).To(Succeed())
+
+				ifaces, err := network.ListNetworkInterfaces(vmCtx, ctx.Client)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(ifaces).To(HaveLen(1))
+				Expect(ifaces[0].GetName()).To(Equal(owned.GetName()))
+			})
+
+			It("excludes unowned SubnetPort CRs", func() {
+				owned := makeVPCIf(vm.Name+"-owned", true)
+				Expect(ctx.Client.Create(ctx, owned)).To(Succeed())
+				unowned := makeVPCIf(vm.Name+"-unowned", false)
+				Expect(ctx.Client.Create(ctx, unowned)).To(Succeed())
+
+				ifaces, err := network.ListNetworkInterfaces(vmCtx, ctx.Client)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(ifaces).To(HaveLen(1))
+				Expect(ifaces[0].GetName()).To(Equal(owned.GetName()))
+			})
+
+			It("does not list NetOP or NCP CRs even if they exist", func() {
+				vpcOwned := makeVPCIf(vm.Name+"-eth0", true)
+				Expect(ctx.Client.Create(ctx, vpcOwned)).To(Succeed())
+				// A NetOP CR with the same VM-name label should be invisible.
+				netopIf := makeNetOPIf(vm.Name+"-eth0", true)
+				Expect(ctx.Client.Create(ctx, netopIf)).To(Succeed())
+
+				ifaces, err := network.ListNetworkInterfaces(vmCtx, ctx.Client)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(ifaces).To(HaveLen(1))
+				Expect(ifaces[0].GetName()).To(Equal(vpcOwned.GetName()))
+			})
+
+			Context("ListOrphanedNetworkInterfaces", func() {
+
+				It("same-named NCP CR from previous provider is invisible when not tracked as a legacy provider", func() {
+					// A leftover NCP CR with the same name as the active VPC CR.
+					ncpIf := makeNCPIf(vm.Name+"-eth0", true)
+					Expect(ctx.Client.Create(ctx, ncpIf)).To(Succeed())
+
+					// Active VPC CR.
+					vpcIf := makeVPCIf(vm.Name+"-eth0", true)
+					Expect(ctx.Client.Create(ctx, vpcIf)).To(Succeed())
+
+					results := network.NetworkInterfaceResults{
+						Results: []network.NetworkInterfaceResult{
+							{
+								ObjectName:         vpcIf.GetName(),
+								ObjectProviderType: pkgcfg.NetworkProviderTypeVPC,
+							},
+						},
+					}
+
+					err := network.ListOrphanedNetworkInterfaces(vmCtx, ctx.Client, &results)
+					Expect(err).ToNot(HaveOccurred())
+					Expect(results.OrphanedNetworkInterfaces).To(BeEmpty())
+				})
+			})
+		})
+
+		Context("with VPC NetworkSettings and a legacy VDS provider", func() {
+
+			JustBeforeEach(func() {
+				ns := newNetworkSettings(netopv1alpha1.NetworkProviderVPC, netopv1alpha1.NetworkProviderVSphereDistributed)
+				Expect(ctx.Client.Create(ctx, ns)).To(Succeed())
+			})
+
+			It("lists owned CRs from both the current and the legacy provider", func() {
+				vpcOwned := makeVPCIf(vm.Name+"-eth0", true)
+				Expect(ctx.Client.Create(ctx, vpcOwned)).To(Succeed())
+				netopOwned := makeNetOPIf(vm.Name+"-eth1", true)
+				Expect(ctx.Client.Create(ctx, netopOwned)).To(Succeed())
+
+				ifaces, err := network.ListNetworkInterfaces(vmCtx, ctx.Client)
+				Expect(err).ToNot(HaveOccurred())
+				names := make([]string, 0, len(ifaces))
+				for _, obj := range ifaces {
+					names = append(names, obj.GetName())
+				}
+				Expect(names).To(ConsistOf(vpcOwned.GetName(), netopOwned.GetName()))
+			})
+
+			It("excludes unowned CRs from either the current or the legacy provider", func() {
+				vpcOwned := makeVPCIf(vm.Name+"-owned", true)
+				Expect(ctx.Client.Create(ctx, vpcOwned)).To(Succeed())
+				vpcUnowned := makeVPCIf(vm.Name+"-vpc-unowned", false)
+				Expect(ctx.Client.Create(ctx, vpcUnowned)).To(Succeed())
+				netopOwned := makeNetOPIf(vm.Name+"-owned-legacy", true)
+				Expect(ctx.Client.Create(ctx, netopOwned)).To(Succeed())
+				netopUnowned := makeNetOPIf(vm.Name+"-netop-unowned", false)
+				Expect(ctx.Client.Create(ctx, netopUnowned)).To(Succeed())
+
+				ifaces, err := network.ListNetworkInterfaces(vmCtx, ctx.Client)
+				Expect(err).ToNot(HaveOccurred())
+				names := make([]string, 0, len(ifaces))
+				for _, obj := range ifaces {
+					names = append(names, obj.GetName())
+				}
+				Expect(names).To(ConsistOf(vpcOwned.GetName(), netopOwned.GetName()))
+			})
+
+			It("does not list NCP CRs, which are neither the current nor the legacy provider", func() {
+				vpcOwned := makeVPCIf(vm.Name+"-eth0", true)
+				Expect(ctx.Client.Create(ctx, vpcOwned)).To(Succeed())
+				ncpIf := makeNCPIf(vm.Name+"-eth1", true)
+				Expect(ctx.Client.Create(ctx, ncpIf)).To(Succeed())
+
+				ifaces, err := network.ListNetworkInterfaces(vmCtx, ctx.Client)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(ifaces).To(HaveLen(1))
+				Expect(ifaces[0].GetName()).To(Equal(vpcOwned.GetName()))
+			})
+
+			Context("ListOrphanedNetworkInterfaces", func() {
+
+				It("treats an unreferenced legacy-provider CR as orphaned, now that it is visible", func() {
+					// Active VPC CR, referenced in the current results.
+					vpcIf := makeVPCIf(vm.Name+"-eth0", true)
+					Expect(ctx.Client.Create(ctx, vpcIf)).To(Succeed())
+					// Leftover NetOP CR from before the migration to VPC; it is
+					// no longer referenced by any result.
+					netopIf := makeNetOPIf(vm.Name+"-eth0-legacy", true)
+					Expect(ctx.Client.Create(ctx, netopIf)).To(Succeed())
+
+					results := network.NetworkInterfaceResults{
+						Results: []network.NetworkInterfaceResult{
+							{
+								ObjectName:         vpcIf.GetName(),
+								ObjectProviderType: pkgcfg.NetworkProviderTypeVPC,
+							},
+						},
+					}
+
+					// Both the current (VPC) and legacy (VDS) providers are
+					// now listed, so the leftover NetOP CR is visible and
+					// correctly reported as orphaned.
+					err := network.ListOrphanedNetworkInterfaces(vmCtx, ctx.Client, &results)
+					Expect(err).ToNot(HaveOccurred())
+					Expect(results.OrphanedNetworkInterfaces).To(HaveLen(1))
+					Expect(results.OrphanedNetworkInterfaces[0].GetName()).To(Equal(netopIf.GetName()))
+				})
+
+				It("does not orphan a legacy-provider CR that is still referenced in the results", func() {
+					vpcIf := makeVPCIf(vm.Name+"-eth0", true)
+					Expect(ctx.Client.Create(ctx, vpcIf)).To(Succeed())
+					netopIf := makeNetOPIf(vm.Name+"-eth1", true)
+					Expect(ctx.Client.Create(ctx, netopIf)).To(Succeed())
+
+					results := network.NetworkInterfaceResults{
+						Results: []network.NetworkInterfaceResult{
+							{
+								ObjectName:         vpcIf.GetName(),
+								ObjectProviderType: pkgcfg.NetworkProviderTypeVPC,
+							},
+							{
+								ObjectName:         netopIf.GetName(),
+								ObjectProviderType: pkgcfg.NetworkProviderTypeVDS,
+							},
+						},
+					}
+
+					err := network.ListOrphanedNetworkInterfaces(vmCtx, ctx.Client, &results)
+					Expect(err).ToNot(HaveOccurred())
+					Expect(results.OrphanedNetworkInterfaces).To(BeEmpty())
+				})
+
+				It("does not list NCP CRs, which are neither the current nor the legacy provider", func() {
+					vpcIf := makeVPCIf(vm.Name+"-eth0", true)
+					Expect(ctx.Client.Create(ctx, vpcIf)).To(Succeed())
+					ncpIf := makeNCPIf(vm.Name+"-eth1", true)
+					Expect(ctx.Client.Create(ctx, ncpIf)).To(Succeed())
+
+					results := network.NetworkInterfaceResults{
+						Results: []network.NetworkInterfaceResult{
+							{
+								ObjectName:         vpcIf.GetName(),
+								ObjectProviderType: pkgcfg.NetworkProviderTypeVPC,
+							},
+						},
+					}
+
 					err := network.ListOrphanedNetworkInterfaces(vmCtx, ctx.Client, &results)
 					Expect(err).ToNot(HaveOccurred())
 					Expect(results.OrphanedNetworkInterfaces).To(BeEmpty())
